804bfb586606f071a1ef2d126d48fd1b2d4ccdaf
H37kouya <h37kouya@gmail.com>
2021-02-07 12:05:10 +0900

    feat circleのベース追加

---
+++ circle/infra/api/index.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export const baseURL = process.env.API_URL ? `${process.env.API_URL}` : 'http://localhost:8000'
+export const axiosInstance = axios.create({
+    baseURL,
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+    },
+    withCredentials: true
+})
