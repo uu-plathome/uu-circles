954c8d3957e5f9d4f44ac79de2c410bbf0e72ddd
H37kouya <h37kouya@gmail.com>
2021-02-07 12:27:18 +0900

    追加

---
+++ circle/lib/types/model/Circle.ts
@@ -0,0 +1,42 @@
+export interface Circle {
+    type: 'Circle'
+    id: number
+    slug: string
+    release: boolean
+    circleType: string
+    name: string
+    nameKana: string
+    shortName: string
+    prefixName: string
+    description: string
+    intro: string
+    placeOfActivity: string
+    placeOfActivityDetail: string
+    doOnlineActivity: boolean
+    dateOfActivityMonday: string
+    dateOfActivityTuesday: string
+    dateOfActivityWednesday: string
+    dateOfActivityThursday: string
+    dateOfActivityFriday: string
+    dateOfActivitySaturday: string
+    dateOfActivitySunday: string
+    dateOfActivityDetail: string
+    admissionFee: string
+    numberOfMembers: number
+    publicEmail: string
+    twitterUrl: string
+    facebookUrl: string
+    instagramUrl: string
+    lineUrl: string
+    youtubeUrl: string
+    homepageUrl: string
+    peingUrl: string
+    githubUrl: string
+    tiktokUrl: string
+    participationUrl: string
+    mainImageUrl: string
+    createdAt: string
+    updatedAt: string
+}
+
+export const isCircle = (v: any): v is Circle => v && v.type === 'Circle'
