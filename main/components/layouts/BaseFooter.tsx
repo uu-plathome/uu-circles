bfc6f7c9d06b84a0b3b977c2baf055aa6f798bf3
H37kouya <h37kouya@gmail.com>
2021-02-09 22:51:52 +0900

    feat footerを少し作った

---
+++ main/components/layouts/BaseFooter.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+
+type Props = {}
+const BaseFooter: FC<Props> = () => {
+    return (
+        <div className="bg-gray-100 px-6">
+            <div className="pt-6">
+                <h2 className="text-lg mb-6">メニュー</h2>
+                
+                <ul>
+                    <li className="mb-3"><a href="" className="text-gray-400 font-bold">サークルを見つける</a></li>
+                    <li className="mb-3"><a href="" className="text-gray-400 font-bold">運営団体について</a></li>
+                    <li className="mb-3"><a href="" className="text-gray-400 font-bold">お問い合わせ</a></li>
+                    <li className="mb-3"><a href="" className="text-gray-400 font-bold">ヘルプ</a></li>
+                </ul>
+            </div>
+
+            <div className="pt-8 text-center">
+                <hr className="border border-gray-200"/>
+                <div className="pt-8 pb-16">
+                    <a href="" className="text-gray-400 px-2">運営団体</a>
+                    <a href="" className="text-gray-400 px-2">利用規約</a>
+                    <a href="" className="text-gray-400 px-2">プライバシーポリシー</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { BaseFooter }
--- main/pages/index.tsx
+++ main/pages/index.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from "next";
+import { BaseHeader } from "@/components/layouts/BaseHeader";
+import { BaseFooter } from "@/components/layouts/BaseFooter";
 
 const Index: NextPage = () => {
     return (
         <div>
+            <BaseHeader />
+
             Hello
+
+            <BaseFooter />
         </div>
     )
 }
