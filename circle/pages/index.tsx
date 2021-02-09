29541cd2952aa49a05b27d3f8a7bb1fb51dd4ad2
H37kouya <h37kouya@gmail.com>
2021-02-09 22:57:03 +0900

    feat footerの追加

--- circle/pages/index.tsx
+++ circle/pages/index.tsx
@@ -1,9 +1,12 @@
+import { BaseFooter } from '@/components/layouts/BaseFooter'
 import { NextPage } from 'next'
 
 const IndexPage: NextPage = () => {
   return (
     <div>
       <p>HELLO</p>
+
+      <BaseFooter />
     </div>
   )
 }
