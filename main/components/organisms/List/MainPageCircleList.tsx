c42efc7bfe4a8a4a11a02c271f8c03551b694012
H37kouya <h37kouya@gmail.com>
2021-02-11 09:50:15 +0900

    feat サークルのビラを表示できるようにした

--- main/components/organisms/List/MainPageCircleList.tsx
+++ main/components/organisms/List/MainPageCircleList.tsx
@@ -1,13 +1,31 @@
 import { FC } from "react";
+import Image from 'next/image'
 import { Circle } from "@/lib/types/model/Circle";
 
 type Props = {
     circles: Circle[]
 }
 const MainPageCircleList: FC<Props> = ({ circles }) => {
+    const width = 400
+    // w : h = 210 : 297
+    const height = width * 297 / 210
+
     return (
         <div>
-
+            <div className="max-w-screen-md md:mx-auto grid grid-cols-3 gap-4">
+                {circles.map((circle) => {
+                    return (
+                        <div key={circle.id}>
+                            <Image 
+                                src={circle.handbillImageUrl} 
+                                alt={`${circle.name}のビラ`}
+                                width={width} 
+                                height={height}
+                            />
+                        </div>
+                    )
+                }) }
+            </div>
         </div>
     )
 }
--- main/pages/index.tsx
+++ main/pages/index.tsx
@@ -4,26 +4,27 @@ import { BaseFooter } from "@/components/layouts/BaseFooter";
 import { MainPageCircleList } from "@/components/organisms/List/MainPageCircleList";
 import { getMain } from "@/infra/api/main";
 import { Circle } from "@/lib/types/model/Circle";
+import { BaseContainer } from "@/components/molecules/Container/BaseContainer";
 
 type Props = {
     circles: Circle[]
 }
 const Index: NextPage<Props> = ({ circles }) => {
+
     return (
         <div>
             <BaseHeader />
 
-            Hello
-            <div>
+            <BaseContainer>
                 <MainPageCircleList circles={circles} />
-            </div>
+            </BaseContainer>
 
             <BaseFooter />
         </div>
     )
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
     const { circles } = await getMain()
 
     return {
