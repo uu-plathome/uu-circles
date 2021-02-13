44d7cf52b99c5b5f96ad18f42592e73f96b281f3
H37kouya <h37kouya@gmail.com>
2021-02-13 22:06:25 +0900

    feat サークル詳細を取得できるようにした

--- api/app/Http/Controllers/Main/CircleNewJoy/ShowCircleNewJoyController.php
+++ api/app/Http/Controllers/Main/CircleNewJoy/ShowCircleNewJoyController.php
@@ -3,18 +3,59 @@
 namespace App\Http\Controllers\Main\CircleNewJoy;
 
 use App\Http\Controllers\Controller;
+use App\Models\CircleNewJoy;
+use App\Support\Arr;
+use App\Usecases\Main\CircleNewJoy\IndexCircleNewJoyUsecase;
+use App\Usecases\Main\Circle\GetCircleBySlugUsecase;
+use App\ValueObjects\CircleNewJoyValueObject;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
 
 class ShowCircleNewJoyController extends Controller
 {
+    private GetCircleBySlugUsecase $getCircleBySlugUsecase;
+    private IndexCircleNewJoyUsecase $indexCircleNewJoyUsecase;
+
+    public function __construct(
+        GetCircleBySlugUsecase $getCircleBySlugUsecase,
+        IndexCircleNewJoyUsecase $indexCircleNewJoyUsecase
+    ) {
+        $this->getCircleBySlugUsecase = $getCircleBySlugUsecase;
+        $this->indexCircleNewJoyUsecase = $indexCircleNewJoyUsecase;
+    }
+
     /**
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, string $slug, int $circleNewJoyId)
     {
-        //
+        $circle = $this->getCircleBySlugUsecase->invoke($slug);
+        $circleNewJoy = CircleNewJoy::nowPublic(Carbon::now())->findOrFail($circleNewJoyId);
+        $circleNewJoys = $this->indexCircleNewJoyUsecase->invoke($circle->id);
+
+        return Arr::camel_keys([
+            'circle'       => $circle->toArray(),
+            'circleNewJoy' => CircleNewJoyValueObject::byEloquent($circleNewJoy),
+            // 新歓開催済み
+            'pastCircleNewJoys'   => (new Collection($circleNewJoys['pastCircleNewJoys']))->map(
+                fn (CircleNewJoyValueObject $circleNewJoyValueObject) => $circleNewJoyValueObject->toArray()
+            )->values()->toArray(),
+            // 新歓開催前
+            'futureCircleNewJoys' => (new Collection($circleNewJoys['futureCircleNewJoys']))->map(
+                fn (CircleNewJoyValueObject $circleNewJoyValueObject) => $circleNewJoyValueObject->toArray()
+            )->values()->toArray(),
+            // 現在開催中
+            'nowCircleNewJoys'    => (new Collection($circleNewJoys['nowCircleNewJoys']))->map(
+                fn (CircleNewJoyValueObject $circleNewJoyValueObject) => $circleNewJoyValueObject->toArray()
+            )->values()->toArray(),
+            // 今日の新歓
+            'todayCircleNewJoys'  => (new Collection($circleNewJoys['todayCircleNewJoys']))->map(
+                fn (CircleNewJoyValueObject $circleNewJoyValueObject) => $circleNewJoyValueObject->toArray()
+            )->values()->toArray(),
+        ]);
     }
 }
--- main/infra/api/circleNewJoy.ts
+++ main/infra/api/circleNewJoy.ts
@@ -23,3 +23,26 @@ export const getCircleNewJoyBySlug = async (slug: string) => {
         todayCircleNewJoys: data.todayCircleNewJoys,
     }
 }
+
+export const showCircleNewJoyBySlug = async (slug: string, circleNewJoyId: number) => {
+    type Response = {
+        /** サークル */ circle: Circle
+        /** 新歓詳細 */ circleNewJoy: CircleNewJoy
+        /** 新歓開催済み */ pastCircleNewJoys: CircleNewJoy[]
+        /** 新歓開催前 */ futureCircleNewJoys: CircleNewJoy[]
+        /** 現在開催中 */ nowCircleNewJoys: CircleNewJoy[]
+        /** 今日の新歓 */ todayCircleNewJoys: CircleNewJoy[]
+    }
+    const { data } = await axiosInstance.get<Response>(
+        `${linkConst.CIRCLE.GROUP}/${slug}/newjoy/${circleNewJoyId}`
+    )
+
+    return {
+        /** サークル */ circle: data.circle,
+        /** 新歓詳細 */ circleNewJoy: data.circleNewJoy,
+        /** 新歓開催済み */ pastCircleNewJoys: data.pastCircleNewJoys,
+        /** 新歓開催前 */futureCircleNewJoys: data.futureCircleNewJoys,
+        /** 現在開催中 */nowCircleNewJoys: data.nowCircleNewJoys,
+        /** 今日の新歓 */todayCircleNewJoys: data.todayCircleNewJoys,
+    }
+}
