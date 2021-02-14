de9f6b0bea56cc90ff44fe3b10e7dd10975b755c
H37kouya <h37kouya@gmail.com>
2021-02-14 17:26:11 +0900

    feat おすすめのカテゴリー追加

--- main/components/molecules/Container/BaseContainer.tsx
+++ main/components/molecules/Container/BaseContainer.tsx
@@ -2,8 +2,10 @@ import { FC } from "react";
 
 const BaseContainer: FC = ({ children }) => {
     return (
-        <div className="md:container mx-auto">
-            { children }
+        <div className="bg-gray-100">
+            <div className="md:container mx-auto">
+                { children }
+            </div>
         </div>
     )
 }
