e6ae4194d626b573aaa488ae62ec9a9931ef5995
H37kouya <h37kouya@gmail.com>
2021-02-15 21:19:53 +0900

    feat add pwa

--- main/next.config.js
+++ main/next.config.js
@@ -1,8 +1,21 @@
-module.exports = {
-    images: {
-      domains: [
-        'localhost',
-        'firebasestorage.googleapis.com',
-      ],
-    },
-  }
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  images: {
+    domains: [
+      'localhost',
+      'firebasestorage.googleapis.com',
+    ],
+  },
+
+  pwa: {
+    dest: "public", // swの出力ディレクトリ
+    runtimeCaching: [
+      {
+        urlPattern:
+          '^https://firebasestorage.googleapis.com/v0/b/uu-circle20.appspot.com/(.*)',
+        handler: 'cacheFirst',
+      },
+    ]
+  },
+});
