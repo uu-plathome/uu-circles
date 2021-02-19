e5d92eeec67a08eb8deeffc6e29057202d38ddfc
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-02-19 20:56:55 +0900

    wip scrollTopが効かない

--- main/components/organisms/Main/MainUucircleTopButtons.tsx
+++ main/components/organisms/Main/MainUucircleTopButtons.tsx
@@ -3,11 +3,18 @@ import Image from 'next/image'
 import Link from 'next/link'
 type Props = {}
 const MainUucircleTopButtons: FC<Props> = () => {
+  if (process.browser) {
+    // ブラウザ側で使う処理
+    // console.log('')
+    // const scrollElement = document.getElementById('top-button-scroll')
+    // scrollElement.scrollTop = 300
+  }
   return (
     <div className="flex justify-center">
       <nav
         className="horizontal_scroll md:flex md:justify-center"
         style={{ margin: '0 auto!important' }}
+        id="top-button-scroll"
       >
         <button
           className="w-screen rounded  m-10  focus:outline-none focus:ring-2 ring-blue-300 ring-offset-2"
