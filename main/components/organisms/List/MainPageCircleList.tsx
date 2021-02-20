0be7bfe9f19b2d8e1595866a16980d753c5f3890
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-02-20 21:43:45 +0900

    wip npmのエラー

--- main/components/organisms/List/MainPageCircleList.tsx
+++ main/components/organisms/List/MainPageCircleList.tsx
@@ -6,6 +6,7 @@ import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper-bundle.min.css'
 import 'swiper/components/navigation/navigation.min.css' //Swiperのnavigation用
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 type Props = {
   circles: Circle[]
@@ -14,43 +15,68 @@ const MainPageCircleList: FC<Props> = ({ circles }) => {
   const width = 400
   // w : h = 210 : 297
   const height = (width * 297) / 210
+  const { isMd } = useMediaQuery()
 
   return (
     <div>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        // onSlideChange={() => console.log('slide change')}
-        // onSwiper={(swiper) => console.log(swiper)}
-        pagination={{ clickable: true }}
-        loop={true}
-        autoplay={{ delay: 2500, disableOnInteraction: false }}
-        navigation
-      >
-        {/* <div className="max-w-screen-md md:mx-auto grid grid-cols-3 gap-4"> */}
-        <div className=" mx-auto max-w-screen-md md:mx-auto md:grid md:grid-cols-3 md:gap-4">
+      {isMd ? (
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          // onSlideChange={() => console.log('slide change')}
+          // onSwiper={(swiper) => console.log(swiper)}
+          pagination={{ clickable: true }}
+          loop={true}
+          autoplay={{ delay: 2500, disableOnInteraction: false }}
+          navigation
+        >
+          {/* <div className="max-w-screen-md md:mx-auto grid grid-cols-3 gap-4"> */}
+          <div className=" mx-auto max-w-screen-md md:mx-auto md:grid md:grid-cols-3 md:gap-4">
+            {circles.map((circle) => {
+              return (
+                <SwiperSlide className="text-center">
+                  <div key={circle.id}>
+                    <Link href="/circle/[slug]" as={`/circle/${circle.slug}`}>
+                      <a>
+                        <Image
+                          src={circle.handbillImageUrl}
+                          alt={`${circle.name}のビラ`}
+                          width={width}
+                          height={height}
+                        />
+                      </a>
+                    </Link>
+
+                    <h3 className="text-center font-bold">{circle.name}</h3>
+                  </div>
+                </SwiperSlide>
+              )
+            })}
+          </div>
+        </Swiper>
+      ) : (
+        <div className="horizontal_scroll mx-auto max-w-screen-md md:mx-auto md:grid md:grid-cols-3 md:gap-4">
           {circles.map((circle) => {
             return (
-              <SwiperSlide className="text-center">
-                <div key={circle.id}>
-                  <Link href="/circle/[slug]" as={`/circle/${circle.slug}`}>
-                    <a>
-                      <Image
-                        src={circle.handbillImageUrl}
-                        alt={`${circle.name}のビラ`}
-                        width={width}
-                        height={height}
-                      />
-                    </a>
-                  </Link>
+              <div key={circle.id}>
+                <Link href="/circle/[slug]" as={`/circle/${circle.slug}`}>
+                  <a>
+                    <Image
+                      src={circle.handbillImageUrl}
+                      alt={`${circle.name}のビラ`}
+                      width={width}
+                      height={height}
+                    />
+                  </a>
+                </Link>
 
-                  <h3 className="text-center font-bold">{circle.name}</h3>
-                </div>
-              </SwiperSlide>
+                <h3 className="text-center font-bold">{circle.name}</h3>
+              </div>
             )
           })}
         </div>
-      </Swiper>
+      )}
+
       <div className="text-center mt-10 mb-10">
         <Link href="">
           <a
