54b773df5d4b4c94b42170006453301d4542d48c
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-02-21 18:05:49 +0900

    wip APIの値がうまく取れない

--- main/pages/circle/newjoy.tsx
+++ main/pages/circle/newjoy.tsx
@@ -5,8 +5,10 @@ import { CircleNewJoy } from '@/lib/types/model/CircleNewJoy'
 import { BaseContainer } from '@/components/molecules/Container/BaseContainer'
 import { BaseLayout } from '@/components/layouts/BaseLayout'
 import Link from 'next/link'
+import { IndexCircleNewJoyList } from '@/components/organisms/List/IndexCircleNewJoyList'
 type Props = {
   errorCode?: number
+
   futureCircleNewJoys?: {
     slug: string
     circleNewJoy: CircleNewJoy
@@ -17,25 +19,34 @@ type Props = {
   }[]
 }
 const Page: NextPage<Props> = ({ futureCircleNewJoys, todayCircleNewJoys }) => {
+  console.log('TOP')
+  console.log(todayCircleNewJoys)
   return (
     <div>
       <BaseLayout>
         <div className="bg-gray-100 px-2">
           <BaseContainer>
             <h1 className="text-2xl py-8">今日の新歓</h1>
-
             <div className="pb-16">
-              <h2 className="font-bold text-lg md:text-center pl-1 mb-3">
-                今日開催予定の新歓イベント
-              </h2>
+              <div className="pb-16">
+                <h2 className="font-bold text-lg md:text-center pl-1 mb-3">
+                  今日開催予定の新歓イベント
+                </h2>
+                {todayCircleNewJoys && todayCircleNewJoys.length > 0 ? (
+                  <IndexCircleNewJoyList
+                    // slug={circle.slug}
+                    circleNewJoys={todayCircleNewJoys}
+                  />
+                ) : (
+                  <p>今日の新歓はありません</p>
+                )}
+              </div>
               <section className=" border-2 border-gray-700 rounded-xl grid grid-cols-5 mx-3 bg-white px-2 py-2 pl-6">
                 <div className="col-span-4">
                   <h2 className="font-bold text-xl">U-lab新歓説明会</h2>
                   <div className="border-b-2  grid grid-cols-8">
                     <p className="text-gray-600 text-xs col-span-1">場所</p>
-                    <p className="text-gray-600 text-xs col-span-6 text-center">
-                      あああ
-                    </p>
+                    <p className="text-gray-600 text-xs col-span-6 text-center"></p>
                   </div>
                   <div className="grid grid-cols-2">
                     <div className="border-b-2  grid grid-cols-8">
