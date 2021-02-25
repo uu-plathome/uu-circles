62e20e9522fb740a75ad10d91d82e115d7c9cd04
H37kouya <h37kouya@gmail.com>
2021-02-25 19:38:27 +0900

    feat プライバシーポリシーの追加

--- main/components/layouts/BaseFooter.tsx
+++ main/components/layouts/BaseFooter.tsx
@@ -180,7 +180,9 @@ const BaseFooter: FC<Props> = () => {
                         <div className="pt-8 pb-16">
                             <a href="" className="text-gray-400 px-2 text-xs">運営団体</a>
                             <a href="" className="text-gray-400 px-2 text-xs">利用規約</a>
-                            <a href="" className="text-gray-400 px-2 text-xs">プライバシーポリシー</a>
+                            <Link href="/privacy">
+                                <a className="text-gray-400 px-2 text-xs">プライバシーポリシー</a>
+                            </Link>
                         </div>
                     </BaseContainer>
                 </div>
