b41850d46882650337befbc0796620ccfd42dab2
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-02-26 20:51:24 +0900

    wip コンポーネント作っておいて適応していなかった

--- main/components/organisms/ListItem/CircleNewJoyListItemPC.tsx
+++ main/components/organisms/ListItem/CircleNewJoyListItemPC.tsx
@@ -3,13 +3,7 @@ import Link from 'next/link'
 import { CircleNewJoy } from '@/lib/types/model/CircleNewJoy'
 import { __ } from '@/lang/ja'
 import dayjs from 'dayjs'
-const getDOW = (circleNewJoy: CircleNewJoy) => {
-  //曜日取得関数 dayjs
-  const date = dayjs(circleNewJoy.startDate)
-  const DOWList = ['日', '月', '火', '水', '木', '金', '土'] //dayjsは日曜始まり
-  const DOW = DOWList[date.day()]
-  return DOW
-}
+import { getDOW } from '@/lib/utils/getDOW'
 const getMonth = (circleNewJoy: CircleNewJoy) => {
   if (circleNewJoy.startDate) {
     const date = dayjs(circleNewJoy.startDate)
@@ -56,6 +50,7 @@ type Props = {
   circleNewJoy: CircleNewJoy
 }
 const CircleNewJoyListItemPC: FC<Props> = ({ slug, circleNewJoy }) => {
+  console.log(circleNewJoy)
   return (
     <div
       className="border border-gray-300 bg-white rounded-xl flex justify-between items-center px-6 py-2 mx-auto mb-2"
@@ -69,7 +64,7 @@ const CircleNewJoyListItemPC: FC<Props> = ({ slug, circleNewJoy }) => {
         }}
       >
         <div className="bg-gray-600 text-white  rounded-2xl rounded-b-none text-center ">
-          <p className="text-xs leading-5">{getDOW(circleNewJoy)}</p>
+          <p className="text-xs leading-5">{getDOW(circleNewJoy.startDate)}</p>
         </div>
         <div
           className=" text-black  text-center rounded-2xl rounded-t-none  items-center pb-4"
