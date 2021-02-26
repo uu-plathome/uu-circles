56d08e310f100baa572d4e536f7f41124fa30c1e
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-02-26 23:48:35 +0900

    wip APIのとり方

--- main/components/organisms/List/IndexCircleNewJoyListForDetail.tsx
+++ main/components/organisms/List/IndexCircleNewJoyListForDetail.tsx
@@ -1,30 +1,33 @@
 import { FC } from 'react'
 import { CircleNewJoyListItemForDetail } from '../ListItem/CircleNewJoyListItemForDetail'
-import { TodayCircleNewJoy } from '@/infra/api/circleNewJoy'
+import { CircleNewJoy } from '@/lib/types/model/CircleNewJoy'
+import { useRouter } from 'next/dist/client/router'
 
 type Props = {
-  circleNewJoys: TodayCircleNewJoy[]
+  circleNewJoy: CircleNewJoy[]
 }
-const IndexCircleNewJoyListForDetail: FC<Props> = ({ circleNewJoys }) => {
+const IndexCircleNewJoyListForDetail: FC<Props> = ({ circleNewJoy }) => {
   return (
     <div className="bg-gray-100">
       <div className="flex justify-center flex-wrap">
-        {circleNewJoys &&
-          circleNewJoys.map((circleNewJoy, idx) => {
+        {circleNewJoy &&
+          circleNewJoy.map((circleNewJoy, idx) => {
             let className = idx % 3 === 0 ? 'place-self-end' : ''
             className = idx % 3 === 1 ? 'place-self-center' : className
             className = idx % 3 === 2 ? 'place-self-start' : className
-
-            return (
-              <div
-                key={circleNewJoy.circleNewJoy.id}
-                className={`md:px-4 ${className}`}
-              >
-                <CircleNewJoyListItemForDetail
-                  todayCircleNewJoy={circleNewJoy}
-                />
-              </div>
-            )
+            const router = useRouter()
+            const { NewJoyId } = router.query
+            if (circleNewJoy.id == NewJoyId) {
+              return (
+                <div
+                  key={circleNewJoy.circleNewJoy.id}
+                  className={`md:px-4 ${className}`}
+                >
+                  <CircleNewJoyListItemForDetail circleNewJoy={circleNewJoy} />
+                </div>
+              )
+            }
+            return 'エラー'
           })}
       </div>
     </div>
