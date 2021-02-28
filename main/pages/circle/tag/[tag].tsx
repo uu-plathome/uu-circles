b184b32c69632288ed3a58c643d94322fe7e0b9a
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-02-28 23:13:57 +0900

    fix circle/tag/[]のふと文字化

--- main/pages/circle/tag/[tag].tsx
+++ main/pages/circle/tag/[tag].tsx
@@ -1,73 +1,78 @@
-import { GetServerSideProps, NextPage } from "next";
-import { BaseFooter } from "@/components/layouts/BaseFooter";
-import { BaseLayout } from "@/components/layouts/BaseLayout";
-import { BaseCircleList } from "@/components/organisms/List/BaseCircleList";
-import { getCircleByTag } from "@/infra/api/circle";
-import { Circle } from "@/lib/types/model/Circle";
-import { TwoColumnContainer } from "@/components/molecules/Container/TwoColumnContainer";
-import { CircleSidebar } from "@/components/organisms/Circles/CircleSidebar";
-import { useRouter } from "next/dist/client/router";
-import { __ } from "@/lang/ja";
-import { categoryToCircleType } from "@/lib/utils/category/Category";
-import { Category } from "@/lib/enum/app/Category";
-import Head from "next/head";
-import { BaseHead } from "@/components/layouts/BaseHead";
+import { GetServerSideProps, NextPage } from 'next'
+import { BaseFooter } from '@/components/layouts/BaseFooter'
+import { BaseLayout } from '@/components/layouts/BaseLayout'
+import { BaseCircleList } from '@/components/organisms/List/BaseCircleList'
+import { getCircleByTag } from '@/infra/api/circle'
+import { Circle } from '@/lib/types/model/Circle'
+import { TwoColumnContainer } from '@/components/molecules/Container/TwoColumnContainer'
+import { CircleSidebar } from '@/components/organisms/Circles/CircleSidebar'
+import { useRouter } from 'next/dist/client/router'
+import { __ } from '@/lang/ja'
+import { categoryToCircleType } from '@/lib/utils/category/Category'
+import { Category } from '@/lib/enum/app/Category'
+import Head from 'next/head'
+import { BaseHead } from '@/components/layouts/BaseHead'
 
 type Props = {
-    errorCode?: number
-    circles?: Circle[]
+  errorCode?: number
+  circles?: Circle[]
 }
-const Page: NextPage<Props> = ({
-    circles
-}) => {
-    const router = useRouter()
-    const { tag } = router.query
+const Page: NextPage<Props> = ({ circles }) => {
+  const router = useRouter()
+  const { tag } = router.query
 
-    return (
-        <div>
-            <BaseHead
-                title={ `${__(String(tag).toUpperCase())}タグ検索` }
-            />
+  return (
+    <div>
+      <BaseHead title={`${__(String(tag).toUpperCase())}タグ検索`} />
 
-            <BaseLayout>
-                <div className="bg-gray-100 px-2">
-                    <TwoColumnContainer sidebar={<CircleSidebar />}>
-                        <h1 className="text-2xl py-8">{ __(String(tag).toUpperCase()) }</h1>
+      <BaseLayout>
+        <div className="bg-gray-100 px-2">
+          <TwoColumnContainer sidebar={<CircleSidebar />}>
+            <h1 className="text-2xl py-8">{__(String(tag).toUpperCase())}</h1>
 
-                        {__(String(tag).toUpperCase(), "CircleTagTitle") ? (
-                            <p className="text-base pb-4">{ __(String(tag).toUpperCase(), "CircleTagTitle") }</p>
-                        ) : ''}
-                        {__(String(tag).toUpperCase(), "CircleTagText") ? (
-                            <p className="text-sm pb-8">{ __(String(tag).toUpperCase(), "CircleTagText") }</p>
-                        ) : ''}
+            {__(String(tag).toUpperCase(), 'CircleTagTitle') ? (
+              <p className="text-base pb-4 font-bold">
+                {__(String(tag).toUpperCase(), 'CircleTagTitle')}
+              </p>
+            ) : (
+              ''
+            )}
+            {__(String(tag).toUpperCase(), 'CircleTagText') ? (
+              <p className="text-sm pb-8">
+                {__(String(tag).toUpperCase(), 'CircleTagText')}
+              </p>
+            ) : (
+              ''
+            )}
 
-                        {/*  サークル一覧 */}
-                        <BaseCircleList circles={circles} />
-                    </TwoColumnContainer>
-                </div>
-
-                {/*  フッター */}
-                <BaseFooter />
-            </BaseLayout>
+            {/*  サークル一覧 */}
+            <BaseCircleList circles={circles} />
+          </TwoColumnContainer>
         </div>
-    )
+
+        {/*  フッター */}
+        <BaseFooter />
+      </BaseLayout>
+    </div>
+  )
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async ({ params, res }) => {
-    if (!params.tag || Array.isArray(params.tag)) {
-        res.statusCode = 404;
-        return { props: { errorCode: 404 } }
-    }
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  params,
+  res,
+}) => {
+  if (!params.tag || Array.isArray(params.tag)) {
+    res.statusCode = 404
+    return { props: { errorCode: 404 } }
+  }
 
-    const {
-        circles 
-    } = await getCircleByTag(params.tag)
+  const { circles } = await getCircleByTag(params.tag)
 
-    return {
-        props: {
-            circles
-        }
-    }
+  return {
+    props: {
+      circles,
+    },
+  }
 }
 
 export default Page
