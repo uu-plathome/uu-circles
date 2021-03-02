bbc76c0ab356cb945a1c9c077de4fa7b64814966
H37kouya <h37kouya@gmail.com>
2021-03-02 21:58:51 +0900

    feat 検索の追加

---
+++ main/components/atoms/form/SearchTextField.tsx
@@ -0,0 +1,60 @@
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FC, InputHTMLAttributes } from 'react'
+
+export type Props = {
+    id: string
+    name: InputHTMLAttributes<any>['name']
+    value: string|number
+    expand?: boolean
+    required?: boolean
+    type?: InputHTMLAttributes<any>['type']
+    placeholder?: InputHTMLAttributes<any>['placeholder']
+    error?: string
+    maxLength?: number
+    pattern?: string
+    disabled?: boolean
+    onChange(e: any): void
+}
+const SearchTextField: FC<Props> = ({
+    id,
+    name,
+    value,
+    type = 'text',
+    placeholder,
+    maxLength,
+    pattern,
+    expand,
+    disabled,
+    onChange
+}) => {
+    return (
+        <div className="flex flex-col">
+            <div className="relative text-gray-600">
+                <input
+                    type={type}
+                    id={id}
+                    name={name}
+                    value={value}
+                    placeholder={placeholder}
+                    onChange={onChange}
+                    maxLength={maxLength}
+                    className="border-2 border-gray-300 bg-white h-10 pl-3 pr-12 rounded-lg text-sm focus:outline-none"
+                    pattern={pattern}
+                    disabled={disabled}
+                    style={{
+                        width: expand ? '100%' : 'auto'
+                    }}
+                />
+
+                <p className="absolute right-0 top-0 mt-0.5 mr-2 md:mr-1 whitespace-nowrap text-black-900 px-1 md:px-2 flex items-center text-xs md:text-base" style={{ height: 34 }}>
+                    <button type="submit">
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export { SearchTextField }
--- main/pages/circle/index.tsx
+++ main/pages/circle/index.tsx
@@ -7,6 +7,10 @@ import { Circle } from "@/lib/types/model/Circle";
 import { TwoColumnContainer } from "@/components/molecules/Container/TwoColumnContainer";
 import { CircleSidebar } from "@/components/organisms/Circles/CircleSidebar";
 import { BaseHead } from "@/components/layouts/BaseHead";
+import { SearchTextField } from "@/components/atoms/form/SearchTextField";
+import { useStringInput } from "@/hooks/useInput";
+import { useRouter } from "next/dist/client/router";
+import { FormEvent } from "react";
 
 type Props = {
     errorCode?: number
@@ -15,6 +19,13 @@ type Props = {
 const Page: NextPage<Props> = ({
     circles
 }) => {
+    const router = useRouter()
+    const name = useStringInput('')
+    const onSubmit = (event: FormEvent) => {
+        event.preventDefault()
+        router.push(`/circle/search/[name]`, `/circle/search/${name.value}`)
+    }
+
     return (
         <div>
             <BaseHead
@@ -26,6 +37,17 @@ const Page: NextPage<Props> = ({
                     <TwoColumnContainer sidebar={<CircleSidebar />}>
                         <div className="px-7">
                             <h1 className="text-2xl py-8">サークル一覧</h1>
+
+                            <div className="md:hidden mb-8">
+                                <form onSubmit={onSubmit}>
+                                    <SearchTextField
+                                        id="search"
+                                        name="search"
+                                        expand
+                                        { ...name }
+                                    />
+                                </form>
+                            </div>
 
                             {/*  サークル一覧 */}
                             <BaseCircleList circles={circles} />
