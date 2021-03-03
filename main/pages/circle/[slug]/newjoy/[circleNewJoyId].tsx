3938044023ef07e572b92897881898c845e6b7aa
H37kouya <h37kouya@gmail.com>
2021-03-03 20:23:53 +0900

    fix いらないサークル紹介を削除

--- main/pages/circle/[slug]/newjoy/[circleNewJoyId].tsx
+++ main/pages/circle/[slug]/newjoy/[circleNewJoyId].tsx
@@ -131,7 +131,8 @@ const Page: NextPage<Props> = ({
                     circleNewJoys={futureCircleNewJoys}
                   />
                 </div>
-                <div className="pb-16">
+
+                <div className="pb-32">
                   <h2 className="font-bold text-lg md:text-center pl-1 mb-3">
                     開催済み
                   </h2>
@@ -141,43 +142,6 @@ const Page: NextPage<Props> = ({
                     circleNewJoys={pastCircleNewJoys}
                   />
                 </div>
-                <div className="pb-16">
-                  <Link href="/circle/[slug]" as={`/circle/${circle.slug}`}>
-                    <a>
-                      <h2 className="font-bold text-lg pl-1 mb-3">
-                        主催サークル
-                      </h2>
-                      <div
-                        className="border border-4 border-gray-300 bg-white rounded-lg flex justify-between items-center px-2 py-2 mx-auto mb-2"
-                        style={{ width: 300 }}
-                      >
-                        <div className="mr-2" style={{ width: 64 }}>
-                          <Image
-                            src={
-                              circle.mainImageUrl
-                                ? circle.mainImageUrl
-                                : '/images/no-image.png'
-                            }
-                            alt={`${circle.name}のロゴ`}
-                            width={64}
-                            height={64}
-                            className="rounded-full border border-gray-300"
-                          />
-                        </div>
-                        <div className="w-full pr-2" style={{ width: 200 }}>
-                          <h3 className="text-black font-bold mb-1 text-sm font-bold">
-                            {circle.name}
-                          </h3>
-                          <div>
-                            <p className="text-xs text-gray-600">
-                              {circle.description}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                  </Link>
-                </div>{' '}
               </div>
             ) : (
               // SP
