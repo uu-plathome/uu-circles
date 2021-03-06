0a1801dec05857646cf7495e979040b52e7a6470
Lint Action <lint-action@samuelmeuli.com>
2021-03-06 17:22:58 +0000

    Fix code style issues with Prettier

--- main/pages/guide/qanda.tsx
+++ main/pages/guide/qanda.tsx
@@ -29,15 +29,18 @@ const Page: NextPage<Props> = () => {
       <BaseLayout>
         <div className="bg-gray-100 px-2">
           <BaseContainer>
-            
             <div className="bg-green-500 py-6">
               <p className="text-center text-5xl text-white">Q and A</p>
             </div>
 
-
             <div className="pb-6">
-              <SubHeaderGreen>Q. タグの一覧など、諸情報はどこにありますか？</SubHeaderGreen>
-              <SubHeaderPink>A. ページ下部からアクセスできます。タグの一覧は”全てのタグ”から確認することが可能です。</SubHeaderPink>
+              <SubHeaderGreen>
+                Q. タグの一覧など、諸情報はどこにありますか？
+              </SubHeaderGreen>
+              <SubHeaderPink>
+                A.
+                ページ下部からアクセスできます。タグの一覧は”全てのタグ”から確認することが可能です。
+              </SubHeaderPink>
             </div>
 
             <div className="pb-8 pt-4">
@@ -46,33 +49,47 @@ const Page: NextPage<Props> = () => {
 
             <div className="pb-6">
               <SubHeaderGreen>Q. 新歓はどこで行われるのですか？</SubHeaderGreen>
-              <SubHeaderPink>A. 新歓はdiscordやzoomというアプリ上で、オンラインで行われます。詳細はこちらから。</SubHeaderPink>
+              <SubHeaderPink>
+                A.
+                新歓はdiscordやzoomというアプリ上で、オンラインで行われます。詳細はこちらから。
+              </SubHeaderPink>
             </div>
 
             <a href="https://discord.com/download">
-                <Image src="/images/banner.png" width="328" height="76" />
+              <Image src="/images/banner.png" width="328" height="76" />
             </a>
 
-
-
             <div className="pb-6">
-              <SubHeaderGreen>Q. 学校生活について質問があります（入学準備など）</SubHeaderGreen>
-              <SubHeaderPink>A. 以下のメディアサイトに情報がまとめてありますので一度ご確認ください。情報が無い場合はDiscordなどで聞いてみてください。また、緊急でない場合はオンライン新歓での話の種なんかにもしてみてください。</SubHeaderPink>
+              <SubHeaderGreen>
+                Q. 学校生活について質問があります（入学準備など）
+              </SubHeaderGreen>
+              <SubHeaderPink>
+                A.
+                以下のメディアサイトに情報がまとめてありますので一度ご確認ください。情報が無い場合はDiscordなどで聞いてみてください。また、緊急でない場合はオンライン新歓での話の種なんかにもしてみてください。
+              </SubHeaderPink>
             </div>
 
             <div className="pb-6">
-              <SubHeaderGreen>Q. サークルの活動内容などについて質問があります。</SubHeaderGreen>
-              <SubHeaderPink>A. 各サークルのページに記載されている連絡先にお問い合わせください。話を聞いただけでロックオンしてくるような、冬眠前の熊みたいな団体は（多分）いないので気軽にどうぞ。</SubHeaderPink>
+              <SubHeaderGreen>
+                Q. サークルの活動内容などについて質問があります。
+              </SubHeaderGreen>
+              <SubHeaderPink>
+                A.
+                各サークルのページに記載されている連絡先にお問い合わせください。話を聞いただけでロックオンしてくるような、冬眠前の熊みたいな団体は（多分）いないので気軽にどうぞ。
+              </SubHeaderPink>
             </div>
 
             <a href="https://discord.com/download">
-                <Image src="/images/uuyell-banner.jpg" width="328" height="76" />
+              <Image src="/images/uuyell-banner.jpg" width="328" height="76" />
             </a>
 
-
             <div className="pb-6">
-              <SubHeaderGreen>Q. このサイトに関して質問があります。</SubHeaderGreen>
-              <SubHeaderPink>A. ページ下部の”お問い合わせ”、またはこちらからどうぞ。</SubHeaderPink>
+              <SubHeaderGreen>
+                Q. このサイトに関して質問があります。
+              </SubHeaderGreen>
+              <SubHeaderPink>
+                A. ページ下部の”お問い合わせ”、またはこちらからどうぞ。
+              </SubHeaderPink>
             </div>
 
             <div className="pb-8 pt-4">
@@ -80,42 +97,61 @@ const Page: NextPage<Props> = () => {
             </div>
 
             <div className="pb-6">
-              <SubHeaderGreen>Q. おすすめのサークルはどこですか？</SubHeaderGreen>
-              <SubHeaderPink>A. U-labというサークルが良いと聞いたことがあります。なんでも、初心者にも優しくスキルを教えてくれる先輩と、上級者でも飽きないくらいの活動が目白押しだそうです。実力問わず、なんとなくおもしろいことをしてみたいなぁと思っている人を募集中らしいですよ。
-詳細はこちらからどうぞ。</SubHeaderPink>
+              <SubHeaderGreen>
+                Q. おすすめのサークルはどこですか？
+              </SubHeaderGreen>
+              <SubHeaderPink>
+                A.
+                U-labというサークルが良いと聞いたことがあります。なんでも、初心者にも優しくスキルを教えてくれる先輩と、上級者でも飽きないくらいの活動が目白押しだそうです。実力問わず、なんとなくおもしろいことをしてみたいなぁと思っている人を募集中らしいですよ。
+                詳細はこちらからどうぞ。
+              </SubHeaderPink>
             </div>
 
             <div className="pb-6">
-              <SubHeaderGreen>Q. 2年生以上向け）自分達のサークルが見当たりません/このサイトへのサークルの登録方法を教えてください。</SubHeaderGreen>
-              <SubHeaderPink>A. 新歓実行委員に直接お問い合わせください。</SubHeaderPink>
+              <SubHeaderGreen>
+                Q.
+                2年生以上向け）自分達のサークルが見当たりません/このサイトへのサークルの登録方法を教えてください。
+              </SubHeaderGreen>
+              <SubHeaderPink>
+                A. 新歓実行委員に直接お問い合わせください。
+              </SubHeaderPink>
             </div>
 
             <div className="pb-6">
-              <SubHeaderGreen>Q. （外部向け）このサイトに関する話が聞きたい/取材がしたいです。</SubHeaderGreen>
-              <SubHeaderPink>A. 運営団体に関することはページ下部の”運営団体”からご確認いただけます。</SubHeaderPink>
+              <SubHeaderGreen>
+                Q. （外部向け）このサイトに関する話が聞きたい/取材がしたいです。
+              </SubHeaderGreen>
+              <SubHeaderPink>
+                A.
+                運営団体に関することはページ下部の”運営団体”からご確認いただけます。
+              </SubHeaderPink>
 
               <div className="pb-8 pt-4">
-              <GreenLgButton href="/">運営団体へ</GreenLgButton>
-            　</div>
+                <GreenLgButton href="/">運営団体へ</GreenLgButton>　
+              </div>
 
-              <SubHeaderPink>A. また、このサイトに関することはページ下部の”お問い合わせ”かこちらからご連絡ください。</SubHeaderPink>
+              <SubHeaderPink>
+                A.
+                また、このサイトに関することはページ下部の”お問い合わせ”かこちらからご連絡ください。
+              </SubHeaderPink>
 
               <div className="pb-8 pt-4">
-              <GreenLgButton href="/">お問い合わせへ</GreenLgButton>
+                <GreenLgButton href="/">お問い合わせへ</GreenLgButton>
+              </div>
             </div>
-            </div>
-
-            
 
             <div className="pb-6">
-              <SubHeaderGreen>Q. （外部向け）このサイトに広告を掲載したいのですが。</SubHeaderGreen>
-              <SubHeaderPink>A. ページ下部の”お問い合わせ”、またはこちらからご連絡ください。</SubHeaderPink>
+              <SubHeaderGreen>
+                Q. （外部向け）このサイトに広告を掲載したいのですが。
+              </SubHeaderGreen>
+              <SubHeaderPink>
+                A. ページ下部の”お問い合わせ”、またはこちらからご連絡ください。
+              </SubHeaderPink>
             </div>
 
             <div className="pb-8 pt-4">
               <GreenLgButton href="/">お問い合わせへ</GreenLgButton>
             </div>
-
           </BaseContainer>
         </div>
 
