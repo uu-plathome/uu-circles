84ead0aa8148e22b1dce1813892dbdd010fc8fa5
HinataKikuchi <haiao4041@gmail.com>
2021-03-11 23:40:09 +0900

    feat could set environment value

--- batch/newjoy_notification/settings.py
+++ batch/newjoy_notification/settings.py
@@ -4,9 +4,8 @@ from dotenv import load_dotenv
 #See
 #https://qiita.com/harukikaneko/items/b004048f8d1eca44cba9
 
-load_dotenv(verbos=True)
-
 dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv(verbose=True, dotenv_path=dotenv_path)
 
 TOKEN = os.environ.get("TOKEN")
+print(TOKEN)
