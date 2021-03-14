cc407f7f11f1dd773f8b503bc1274ff4271558cf
H37kouya <h37kouya@gmail.com>
2021-03-14 09:09:40 +0900

    feat "blocking"

--- main/pages/circle/[slug]/newjoy.tsx
+++ main/pages/circle/[slug]/newjoy.tsx
@@ -254,7 +254,7 @@ export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
 
 export const getStaticPaths: GetStaticPaths = async () => ({
   paths: [],
-  fallback: true,
+  fallback: 'blocking',
 })
 
 export default Page
