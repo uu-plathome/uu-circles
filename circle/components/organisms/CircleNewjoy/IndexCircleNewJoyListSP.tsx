3c35d002384ae591f93a5f3cbf0a79ce699b7e33
H37kouya <h37kouya@gmail.com>
2021-03-15 20:26:09 +0900

    feat 新歓一覧の作成

---
+++ circle/components/organisms/CircleNewjoy/IndexCircleNewJoyListSP.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react'
+import { CircleNewJoy } from '@/lib/types/model/CircleNewJoy'
+import { CircleNewJoyListItemSP } from '@/components/organisms/CircleNewjoy/CircleNewJoyListItemSP'
+
+type Props = {
+  circleId: number
+  circleNewJoys: CircleNewJoy[]
+}
+const IndexCircleNewJoyListSP: FC<Props> = ({ circleId, circleNewJoys }) => {
+  return (
+    <div className="bg-gray-100">
+      <div className="md:flex justify-center flex-wrap">
+        {circleNewJoys &&
+          circleNewJoys.map((circleNewJoy, idx) => {
+            let className = idx % 3 === 0 ? 'place-self-end' : ''
+            className = idx % 3 === 1 ? 'place-self-center' : className
+            className = idx % 3 === 2 ? 'place-self-start' : className
+
+            return (
+              <div key={circleNewJoy.id} className={`md:px-4 ${className}`}>
+                <CircleNewJoyListItemSP
+                  circleId={circleId}
+                  circleNewJoy={circleNewJoy}
+                />
+              </div>
+            )
+          })}
+      </div>
+    </div>
+  )
+}
+
+export { IndexCircleNewJoyListSP }
--- circle/lib/utils/Date.ts
+++ circle/lib/utils/Date.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs'
+
 export const isDate = (strDate: string) => {
   // 空文字は無視
   if (strDate === '') {
@@ -58,4 +60,88 @@ export const isDatetime = (strDatetime: string) => {
   }
 
   return true
+}
+
+//曜日を返す
+export const getDOW = (originStartDate: Date | string) => {
+  if (!originStartDate) {
+    return '未定'
+  }
+  const date = dayjs(originStartDate)
+  const DOWList = ['日', '月', '火', '水', '木', '金', '土'] //dayjsは日曜始まり
+  const DOW = DOWList[date.day()]
+  return DOW
+}
+
+// import getDOW as gtag from '@/lib/utils/GetDOW'
+//月を返す
+export const getMonth = (originStartDate: Date | string) => {
+  if (!originStartDate) {
+    return '未定'
+  }
+  const date = dayjs(originStartDate)
+
+  return date.format('M')
+}
+
+//日にちを返す
+export const getDay = (originStartDate: Date | string) => {
+  if (!originStartDate) {
+    return '未定'
+  }
+  const date = dayjs(originStartDate)
+
+  return date.format('D')
+}
+
+//2021/2/4などを返す
+export const getDate = (originStartDate: Date | string) => {
+  if (!originStartDate) {
+    return '未定'
+  }
+  const date = dayjs(originStartDate)
+
+  return date.format('YYYY/MM/DD')
+}
+
+//18:00-21:00などを返す
+export const getTime = (
+  originStartDate: Date | string,
+  originEndDate: Date | string
+) => {
+  if (originStartDate && originEndDate) {
+    const startDate = dayjs(originStartDate)
+    const endDate = dayjs(originEndDate)
+
+    return `${startDate.format('HH:mm')}-${endDate.format('HH:mm')}`
+  }
+
+  if (originStartDate) {
+    const startDate = dayjs(originStartDate)
+
+    return `${startDate.format('HH:mm')}-`
+  }
+
+  if (originEndDate) {
+    const endDate = dayjs(originEndDate)
+
+    return `-${endDate.format('HH:mm')}`
+  }
+
+  return '未定'
+}
+
+// 2021年2月26日 18:00-21:00などを返す
+export const getFullJPDate = (
+  originStartDate: Date | string,
+  originEndDate: Date | string
+) => {
+  if (!originStartDate) {
+    return '未定'
+  }
+  const date = dayjs(originStartDate)
+
+  const fullDate = date.format('YYYY年M月D日')
+
+  return fullDate + '　' + getTime(originStartDate, originEndDate)
 }
