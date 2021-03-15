08cedf61ef76c60e33a12b517f5ffdf437d65353
H37kouya <h37kouya@gmail.com>
2021-03-15 21:07:26 +0900

    feat logの追加

--- api/app/Http/Controllers/Admin/CircleTag/GetCircleTagController.php
+++ api/app/Http/Controllers/Admin/CircleTag/GetCircleTagController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\CircleInformation;
 use App\Models\CircleTag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class GetCircleTagController extends Controller
 {
@@ -18,6 +19,8 @@ class GetCircleTagController extends Controller
      */
     public function __invoke(Request $request, int $circleId)
     {
+        Log::debug("CreateOrUpdateCircleTagController args circleId=$circleId");
+
         $circleInformation = CircleInformation::whereCircleId($circleId)->first();
         $circleTag = CircleTag::whereCircleId($circleId)->first();
 
