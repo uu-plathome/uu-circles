56a4f46e733bb78bd0f4285290db45ee27a9aeec
HinataKikuchi <haiao4041@gmail.com>
2021-03-18 18:56:13 +0900

    add README and .env.example

--- batch/newjoy_notification/settings.py
+++ batch/newjoy_notification/settings.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 #See
 #https://qiita.com/harukikaneko/items/b004048f8d1eca44cba9
 
+
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(verbose=True, dotenv_path=dotenv_path)
 
