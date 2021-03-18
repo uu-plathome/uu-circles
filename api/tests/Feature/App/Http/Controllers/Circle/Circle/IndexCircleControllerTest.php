f955288a5c77d6d23f3bd242a49b9f25d5940c1e
H37kouya <h37kouya@gmail.com>
2021-03-18 22:30:52 +0900

    test テスト追加

---
+++ api/tests/Feature/App/Http/Controllers/Circle/Circle/IndexCircleControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\Circle\Auth;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Tests\Traits\RefreshDatabaseLite;
+use Tests\TestCase;
+
+class IndexCircleControllerTest extends TestCase
+{
+    use RefreshDatabaseLite;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Log::info("IndexCircleControllerTest");
+    }
+
+    /**
+     * 各テストの前にデータベースをシードする必要があるかどうかを示す
+     *
+     * @var bool
+     */
+    protected $seed = true;
+
+    public function testRequest()
+    {
+        Log::info("testRequest");
+
+        // GIVEN
+        /** @var \App\Models\User $user */
+        $user = User::whereActive(true)
+            ->hasByNonDependentSubquery('circleUser')
+            ->inRandomOrder()
+            ->first();
+        $this->assertNotNull($user);
+
+        // WHEN
+        $response = $this->get('/circle/api/circle', [
+            'Authorization' => "Bearer $user->api_token",
+        ]);
+
+        // THEN
+        $response->assertOk();
+    }
+}
---
+++ api/tests/Feature/App/Http/Controllers/Circle/Circle/ShowCircleControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\Circle\Auth;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Tests\Traits\RefreshDatabaseLite;
+use Tests\TestCase;
+
+class ShowCircleControllerTest extends TestCase
+{
+    use RefreshDatabaseLite;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Log::info("ShowCircleControllerTest");
+    }
+
+    /**
+     * 各テストの前にデータベースをシードする必要があるかどうかを示す
+     *
+     * @var bool
+     */
+    protected $seed = true;
+
+    public function testRequest()
+    {
+        Log::info("testRequest");
+
+        // GIVEN
+        /** @var \App\Models\User $user */
+        $user = User::whereActive(true)
+            ->hasByNonDependentSubquery('circleUser')
+            ->inRandomOrder()
+            ->first();
+        $this->assertNotNull($user);
+
+        // WHEN
+        $response = $this->get("/circle/api/circle/{$user->circleUser->circleId}", [
+            'Authorization' => "Bearer $user->api_token",
+        ]);
+
+        // THEN
+        $response->assertOk();
+    }
+}
