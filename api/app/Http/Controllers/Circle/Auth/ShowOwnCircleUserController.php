45595400b32ea86177fe131049f9c8188f3abe9e
H37kouya <h37kouya@gmail.com>
2021-03-18 22:26:50 +0900

    test Circleの認証のテスト追加

---
+++ api/app/Http/Controllers/Circle/Auth/ShowOwnCircleUserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Circle\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Support\Arr;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class ShowOwnCircleUserController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function __invoke(Request $request)
+    {
+        Log::debug("ShowOwnCircleUserController args none");
+
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+        if (!$user->circleUser) {
+            Log::info("[INFO] ShowOwnCircleUserController code=400, userId=$user->id");
+            throw new AuthorizationException();
+        }
+
+        return Arr::camel_keys($request->user()->toArray());
+    }
+}
---
+++ api/tests/Feature/App/Http/Controllers/Circle/Auth/LoginCircleControllerTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\Circle\Auth;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Tests\Traits\RefreshDatabaseLite;
+use Tests\TestCase;
+
+class LoginCircleControllerTest extends TestCase
+{
+    use RefreshDatabaseLite;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Log::info("LoginCircleControllerTest");
+        Cache::clear();
+    }
+
+    /**
+     * 各テストの前にデータベースをシードする必要があるかどうかを示す
+     *
+     * @var bool
+     */
+    protected $seed = true;
+
+    public function testSuccess_usernameでログインできる()
+    {
+        Log::info("testSuccess_usernameでログインできる");
+
+        // GIVEN
+        /** @var \App\Models\User $user */
+        $user = User::whereActive(true)
+            ->hasByNonDependentSubquery('circleUser')
+            ->inRandomOrder()
+            ->first();
+        $this->assertNotNull($user);
+
+        // WHEN
+        $response = $this->post('/circle/api/login', [
+            'usernameOrEmail' => $user->username,
+            'password'        => 'Test1234@@',
+        ]);
+
+        // THEN
+        $response->assertOk();
+        $this->assertArrayHasKey('username', $response);
+        $this->assertArrayHasKey('email', $response);
+        $this->assertSame($user->username, $response['username']);
+        $this->assertSame($user->email, $response['email']);
+    }
+
+    public function testSuccess_emailでログインできる()
+    {
+        Log::info("testSuccess_emailでログインできる");
+
+        // GIVEN
+        /** @var \App\Models\User $user */
+        $user = User::whereActive(true)
+            ->hasByNonDependentSubquery('circleUser')
+            ->inRandomOrder()
+            ->first();
+        $this->assertNotNull($user);
+
+        // WHEN
+        $response = $this->post('/circle/api/login', [
+            'usernameOrEmail' => $user->email,
+            'password'        => 'Test1234@@',
+        ]);
+
+        // THEN
+        $response->assertOk();
+        $this->assertArrayHasKey('username', $response);
+        $this->assertArrayHasKey('email', $response);
+        $this->assertSame($user->username, $response['username']);
+        $this->assertSame($user->email, $response['email']);
+    }
+
+    public function testRequest_ログイン中のとき()
+    {
+        Log::info("testRequest");
+
+        // GIVEN
+        /** @var \App\Models\User $user */
+        $user = User::whereActive(true)
+            ->hasByNonDependentSubquery('circleUser')
+            ->inRandomOrder()
+            ->first();
+        $this->assertNotNull($user);
+
+        // WHEN
+        $response = $this->get('/circle/api/login', [
+            'Authorization' => "Bearer $user->api_token",
+        ]);
+
+        // THEN
+        $response->assertStatus(405);
+    }
+}
