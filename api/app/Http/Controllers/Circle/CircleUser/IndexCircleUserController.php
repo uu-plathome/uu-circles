2e7637bf3139cce4efbe63f7cb177e57912a77cf
H37kouya <h37kouya@gmail.com>
2021-03-20 16:25:11 +0900

    feat 部員アカウント一覧作成

--- api/app/Http/Controllers/Circle/CircleUser/IndexCircleUserController.php
+++ api/app/Http/Controllers/Circle/CircleUser/IndexCircleUserController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Circle\CircleUser;
 
 use App\Http\Controllers\Circle\Traits\Permission;
 use App\Http\Controllers\Controller;
+use App\Models\Circle;
 use App\Models\User;
 use App\Support\Arr;
+use App\ValueObjects\CircleValueObject;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -21,6 +23,7 @@ class IndexCircleUserController extends Controller
         $user = $request->user();
         $this->permissionCircle($user, $circleId);
 
+        $circle = Circle::whereRelease(true)->findOrFail($circleId);
         $circleUsers = User::whereActive(true)
             ->hasByNonDependentSubquery('circleUsers', function ($query) use ($circleId) {
                 /** @var \App\Models\CircleUser $query */
@@ -28,6 +31,14 @@ class IndexCircleUserController extends Controller
             })->get();
 
         return Arr::camel_keys([
+            'circle' => Arr::camel_keys(
+                CircleValueObject::byEloquent(
+                    $circle,
+                    $circle->circleInformation,
+                    null
+                )->toArray()
+            ),
+
             'data' => $circleUsers->toArray(),
         ]);
     }
