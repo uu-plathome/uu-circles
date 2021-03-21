23d08b28da1e6b3b8e885891ac8b583e2a883be9
H37kouya <h37kouya@gmail.com>
2021-03-21 16:30:13 +0900

    サークルを脱退するにモーダル追加

--- circle/components/organisms/List/CircleWithdrawalList.tsx
+++ circle/components/organisms/List/CircleWithdrawalList.tsx
@@ -1,12 +1,38 @@
+import { GrayButton } from "@/components/atoms/buttons/GrayButton"
+import { RedButton } from "@/components/atoms/buttons/RedButton"
 import { Circle } from "@/lib/types/model/Circle"
-import { FC } from "react"
+import { FC, useState } from "react"
+import Modal from 'react-modal'
 
-const CircleWithdrawalListItem: FC<{
-  circle: Circle,
+const customStyles = {
+  content: {
+    padding: '20px 12px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '300px',
+    height: '200px',
+  },
+}
+
+type DeleteButtonProps = {
+  circle: Circle
   onWithdrawal(): void
-}> = ({ circle, onWithdrawal }) => {
-    return (
-      <a className="shadow hover:shadow-md cursor-pointer" onClick={() => onWithdrawal()}>
+}
+const DeleteButton: FC<DeleteButtonProps> = ({ circle, onWithdrawal }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onClickDeleteButton = () => {
+    setIsOpen(false)
+    onWithdrawal()
+  }
+
+  return (
+    <div>
+      <a className="shadow hover:shadow-md cursor-pointer" onClick={() => setIsOpen(true)}>
         <div
           className="flex justify-center items-center rounded border border-gray-200 bg-white py-6"
           style={{ width: 280 }}
@@ -14,6 +40,39 @@ const CircleWithdrawalListItem: FC<{
           <p className="font-lg font-bold">{ circle.name }</p>
         </div>
       </a>
+
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        style={customStyles}
+        contentLabel="サークルを脱退"
+      >
+        <h2 className="text-center text-lg mb-4 font-bold">
+          本当にサークルを脱退しますか？
+        </h2>
+
+        <p className="mb-8 text-center">{circle.name}</p>
+
+        <div className="flex justify-center">
+          <div className="mx-2">
+            <GrayButton onClick={() => setIsOpen(false)}>閉じる</GrayButton>
+          </div>
+          <div className="mx-2">
+            <RedButton onClick={onClickDeleteButton}>脱退</RedButton>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  )
+}
+
+
+const CircleWithdrawalListItem: FC<{
+  circle: Circle,
+  onWithdrawal(): void
+}> = ({ circle, onWithdrawal }) => {
+    return (
+      <DeleteButton circle={circle} onWithdrawal={onWithdrawal} />
     )
 }
 
