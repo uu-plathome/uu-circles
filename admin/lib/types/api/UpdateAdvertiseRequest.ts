51f983209c056a34e62cdbaa941ca69c17f483f1
Lint Action <lint-action@samuelmeuli.com>
2021-03-22 07:45:05 +0000

    Fix code style issues with Prettier

--- admin/lib/types/api/UpdateAdvertiseRequest.ts
+++ admin/lib/types/api/UpdateAdvertiseRequest.ts
@@ -2,32 +2,36 @@
  * リクエストボディ
  */
 export interface UpdateAdvertiseRequest {
-    type: 'UpdateAdvertiseRequest'
-    title: string
-    link?: string
-    mainImageUrl?: string
-    active?: boolean
-    advertiseType?: string
-    publishFrom?: any
-    publishTo?: any
+  type: 'UpdateAdvertiseRequest'
+  title: string
+  link?: string
+  mainImageUrl?: string
+  active?: boolean
+  advertiseType?: string
+  publishFrom?: any
+  publishTo?: any
 }
 
 /**
  * バリデーションエラー
  */
 export interface UpdateAdvertiseRequestValidationError {
-    type: 'UpdateAdvertiseRequestValidationError'
-    errors: {
-        title?: string[]
-        link?: string[]
-        mainImageUrl?: string[]
-        active?: string[]
-        advertiseType?: string[]
-        publishFrom?: string[]
-        publishTo?: string[]
-    }
-    message: string
+  type: 'UpdateAdvertiseRequestValidationError'
+  errors: {
+    title?: string[]
+    link?: string[]
+    mainImageUrl?: string[]
+    active?: string[]
+    advertiseType?: string[]
+    publishFrom?: string[]
+    publishTo?: string[]
+  }
+  message: string
 }
 
-export const isUpdateAdvertiseRequest = (v: any): v is UpdateAdvertiseRequest => v && v.type === 'UpdateAdvertiseRequest'
-export const isUpdateAdvertiseRequestValidationError = (v: any): v is UpdateAdvertiseRequestValidationError => v && v.type === 'UpdateAdvertiseRequestValidationError'
+export const isUpdateAdvertiseRequest = (v: any): v is UpdateAdvertiseRequest =>
+  v && v.type === 'UpdateAdvertiseRequest'
+export const isUpdateAdvertiseRequestValidationError = (
+  v: any
+): v is UpdateAdvertiseRequestValidationError =>
+  v && v.type === 'UpdateAdvertiseRequestValidationError'
