e0cc17e2a53f3db1a90c96d46bfedd8153d293c8
H37kouya <h37kouya@gmail.com>
2021-03-22 15:46:26 +0900

    add

--- admin/lib/types/api/CreateAdvertiseRequest.ts
+++ admin/lib/types/api/CreateAdvertiseRequest.ts
@@ -2,34 +2,32 @@
  * リクエストボディ
  */
 export interface CreateAdvertiseRequest {
-  type: 'CreateAdvertiseRequest'
-  title: string
-  link?: string
-  mainImageUrl?: string
-  active?: boolean
-  publishFrom?: any
-  publishTo?: any
+    type: 'CreateAdvertiseRequest'
+    title: string
+    link?: string
+    mainImageUrl?: string
+    active?: boolean
+    advertiseType?: string
+    publishFrom?: any
+    publishTo?: any
 }
 
 /**
  * バリデーションエラー
  */
 export interface CreateAdvertiseRequestValidationError {
-  type: 'CreateAdvertiseRequestValidationError'
-  errors: {
-    title?: string[]
-    link?: string[]
-    mainImageUrl?: string[]
-    active?: string[]
-    publishFrom?: string[]
-    publishTo?: string[]
-  }
-  message: string
+    type: 'CreateAdvertiseRequestValidationError'
+    errors: {
+        title?: string[]
+        link?: string[]
+        mainImageUrl?: string[]
+        active?: string[]
+        advertiseType?: string[]
+        publishFrom?: string[]
+        publishTo?: string[]
+    }
+    message: string
 }
 
-export const isCreateAdvertiseRequest = (v: any): v is CreateAdvertiseRequest =>
-  v && v.type === 'CreateAdvertiseRequest'
-export const isCreateAdvertiseRequestValidationError = (
-  v: any
-): v is CreateAdvertiseRequestValidationError =>
-  v && v.type === 'CreateAdvertiseRequestValidationError'
+export const isCreateAdvertiseRequest = (v: any): v is CreateAdvertiseRequest => v && v.type === 'CreateAdvertiseRequest'
+export const isCreateAdvertiseRequestValidationError = (v: any): v is CreateAdvertiseRequestValidationError => v && v.type === 'CreateAdvertiseRequestValidationError'
