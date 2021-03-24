7e4e19ee7fddb3f67c89cb9771abdf5b625ee37c
H37kouya <h37kouya@gmail.com>
2021-03-24 12:26:01 +0900

    feat

--- circle/components/molecules/Breadcrumbs/BaseBreadcrumbs.tsx
+++ circle/components/molecules/Breadcrumbs/BaseBreadcrumbs.tsx
@@ -2,6 +2,7 @@ import { faHome, IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 import { FC } from 'react'
+import { BaseContainer } from '../Container/BaseContainer'
 
 export type BaseBreadcrumbItem = {
   text: string
@@ -30,50 +31,54 @@ const BaseBreadcrumbs: FC<Props> = ({ items }) => {
   console.log('BaseBreadcrumbs', breadcrumbsItems)
 
   return (
-    <div>
-      <div className="px-4 py-2">
-        {breadcrumbsItems &&
-          breadcrumbsItems.map((item, idx) => {
-            return (
-              <div
-                key={`BaseBreadcrumbs-${idx}-${item.href}`}
-                className="inline"
-              >
-                {!item.href || !item.active ? (
-                  <Link href={item.href} as={item.as}>
-                    <a>
+    <div className="">
+      <BaseContainer>
+        <div className="border-b border-gray-300 px-4 py-2">
+          {breadcrumbsItems &&
+            breadcrumbsItems.map((item, idx) => {
+              return (
+                <div
+                  key={`BaseBreadcrumbs-${idx}-${item.href}`}
+                  className="inline"
+                >
+                  {!item.href || !item.active ? (
+                    <Link href={item.href} as={item.as}>
+                      <a>
+                        {item.icon ? (
+                          <FontAwesomeIcon icon={item.icon} className="mr-1" />
+                        ) : (
+                          ''
+                        )}
+                        <span className="text-sm text-blue-600">
+                          {item.text}
+                        </span>
+                        {breadcrumbsItems.length - 1 !== idx ? (
+                          <span> / </span>
+                        ) : (
+                          ''
+                        )}
+                      </a>
+                    </Link>
+                  ) : (
+                    <span>
                       {item.icon ? (
                         <FontAwesomeIcon icon={item.icon} className="mr-1" />
                       ) : (
                         ''
                       )}
-                      <span className="text-sm text-blue-600">{item.text}</span>
+                      <span className="text-sm">{item.text}</span>
                       {breadcrumbsItems.length - 1 !== idx ? (
                         <span> / </span>
                       ) : (
                         ''
                       )}
-                    </a>
-                  </Link>
-                ) : (
-                  <span>
-                    {item.icon ? (
-                      <FontAwesomeIcon icon={item.icon} className="mr-1" />
-                    ) : (
-                      ''
-                    )}
-                    <span className="text-sm">{item.text}</span>
-                    {breadcrumbsItems.length - 1 !== idx ? (
-                      <span> / </span>
-                    ) : (
-                      ''
-                    )}
-                  </span>
-                )}
-              </div>
-            )
-          })}
-      </div>
+                    </span>
+                  )}
+                </div>
+              )
+            })}
+        </div>
+      </BaseContainer>
     </div>
   )
 }
