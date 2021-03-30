d747291e7ea9b4b9c2487638b34c10ec320c67d3
H37kouya <h37kouya@gmail.com>
2021-03-30 20:39:54 +0900

    fix

--- admin/pages/_app.tsx
+++ admin/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
