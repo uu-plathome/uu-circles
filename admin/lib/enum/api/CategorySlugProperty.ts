142b3ad6009fe93efacb44b968ece7ad5e9fecfb
Lint Action <lint-action@samuelmeuli.com>
2021-04-01 05:20:45 +0000

    Fix code style issues with Prettier

--- admin/lib/enum/api/CategorySlugProperty.ts
+++ admin/lib/enum/api/CategorySlugProperty.ts
@@ -1,4 +1,3 @@
-
 export const CategorySlugProperty = {
   official_organization: 'official_organization',
 
@@ -6,20 +5,27 @@ export const CategorySlugProperty = {
 
   student_group: 'student_group',
 
-  club: 'club'
+  club: 'club',
 } as const
 
 export type CategorySlugPropertyKey = keyof typeof CategorySlugProperty
 export type CategorySlugProperty = typeof CategorySlugProperty[keyof typeof CategorySlugProperty]
 
+export const getAllCategorySlugProperty = (): CategorySlugProperty[] =>
+  Object.values(CategorySlugProperty)
 
-export const getAllCategorySlugProperty = (): CategorySlugProperty[] => Object.values(CategorySlugProperty)
+export const getAllCategorySlugPropertyKey = (): CategorySlugPropertyKey[] =>
+  Object.keys(CategorySlugProperty) as CategorySlugPropertyKey[]
 
-export const getAllCategorySlugPropertyKey = (): CategorySlugPropertyKey[] => Object.keys(CategorySlugProperty) as CategorySlugPropertyKey[]
+export const isCategorySlugProperty = (s: any): s is CategorySlugProperty =>
+  Object.values(CategorySlugProperty).includes(s)
 
-export const isCategorySlugProperty = (s: any): s is CategorySlugProperty => Object.values(CategorySlugProperty).includes(s)
-
-export const isOfficialOrganization = (v: any): v is 'official_organization' => v === CategorySlugProperty.official_organization
-export const isUnofficialOrganization = (v: any): v is 'unofficial_organization' => v === CategorySlugProperty.unofficial_organization
-export const isStudentGroup = (v: any): v is 'student_group' => v === CategorySlugProperty.student_group
+export const isOfficialOrganization = (v: any): v is 'official_organization' =>
+  v === CategorySlugProperty.official_organization
+export const isUnofficialOrganization = (
+  v: any
+): v is 'unofficial_organization' =>
+  v === CategorySlugProperty.unofficial_organization
+export const isStudentGroup = (v: any): v is 'student_group' =>
+  v === CategorySlugProperty.student_group
 export const isClub = (v: any): v is 'club' => v === CategorySlugProperty.club
