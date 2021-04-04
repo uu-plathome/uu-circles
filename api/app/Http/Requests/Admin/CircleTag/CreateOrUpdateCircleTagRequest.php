77463b60985c49544168bc78a9d1a184ab5353c8
H37kouya <h37kouya@gmail.com>
2021-04-04 22:19:59 +0900

    refactor circleTagのリファクタリング

--- api/app/Http/Requests/Admin/CircleTag/CreateOrUpdateCircleTagRequest.php
+++ api/app/Http/Requests/Admin/CircleTag/CreateOrUpdateCircleTagRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests\Admin\CircleTag;
 use App\Entity\CircleTagEntity;
 use App\Enum\CircleTagModel;
 use App\Support\Arr;
+use App\Usecases\Admin\CircleTag\Params\CreateOrUpdateCircleTagUsecaseParam;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -31,28 +32,53 @@ class CreateOrUpdateCircleTagRequest extends FormRequest
             'circle_tag'   => 'nullable|array',
             'circle_tag.*' => [
                 'string',
-                Rule::in([
-                    CircleTagModel::SPORT,
-                    CircleTagModel::MUSIC,
-                    CircleTagModel::CULTURE,
-                    CircleTagModel::NATURE,
-                    CircleTagModel::VOLUNTEER,
-                    CircleTagModel::INTERNATIONAL,
-                    CircleTagModel::INCARE,
-                    CircleTagModel::LOOSE,
-                    CircleTagModel::COMMUNITY,
-                    CircleTagModel::PROGRAMMING,
-                    CircleTagModel::URGENT_RECRUITMENT,
-                    CircleTagModel::MYSTERY,
-                ]),
+                Rule::in(self::canSelectedTags()),
             ],
         ]);
     }
 
-    public function makeCircleTagEntity(): CircleTagEntity
+    /**
+     * 変更可能なタグ
+     *
+     * @return array
+     */
+    public static function canSelectedTags(): array
+    {
+        return [
+            CircleTagModel::SPORT,
+            CircleTagModel::MUSIC,
+            CircleTagModel::CULTURE,
+            CircleTagModel::NATURE,
+            CircleTagModel::VOLUNTEER,
+            CircleTagModel::INTERNATIONAL,
+            CircleTagModel::INCARE,
+            CircleTagModel::LOOSE,
+            CircleTagModel::COMMUNITY,
+            CircleTagModel::PROGRAMMING,
+            CircleTagModel::URGENT_RECRUITMENT,
+            CircleTagModel::MYSTERY,
+        ];
+    }
+
+    public function makeCreateOrUpdateCircleTagUsecaseParam(): CreateOrUpdateCircleTagUsecaseParam
     {
         $request = Arr::snake_keys($this->validated());
 
-        return CircleTagEntity::of($request['circle_tag'] ? $request['circle_tag'] : []);
+        $param = new CreateOrUpdateCircleTagUsecaseParam();
+        $param->circle_id = $this->circleId;
+        $param->sport = in_array(CircleTagModel::SPORT, $request, true);
+        $param->music = in_array(CircleTagModel::MUSIC, $request, true);
+        $param->culture = in_array(CircleTagModel::CULTURE, $request, true);
+        $param->volunteer = in_array(CircleTagModel::VOLUNTEER, $request, true);
+        $param->nature = in_array(CircleTagModel::NATURE, $request, true);
+        $param->international = in_array(CircleTagModel::INTERNATIONAL, $request, true);
+        $param->incare = in_array(CircleTagModel::INCARE, $request, true);
+        $param->loose = in_array(CircleTagModel::LOOSE, $request, true);
+        $param->community = in_array(CircleTagModel::COMMUNITY, $request, true);
+        $param->programming = in_array(CircleTagModel::PROGRAMMING, $request, true);
+        $param->urgent_recruitment = in_array(CircleTagModel::URGENT_RECRUITMENT, $request, true);
+        $param->mystery = in_array(CircleTagModel::MYSTERY, $request, true);
+
+        return $param;
     }
 }
--- api/tests/Feature/App/Http/Controllers/Admin/Advertise/ShowAdvertiseControllerTest.php
+++ api/tests/Feature/App/Http/Controllers/Admin/Advertise/ShowAdvertiseControllerTest.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Str;
 use Tests\Traits\RefreshDatabaseLite;
 use Tests\TestCase;
 
-class ShowCircleControllerTest extends TestCase
+class ShowAdvertiseControllerTest extends TestCase
 {
     use RefreshDatabaseLite;
 
@@ -33,15 +33,22 @@ class ShowCircleControllerTest extends TestCase
         Log::info("testRequest");
 
         // GIVEN
+        Advertise::query()->delete();
+        $this->assertCount(0, Advertise::all());
+
+        $advertises = factory(Advertise::class, 1)->create();
+        $this->assertTrue($advertises instanceof Collection);
         /** @var Advertise $advertise */
-        $circle = Circle::inRandomOrder()->first();
+        $advertise = $advertises[0];
+        $advertise = Advertise::find($advertise->id);
         Log::info('[GIVEN]', [
-            'circle' => $circle,
-            'id'     => $circle->id,
+            'advertises' => $advertises,
+            'id'         => $advertise->id,
         ]);
 
+
         // WHEN
-        $response = $this->get("/admin/api/circle/$circle->id", [
+        $response = $this->get("/admin/api/advertise/$advertise->id", [
             'Authorization' => 'Bearer test1234',
         ]);
 
@@ -49,5 +56,42 @@ class ShowCircleControllerTest extends TestCase
         $response->assertOk();
         $this->assertArrayHasKey('data', $response);
         $this->assertIsArray($response['data']);
+
+        $data = $response['data'];
+        $this->assertArrayHasKey(Str::camel(AdvertiseProperty::title), $data);
+        $this->assertSame(
+            $advertise->title,
+            $data[Str::camel(AdvertiseProperty::title)]
+        );
+        $this->assertArrayHasKey(Str::camel(AdvertiseProperty::link), $data);
+        $this->assertSame(
+            $advertise->link,
+            $data[Str::camel(AdvertiseProperty::link)]
+        );
+        $this->assertArrayHasKey(Str::camel(AdvertiseProperty::active), $data);
+        $this->assertSame(
+            $advertise->active,
+            $data[Str::camel(AdvertiseProperty::active)]
+        );
+        $this->assertArrayHasKey(Str::camel(AdvertiseProperty::advertise_type), $data);
+        $this->assertSame(
+            $advertise->advertise_type,
+            $data[Str::camel(AdvertiseProperty::advertise_type)]
+        );
+        $this->assertArrayHasKey(Str::camel(AdvertiseProperty::main_image_url), $data);
+        $this->assertSame(
+            $advertise->main_image_url,
+            $data[Str::camel(AdvertiseProperty::main_image_url)]
+        );
+        $this->assertArrayHasKey(Str::camel(AdvertiseProperty::publish_to), $data);
+        $this->assertSame(
+            $advertise->publish_to,
+            $data[Str::camel(AdvertiseProperty::publish_to)]
+        );
+        $this->assertArrayHasKey(Str::camel(AdvertiseProperty::publish_from), $data);
+        $this->assertSame(
+            $advertise->publish_from,
+            $data[Str::camel(AdvertiseProperty::publish_from)]
+        );
     }
 }
