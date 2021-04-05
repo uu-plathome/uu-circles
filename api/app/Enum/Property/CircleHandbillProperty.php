3606ed8d5489e400c2f4ea37d568e705bfc82847
H37kouya <h37kouya@gmail.com>
2021-04-05 13:50:37 +0900

    feat Pに変更

---
+++ api/app/Enum/Property/CircleHandbillProperty.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enum\Property;
+
+class CircleHandbillProperty
+{
+    const id = 'id';
+    const created_at = 'created_at';
+    const updated_at = 'updated_at';
+
+    const circle_id = 'circle_id';
+    const image_url = 'image_url';
+    const year = 'year';
+}
---
+++ api/app/Enum/Property/CircleInvitationProperty.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enum\Property;
+
+class CircleInvitationProperty
+{
+    const id = 'id';
+    const created_at = 'created_at';
+    const updated_at = 'updated_at';
+
+    const circle_id = 'circle_id';
+    const token = 'token';
+    const active = 'active';
+    const created_user_id = 'created_user_id';
+}
