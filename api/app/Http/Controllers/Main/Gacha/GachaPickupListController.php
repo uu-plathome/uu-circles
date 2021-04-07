e0d3c9df5cb45726cc8d1fa52489f28312ade2ba
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-04-07 16:00:36 +0900

    feat ランダムに3つのサークルを取得する

--- api/app/Http/Controllers/Main/Gacha/GachaPickupListController.php
+++ api/app/Http/Controllers/Main/Gacha/GachaPickupListController.php
@@ -6,16 +6,27 @@ namespace App\Http\Controllers\Main\Gacha;
 
 use App\Http\Controllers\Controller;
 use App\Support\Arr;
+use App\Usecases\Main\Gacha\GetGachaPickupListUsecase;
 use Illuminate\Support\Facades\Log;
 
 class GachaPickupListController  extends Controller
 {
+    private GetGachaPickupListUsecase $getGachaPickupListUsecase;
+
+    public function  __construct(GetGachaPickupListUsecase $getGachaPickupListUsecase)
+    {
+        $this->getGachaPickupListUsecase = $getGachaPickupListUsecase;
+    }
+
     public function __invoke()
     {
         Log::debug('GachaPickupListController args none');
+
+        $pickupList = $this->getGachaPickupListUsecase->invoke();
+
         return Arr::camel_keys([
-            "pickupCircle"=> [],
-            "pickupDate"=> "2021-4-7",
+            "pickupCircle"=> $pickupList->toArrayPickupCircles(),
+            "pickupDate"=> $pickupList->pickupDate,
         ]);
     }
 }
