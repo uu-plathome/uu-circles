613e9d99185f73df35d380870934cf55be55b430
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-04-07 17:30:53 +0900

    feat modelのfillableとEnum?

--- api/app/Models/CircleGachaResult.php
+++ api/app/Models/CircleGachaResult.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Enum\Property\CircleGachaResultProperty as P;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CircleGachaResult extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        P::gacha_hash,
+        P::identifier_hash,
+        P::result_circle_ids,
+        P::pickup_circle_ids,
+    ];
 }
--- api/app/Models/Identifier.php
+++ api/app/Models/Identifier.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Enum\Property\IdentifierProperty as P;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Identifier extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        P::identifier_hash,
+    ];
 }
