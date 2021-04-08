370a7e45553af0e81502ae340a84c14e5bad8a46
H37kouya <h37kouya@gmail.com>
2021-04-08 21:56:34 +0900

    feat 週の活動頻度の追加

--- main/pages/statistics.tsx
+++ main/pages/statistics.tsx
@@ -1,3 +1,4 @@
+import colors from '@/colors'
 import { BaseFooter } from '@/components/layouts/BaseFooter'
 import { BaseHead } from '@/components/layouts/BaseHead'
 import { BaseLayout } from '@/components/layouts/BaseLayout'
@@ -6,6 +7,7 @@ import { getStatistics } from '@/infra/api/statistics'
 import { Statistics } from '@/lib/types/model/Statistics'
 import { GetStaticProps, NextPage } from 'next'
 import { FC } from 'react'
+import { Doughnut } from 'react-chartjs-2'
 
 const SubHeader: FC = ({ children }) => {
   return <h2 className="text-xl font-bold text-center mb-4">{children}</h2>
@@ -44,6 +46,52 @@ const Page: NextPage<Props> = ({ statistics }) => {
                 </b>
                 円です！
               </p>
+            </div>
+
+            <div className="pb-16 px-4">
+              <SubHeader>週の活動頻度</SubHeader>
+              <Doughnut
+                width={300}
+                data={{
+                  labels: [
+                    '週0',
+                    '週1',
+                    '週2',
+                    '週3',
+                    '週4',
+                    '週5',
+                    '週6',
+                    '週7',
+                  ],
+                  datasets: [
+                    {
+                      label: 'My First Dataset',
+                      data: [
+                        statistics.activityFrequency.zero,
+                        statistics.activityFrequency.one,
+                        statistics.activityFrequency.two,
+                        statistics.activityFrequency.three,
+                        statistics.activityFrequency.four,
+                        statistics.activityFrequency.five,
+                        statistics.activityFrequency.six,
+                        statistics.activityFrequency.seven,
+                      ],
+                      backgroundColor: [
+                        colors.white,
+                        colors.gray[300],
+                        colors.gray[600],
+                        colors.blue[800],
+                        colors.green[700],
+                        colors.orange[400],
+                        colors.red[400],
+                        colors.red[600],
+                      ],
+                      hoverOffset: 4,
+                    },
+                  ],
+                }}
+                options={{ maintainAspectRatio: false }}
+              />
             </div>
           </BaseContainer>
         </div>
