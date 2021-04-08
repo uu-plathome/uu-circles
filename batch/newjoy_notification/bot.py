61fe665428c22cc634bfac08f6b0da2bd1e3a42b
HinataKikuchi <haiao4041@gmail.com>
2021-04-08 22:30:32 +0900

    remove no needed codes

--- batch/newjoy_notification/bot.py
+++ batch/newjoy_notification/bot.py
@@ -5,7 +5,6 @@ from datetime import datetime, timedelta, timezone
 import requests
 import json
 import io
-import itertools
 import create_text
 from domain import todayCircleNewJoy as domain
 
@@ -22,41 +21,56 @@ CHANNEL_ID = int(settings.TEST_CHANNEL_ID)
 payload = {'key': 'value'}
 r = requests.get(API_URL, params=payload).json()
 
-# ###TEST_API_INFORMATION###
-# # r = requests.get(TEST_API_URL, params=payload).json()
+##TEST_API_INFORMATION###
+# r = requests.get(TEST_API_URL, params=payload).json()
 
-# ###MAKE_BOT###
-# # 接続に必要なオブジェクトを生成
-# client = discord.Client()
+###MAKE_BOT###
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
-# ###SET_LOOP###
-# #ループ処理
-# @client.event
-# async def on_ready():
-#     channel = client.get_channel(CHANNEL_ID)
-#     #アナウンス
-#     if (len(r['todayCircleNewJoys']) == 0):
-#         await channel.send('***:crescent_moon:今日の新歓はありません***')
-#         exit()
-#     else:
-#         now = create_text.get_now()
-#         await channel.send('***☀️今日の新歓 '+now+'***')
-#         message = create_text.make_text(r)
-#         for text in message:
-#             await channel.send(text)
-#         exit()
 
-# client.run(TOKEN)
+
+todayCircleNewJoys = map(lambda newjoy: domain.TodayCircleNewJoy(newjoy), r['todayCircleNewJoys'])
+###SET_LOOP###
+#ループ処理
+@client.event
+async def on_ready():
+    channel = client.get_channel(CHANNEL_ID)
+    #アナウンス
+    if (len(r['todayCircleNewJoys']) == 0):
+        await channel.send('***:crescent_moon:今日の新歓はありません***')
+        exit()
+    else:
+        now = create_text.get_now()
+        await channel.send('***☀️今日の新歓 '+now+'***')
+        for idx, newjoy in enumerate(todayCircleNewJoys):
+            message = newjoy.make_text(idx)
+        #     message = domain.TodayCircleNewJoy(newjoy).make_text(idx)
+            await channel.send(message)
+        exit()
+
+client.run(TOKEN)
 
 ###TEST_CODES###
-print(type(r['todayCircleNewJoys'][0]))
-print(r['todayCircleNewJoys'][0])
+# print(type(r['todayCircleNewJoys'][0]))
+# print(r['todayCircleNewJoys'][0])
 # A = CircleNewJoy.CircleNewJoy(r['todayCircleNewJoys'][0]['CircleNewJoy'])
 
-A = domain.TodayCircleNewJoy(r['todayCircleNewJoys'][0])
-# print(A.slug)
-# print(A.circleNewJoy.format_startDay())
-print(A.make_text(1))
+# A = domain.TodayCircleNewJoy(r['todayCircleNewJoys'][0])
+# # print(A.slug)
+# # print(A.circleNewJoy.format_startDay())
 
-# print(type(A))
+# # print(type(A))
 
+# for i, test in enumerate(r['todayCircleNewJoys']):
+#     print(i, test)
+
+newjoys = []
+time_arr = []
+room_arr = []
+room_resist_arr = []
+
+# for idx, newjoy in enumerate(r['todayCircleNewJoys']):
+#     newjoys.append(domain.TodayCircleNewJoy(newjoy))
+
+todayCircleNewJoys = list(map(lambda newjoy: domain.TodayCircleNewJoy(newjoy), r['todayCircleNewJoys']))
