b001edf1d6df739d5343c2c0ebdd594902158a1a
H37kouya <h37kouya@gmail.com>
2021-04-12 11:18:54 +0900

    fix model修正

--- api/app/Models/Identifier.php
+++ api/app/Models/Identifier.php
@@ -3,16 +3,17 @@
 namespace App\Models;
 
 use App\Enum\Property\IdentifierProperty as P;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Identifier extends Model
 {
     protected $fillable = [
         P::identifier_hash,
     ];
-    public function CircleGachaResult ()
+
+    public function circleGachaResults(): HasMany
     {
-        return $this->hasOne(CircleGachaResult::class);
+        return $this->hasMany(CircleGachaResult::class);
     }
 }
