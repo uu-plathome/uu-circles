4d915e93a288d2aea30a7d653eb7713dc6283461
H37kouya <h37kouya@gmail.com>
2021-04-13 20:45:59 +0900

    fix type

--- batch/newjoy_notification/settings.py
+++ batch/newjoy_notification/settings.py
@@ -1,14 +1,14 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-#See
-#https://qiita.com/harukikaneko/items/b004048f8d1eca44cba9
+# See
+# https://qiita.com/harukikaneko/items/b004048f8d1eca44cba9
 
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(verbose=True, dotenv_path=dotenv_path)
 
-TOKEN = os.environ.get("TOKEN")
-CHANNEL_ID = os.environ.get("CHANNEL_ID")
-TEST_CHANNEL_ID = os.environ.get("TEST_CHANNEL_ID")
-API_URL = os.environ.get("API_URL")
+TOKEN = str(os.environ.get("TOKEN"))
+CHANNEL_ID = str(os.environ.get("CHANNEL_ID"))
+TEST_CHANNEL_ID = str(os.environ.get("TEST_CHANNEL_ID"))
+API_URL = str(os.environ.get("API_URL"))
