77b180bcd33eb3196eefc3f8bb55b8a2c9f72ff4
H37kouya <h37kouya@gmail.com>
2021-04-22 20:55:42 +0900

    feat uu-yellの記事を取得できるようにした

---
+++ api/app/Enum/Property/UuyellPostProperty.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Enum\Property;
+
+class UuyellPostProperty
+{
+    const id = 'id';
+
+    const wordpress_id = 'wordpress_id';
+
+    const title = 'title';
+
+    const slug = 'slug';
+
+    const link = 'link';
+
+    const date = 'date';
+
+    const featured_media = 'featured_media';
+
+    const media_source_url = 'media_source_url';
+
+    const media_alt_text = 'media_alt_text';
+
+    const published = 'published';
+
+    const notified_at = 'notified_at';
+
+    const created_at = 'created_at';
+
+    const updated_at = 'updated_at';
+}
