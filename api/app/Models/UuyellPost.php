ad312ba72c149c0dc466548c06f3db0c819b7c11
H37kouya <h37kouya@gmail.com>
2021-04-22 20:55:42 +0900

    feat uu-yellの記事を取得できるようにした

---
+++ api/app/Models/UuyellPost.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Enum\Property\UuyellPostProperty as P;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UuyellPost extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        P::wordpress_id,
+        P::title,
+        P::slug,
+        P::link,
+        P::date,
+        P::featured_media,
+        P::media_source_url,
+        P::media_alt_text,
+        P::published,
+        P::notified_at,
+    ];
+}
