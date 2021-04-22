5aafd990eabf603184243b4cc20cc5a77efff6b8
H37kouya <h37kouya@gmail.com>
2021-04-22 15:35:01 +0900

    fix

--- batch/newjoy_notification/bot.py
+++ batch/newjoy_notification/bot.py
@@ -39,41 +39,43 @@ time_arr = sorted(todayCircleNewJoys, key=lambda x: x.circleNewJoy.startDate)
 print(list(map(lambda x: x.circleNewJoy.startDate, time_arr)))
 
 ###MAKE_TIME_DICT###
-time_dict = {}
+# time_dict = {}
 #key = hour * 60 + min
-#start_hour * 60(min), end_hour * 60(min), interval = 30(min)
-for dict_key in range(7 * 60, 32 * 60,30):
-    time_dict[dict_key] = []
+# start_hour * 60(min), end_hour * 60(min), interval = 30(min)
+# for dict_key in range(7 * 60, 32 * 60,30):
+#     time_dict[dict_key] = []
 
-for newjoy in todayCircleNewJoys:
-    print(newjoy.circleNewJoy.occupancyStartTime(), newjoy.circleNewJoy.occupancyEndTime())
-    duration = abs(newjoy.circleNewJoy.occupancyStartTime() - newjoy.circleNewJoy.occupancyEndTime())
-    print(duration.seconds)
-    room_count = duration.seconds / (30 * 60)
-    print(room_count)
-    for dict_key in range(0, int(room_count)):
-        time_dict[dict_key * 30 + newjoy.circleNewJoy.occupancyStartTime().hour * 60 + newjoy.circleNewJoy.occupancyStartTime().minute].append(newjoy.circleNewJoy.circleNewJoyId)
+# for newjoy in todayCircleNewJoys:
+#     print(newjoy.circleNewJoy.occupancyStartTime(), newjoy.circleNewJoy.occupancyEndTime())
+#     duration = abs(newjoy.circleNewJoy.occupancyStartTime() - newjoy.circleNewJoy.occupancyEndTime())
+#     print(duration.seconds)
+#     room_count = duration.seconds / (30 * 60)
+#     print(room_count)
+#     for dict_key in range(0, int(room_count)):
+#         time_dict[dict_key * 30 + newjoy.circleNewJoy.occupancyStartTime().hour * 60 + newjoy.circleNewJoy.occupancyStartTime().minute].append(newjoy.circleNewJoy.circleNewJoyId)
 
-print(time_dict)
+# print(time_dict)
 
 # ###SET_LOOP###
 # # ループ処理
-# @client.event
-# async def on_ready():
-#     channel = client.get_channel(CHANNEL_ID)
-#     # アナウンス
-#     if (len(r['todayCircleNewJoys']) == 0):
-#         await channel.send('***:crescent_moon:今日の新歓はありません***')
-#         exit()
-#     else:
-#         now = get_now()
-#         await channel.send('***☀️今日の新歓 '+now+'***')
-#         for idx, newjoy in enumerate(time_arr):
-#             message = newjoy.make_text(idx)
-#             await channel.send(message)
-#         exit()
 
-# client.run(TOKEN)
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(CHANNEL_ID)
+    # アナウンス
+    if (len(r['todayCircleNewJoys']) == 0):
+        await channel.send('***:crescent_moon:今日の新歓はありません***')
+        exit()
+    else:
+        now = get_now()
+        await channel.send('***☀️今日の新歓 '+now+'***')
+        for idx, newjoy in enumerate(time_arr):
+            message = newjoy.make_text(idx)
+            await channel.send(message)
+        exit()
+
+client.run(TOKEN)
 
 ###TEST_CODES###
 
@@ -90,4 +92,3 @@ print(time_dict)
 # for n, i in enumerate(time_arr):
 #     i.idx =n
 #     print(i.idx,i.name, i.circleNewJoy.format_startDay())
-
