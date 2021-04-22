0296d08472d2af7fc981d4f0ff225192ee446b0b
H37kouya <h37kouya@gmail.com>
2021-04-22 15:25:55 +0900

    feat github actionの追加と変数名変更

--- batch/uuyell_notification/settings.py
+++ batch/uuyell_notification/settings.py
@@ -1,15 +1,15 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-#See
-#https://qiita.com/harukikaneko/items/b004048f8d1eca44cba9
+# See
+# https://qiita.com/harukikaneko/items/b004048f8d1eca44cba9
 
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(verbose=True, dotenv_path=dotenv_path)
 
 TOKEN = os.environ.get("TOKEN")
-CHANNEL_ID = os.environ.get("CHANNEL_ID")
-TEST_CHANNEL_ID = os.environ.get("TEST_CHANNEL_ID")
-API_URL = os.environ.get("API_URL")
-TEST_API_URL = os.environ.get("TEST_API_URL")
+UU_YELL_CHANNEL_ID = os.environ.get("UU_YELL_CHANNEL_ID")
+UU_YELL_TEST_CHANNEL_ID = os.environ.get("UU_YELL_TEST_CHANNEL_ID")
+UU_YELL_API_URL = os.environ.get("UU_YELL_API_URL")
+UU_YELL_TEST_API_URL = os.environ.get("UU_YELL_TEST_API_URL")
