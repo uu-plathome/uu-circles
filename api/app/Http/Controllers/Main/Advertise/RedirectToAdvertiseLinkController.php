8240b5da191ad7a2e9e5edfdc62fd165fe696839
H37kouya <h37kouya@gmail.com>
2021-04-25 11:33:35 +0900

    feat add log

--- api/app/Http/Controllers/Main/Advertise/RedirectToAdvertiseLinkController.php
+++ api/app/Http/Controllers/Main/Advertise/RedirectToAdvertiseLinkController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Main\Advertise;
 use App\Models\Advertise;
 use App\Models\AdvertiseCounter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class RedirectToAdvertiseLinkController
 {
@@ -13,6 +14,10 @@ class RedirectToAdvertiseLinkController
      */
     public function __invoke(Request $request, string $slug)
     {
+        Log::debug("RedirectToAdvertiseLinkController", [
+            'slug' => $slug,
+        ]);
+
         $advertise = Advertise::whereSlug($slug)->first();
 
         if (is_null($advertise)) {
@@ -32,11 +37,18 @@ class RedirectToAdvertiseLinkController
             return;
         }
 
+        Log::debug("RedirectToAdvertiseLinkController count up start");
+
+        // 広告のクリック数
         AdvertiseCounter::whereAdvertiseId($advertise->id)
             ->whereLink($advertise->link)
             ->inRandomOrder()
             ->first()
             ->increment('count', 1);
+
+        Log::debug("RedirectToAdvertiseLinkController redirect to", [
+            'link' => $advertise->link,
+        ]);
 
         return redirect()->away($advertise->link);
     }
