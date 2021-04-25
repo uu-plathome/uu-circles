6a7bb0c2c5fb52d268e3760f6d38edf1895ac3d0
H37kouya <h37kouya@gmail.com>
2021-04-25 17:18:20 +0900

    feat seederの追加

--- api/database/seeders/AdvertisesTableSeeder.php
+++ api/database/seeders/AdvertisesTableSeeder.php
@@ -2,14 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Enum\Property\AdvertiseCounterProperty;
 use App\Models\Advertise;
+use App\Models\AdvertiseCounter;
 use Exception;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class AdvertisesTableSeeder extends Seeder
 {
+    const COUNTER_ROWS = 20;
+
     /**
      * Run the database seeds.
      *
@@ -22,15 +28,30 @@ class AdvertisesTableSeeder extends Seeder
 
         try {
             $idx = 1;
+            $now = Carbon::now();
             /** @var Advertise $user */
             factory(Advertise::class, 10)
                 ->make()
-                ->each(function (Advertise $advertise) use ($idx) {
+                ->each(function (Advertise $advertise) use ($idx, $now) {
                     $advertise->fill([
                         'title' => $advertise->title . $idx,
                         'slug'  => Str::uuid(),
                     ])->save();
                     $idx++;
+
+                    $advertiseCounter = [];
+
+                    for ($i = self::COUNTER_ROWS; $i > 0; $i--) {
+                        $advertiseCounter[] = [
+                            AdvertiseCounterProperty::link         => $advertise->link,
+                            AdvertiseCounterProperty::count        => rand(0, 1000),
+                            AdvertiseCounterProperty::advertise_id => $advertise->id,
+                            AdvertiseCounterProperty::created_at   => $now,
+                            AdvertiseCounterProperty::updated_at   => $now,
+                        ];
+                    }
+
+                    AdvertiseCounter::insert($advertiseCounter);
                 });
 
             DB::commit();
@@ -38,5 +59,7 @@ class AdvertisesTableSeeder extends Seeder
             DB::rollBack();
             throw $e;
         }
+
+        Artisan::call('aggregate:advertise-counter');
     }
 }
