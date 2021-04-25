744b9816529001d0988622ee420a2ef4f6f1b1c0
H37kouya <h37kouya@gmail.com>
2021-04-25 14:57:49 +0900

    fix

--- api/database/migrations/2021_04_25_104952_add_slug_unique_to_advertise.php
+++ api/database/migrations/2021_04_25_104952_add_slug_unique_to_advertise.php
@@ -19,14 +19,6 @@ class AddSlugUniqueToAdvertise extends Migration
             $table->uuid('slug')->nullable()->comment('広告のslug')->change();
         });
 
-        DB::table('advertises')->whereNull('slug')->orderBy('id')->chunk(100, function ($advertises) {
-            foreach ($advertises as $advertise) {
-                $advertise->update([
-                    'slug' => Str::uuid(),
-                ]);
-            }
-        });
-
         Schema::table('advertises', function (Blueprint $table) {
             $table->uuid('slug')->unique()->comment('広告のslug')->change();
         });
