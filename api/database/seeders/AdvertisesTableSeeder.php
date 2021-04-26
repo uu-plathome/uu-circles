b9fe2e0a650ea814933b0ef75e66f2144256caa5
H37kouya <h37kouya@gmail.com>
2021-04-26 16:18:30 +0900

    feat 画像のURL追加

--- api/database/seeders/AdvertisesTableSeeder.php
+++ api/database/seeders/AdvertisesTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Enum\Property\AdvertiseCounterProperty;
+use App\Enum\Property\AdvertiseProperty;
 use App\Models\Advertise;
 use App\Models\AdvertiseCounter;
 use Exception;
@@ -34,8 +35,12 @@ class AdvertisesTableSeeder extends Seeder
                 ->make()
                 ->each(function (Advertise $advertise) use ($idx, $now) {
                     $advertise->fill([
-                        'title' => $advertise->title . $idx,
-                        'slug'  => Str::uuid(),
+                        AdvertiseProperty::title  => $advertise->title . $idx,
+                        AdvertiseProperty::slug   => Str::uuid(),
+                        AdvertiseProperty::active => $idx === 0,
+                        AdvertiseProperty::main_image_url => $idx === 0 ?
+                            'https://firebasestorage.googleapis.com/v0/b/uu-circle20.appspot.com/o/circles%2Fcycle-club.jpg?alt=media&token=d934d034-58f9-4082-bf72-2f2524713ddc' :
+                            null,
                     ])->save();
                     $idx++;
 
