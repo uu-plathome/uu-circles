e16ca03299a8aba7210cd08d4600d2f4e6e877e7
H37kouya <h37kouya@gmail.com>
2021-05-01 21:13:06 +0900

    feat お知らせのmodelの作成

---
+++ api/app/Enum/Property/AnnouncementProperty.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Enum\Property;
+
+class AnnouncementProperty
+{
+    const id = 'id';
+    const title = 'title';
+    const description = 'description';
+    const link = 'link';
+    const announcement_type = 'announcement_type';
+    const for_main_view = 'for_main_view';
+    const for_circle_view = 'for_circle_view';
+    const for_circle_mail = 'for_circle_mail';
+    const for_admin_view = 'for_admin_view';
+    const for_admin_mail = 'for_admin_mail';
+    const for_newjoy_discord = 'for_newjoy_discord';
+    const notification_time = 'notification_time';
+    const active = 'active';
+    const publish_from = 'publish_from';
+    const publish_to = 'publish_to';
+    const deleted_at = 'deleted_at';
+    const created_at = 'created_at';
+    const updated_at = 'updated_at';
+}
