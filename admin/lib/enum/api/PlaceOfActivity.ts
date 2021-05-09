6009080114961ae1273e990fb1efa41275d54974
Lint Action <lint-action@samuelmeuli.com>
2021-05-09 06:46:23 +0000

    Fix code style issues with Prettier

--- admin/lib/enum/api/PlaceOfActivity.ts
+++ admin/lib/enum/api/PlaceOfActivity.ts
@@ -35,7 +35,7 @@ export const PlaceOfActivity = {
   /**
    * その他
    */
-  OTHER: 'OTHER'
+  OTHER: 'OTHER',
 } as const
 
 export type PlaceOfActivityKey = keyof typeof PlaceOfActivity
@@ -44,15 +44,18 @@ export type PlaceOfActivity = typeof PlaceOfActivity[keyof typeof PlaceOfActivit
 /**
  * 活動場所
  */
-export const getAllPlaceOfActivity = (): PlaceOfActivity[] => Object.values(PlaceOfActivity)
+export const getAllPlaceOfActivity = (): PlaceOfActivity[] =>
+  Object.values(PlaceOfActivity)
 /**
  * 活動場所
  */
-export const getAllPlaceOfActivityKey = (): PlaceOfActivityKey[] => Object.keys(PlaceOfActivity) as PlaceOfActivityKey[]
+export const getAllPlaceOfActivityKey = (): PlaceOfActivityKey[] =>
+  Object.keys(PlaceOfActivity) as PlaceOfActivityKey[]
 /**
  * 活動場所
  */
-export const isPlaceOfActivity = (s: any): s is PlaceOfActivity => Object.values(PlaceOfActivity).includes(s)
+export const isPlaceOfActivity = (s: any): s is PlaceOfActivity =>
+  Object.values(PlaceOfActivity).includes(s)
 
 /**
  * 峰キャンパス
@@ -65,11 +68,13 @@ export const isYoto = (v: any): v is 'YOTO' => v === PlaceOfActivity.YOTO
 /**
  * 峰キャンパス, 陽東キャンパス
  */
-export const isMineAndYoto = (v: any): v is 'MINE_AND_YOTO' => v === PlaceOfActivity.MINE_AND_YOTO
+export const isMineAndYoto = (v: any): v is 'MINE_AND_YOTO' =>
+  v === PlaceOfActivity.MINE_AND_YOTO
 /**
  * 新歓Discord
  */
-export const isNewjoyDiscord = (v: any): v is 'NEWJOY_DISCORD' => v === PlaceOfActivity.NEWJOY_DISCORD
+export const isNewjoyDiscord = (v: any): v is 'NEWJOY_DISCORD' =>
+  v === PlaceOfActivity.NEWJOY_DISCORD
 /**
  * Zoom
  */
@@ -77,7 +82,8 @@ export const isZoom = (v: any): v is 'ZOOM' => v === PlaceOfActivity.ZOOM
 /**
  * Discord
  */
-export const isDiscord = (v: any): v is 'DISCORD' => v === PlaceOfActivity.DISCORD
+export const isDiscord = (v: any): v is 'DISCORD' =>
+  v === PlaceOfActivity.DISCORD
 /**
  * その他
  */
