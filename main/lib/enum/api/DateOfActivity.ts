225a57d89460d64ec2767fe3f21aba113ae61aca
Lint Action <lint-action@samuelmeuli.com>
2021-05-09 06:47:26 +0000

    Fix code style issues with Prettier

--- main/lib/enum/api/DateOfActivity.ts
+++ main/lib/enum/api/DateOfActivity.ts
@@ -10,7 +10,7 @@ export const DateOfActivity = {
   /**
    * 隔週
    */
-  EVERY_OTHER_WEEK: 'EVERY_OTHER_WEEK'
+  EVERY_OTHER_WEEK: 'EVERY_OTHER_WEEK',
 } as const
 
 export type DateOfActivityKey = keyof typeof DateOfActivity
@@ -19,21 +19,26 @@ export type DateOfActivity = typeof DateOfActivity[keyof typeof DateOfActivity]
 /**
  * 活動日時
  */
-export const getAllDateOfActivity = (): DateOfActivity[] => Object.values(DateOfActivity)
+export const getAllDateOfActivity = (): DateOfActivity[] =>
+  Object.values(DateOfActivity)
 /**
  * 活動日時
  */
-export const getAllDateOfActivityKey = (): DateOfActivityKey[] => Object.keys(DateOfActivity) as DateOfActivityKey[]
+export const getAllDateOfActivityKey = (): DateOfActivityKey[] =>
+  Object.keys(DateOfActivity) as DateOfActivityKey[]
 /**
  * 活動日時
  */
-export const isDateOfActivity = (s: any): s is DateOfActivity => Object.values(DateOfActivity).includes(s)
+export const isDateOfActivity = (s: any): s is DateOfActivity =>
+  Object.values(DateOfActivity).includes(s)
 
 /**
  * 毎週
  */
-export const isEveryWeek = (v: any): v is 'EVERY_WEEK' => v === DateOfActivity.EVERY_WEEK
+export const isEveryWeek = (v: any): v is 'EVERY_WEEK' =>
+  v === DateOfActivity.EVERY_WEEK
 /**
  * 隔週
  */
-export const isEveryOtherWeek = (v: any): v is 'EVERY_OTHER_WEEK' => v === DateOfActivity.EVERY_OTHER_WEEK
+export const isEveryOtherWeek = (v: any): v is 'EVERY_OTHER_WEEK' =>
+  v === DateOfActivity.EVERY_OTHER_WEEK
