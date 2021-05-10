c9a321a5654e14854055389c95c5563baeeccdf3
H37kouya <h37kouya@gmail.com>
2021-05-10 22:13:05 +0900

    feat お知らせのCreate, Updateができるようになった

--- api/app/Usecases/Admin/Announcement/CreateAnnouncementUsecase.php
+++ api/app/Usecases/Admin/Announcement/CreateAnnouncementUsecase.php
@@ -2,15 +2,66 @@
 
 namespace App\Usecases\Admin\Announcement;
 
+use App\Enum\Property\AnnouncementProperty;
+use App\Models\Announcement;
 use App\Usecases\Admin\Announcement\Params\CreateAnnouncementUsecaseParam;
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class CreateAnnouncementUsecase
 {
+    /**
+     * お知らせの追加
+     *
+     * @param CreateAnnouncementUsecaseParam $param
+     * @throws Exception
+     */
     public function invoke(CreateAnnouncementUsecaseParam $param)
     {
         Log::debug("CreateAnnouncementUsecase args", [
             "CreateAnnouncementUsecaseParam" => $param,
         ]);
+
+        // インサート用のデータ
+        $insertData = $this->getInsertData($param);
+
+        DB::beginTransaction();
+        try {
+            // お知らせをDBに保存する
+            (new Announcement())->fill($insertData)
+                ->save();
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * DBに挿入するデータ
+     *
+     * @param CreateAnnouncementUsecaseParam $param
+     * @return array
+     */
+    private function getInsertData(CreateAnnouncementUsecaseParam $param): array
+    {
+        return [
+            AnnouncementProperty::title              => $param->title,
+            AnnouncementProperty::description        => $param->description,
+            AnnouncementProperty::link               => $param->link,
+            AnnouncementProperty::announcement_type  => $param->announcement_type,
+            AnnouncementProperty::for_main_view      => $param->for_main_view,
+            AnnouncementProperty::for_circle_view    => $param->for_circle_view,
+            AnnouncementProperty::for_circle_mail    => $param->for_circle_mail,
+            AnnouncementProperty::for_admin_view     => $param->for_admin_view,
+            AnnouncementProperty::for_admin_mail     => $param->for_admin_mail,
+            AnnouncementProperty::for_newjoy_discord => $param->for_newjoy_discord,
+            AnnouncementProperty::active             => $param->active,
+            AnnouncementProperty::notification_time  => $param->notification_time,
+            AnnouncementProperty::publish_from       => $param->publish_from,
+            AnnouncementProperty::publish_to         => $param->publish_to,
+        ];
     }
 }
--- api/app/Usecases/Admin/Announcement/Params/CreateAnnouncementUsecaseParam.php
+++ api/app/Usecases/Admin/Announcement/Params/CreateAnnouncementUsecaseParam.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Carbon;
 class CreateAnnouncementUsecaseParam
 {
     public string $title;
-    public string $description;
-    public string $link;
+    public ?string $description;
+    public ?string $link;
     public string $announcement_type;
     public bool $for_main_view;
     public bool $for_circle_view;
@@ -17,7 +17,7 @@ class CreateAnnouncementUsecaseParam
     public bool $for_admin_mail;
     public bool $for_newjoy_discord;
     public bool $active;
-    public Carbon $notification_time;
-    public Carbon $publish_from;
-    public Carbon $publish_to;
+    public ?Carbon $notification_time;
+    public ?Carbon $publish_from;
+    public ?Carbon $publish_to;
 }
