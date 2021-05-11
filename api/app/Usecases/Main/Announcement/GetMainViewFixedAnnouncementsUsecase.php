e67d2bb7980374f76f262ed6255528b57dbbac79
H37kouya <h37kouya@gmail.com>
2021-05-11 20:10:47 +0900

    feat メイン画面のUsecase追加

---
+++ api/app/Usecases/Main/Announcement/GetMainViewFixedAnnouncementsUsecase.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Usecases\Main\Announcement;
+
+use App\Enum\Property\AnnouncementProperty;
+use App\Models\Announcement;
+use App\Usecases\Main\Announcement\Dto\GetMainViewFixedAnnouncementsUsecaseDto;
+use App\Usecases\Main\Announcement\Dto\MainAnnouncementDto;
+use Illuminate\Support\Carbon;
+
+class GetMainViewFixedAnnouncementsUsecase
+{
+    /**
+     * メイン画面に固定して、表示するお知らせを取得
+     */
+    public function invoke(): GetMainViewFixedAnnouncementsUsecaseDto
+    {
+        $now = Carbon::now();
+
+        // DBからお知らせを取得
+        $announcements = Announcement::nowPublic($now)
+            ->whereIsMainViewFixed(true)
+            ->select([
+                AnnouncementProperty::id,
+                AnnouncementProperty::title,
+                AnnouncementProperty::description,
+                AnnouncementProperty::announcement_type,
+                AnnouncementProperty::importance,
+                AnnouncementProperty::active,
+                AnnouncementProperty::publish_from,
+                AnnouncementProperty::publish_to,
+            ])
+            ->get();
+
+        // 戻り値の生成
+        $dto = new GetMainViewFixedAnnouncementsUsecaseDto();
+        $dto->announcements = $announcements->map(
+            fn (Announcement $announcement) => MainAnnouncementDto::byEloquent($announcement)
+        )->toArray();
+        return $dto;
+    }
+}
