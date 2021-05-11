9bc2558f2b5c2df81cec765fab56c5b283013390
H37kouya <h37kouya@gmail.com>
2021-05-11 19:06:04 +0900

    feat seederの追加と修正

---
+++ api/database/factories/AnnouncementFactory.php
@@ -0,0 +1,49 @@
+<?
+
+namespace Database\Factories;
+
+use App\Enum\AnnouncementType;
+use App\Enum\Importance;
+use App\Enum\Property\AnnouncementProperty;
+use App\Models\Announcement;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AnnouncementFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Announcement::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            AnnouncementProperty::title              => 'アンケートにご回答お願いします。',
+            AnnouncementProperty::announcement_type  => AnnouncementType::QUESTIONNAIRE,
+            AnnouncementProperty::link               => 'https://uu-circles.com',
+            AnnouncementProperty::importance         => Importance::HIGH,
+            AnnouncementProperty::is_main_view_fixed => true,
+        ];
+    }
+
+    /**
+     * 管理者画面でのみ、固定するお知らせ
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function isAdminViewFixed(): Factory
+    {
+        return $this->state(fn (array $attributes) => [
+            AnnouncementProperty::title               => 'サークルが追加できない現象に関して',
+            AnnouncementProperty::announcement_type   => AnnouncementType::BUG,
+            AnnouncementProperty::is_admin_view_fixed => true,
+        ]);
+    }
+}
