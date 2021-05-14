68beddb149a5bae1c6c6fbe84caf81a2753fafc8
H37kouya <h37kouya@gmail.com>
2021-05-14 23:20:40 +0900

    feat seeder修正

--- api/database/seeders/AnnouncementSeeder.php
+++ api/database/seeders/AnnouncementSeeder.php
@@ -2,12 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Enum\Property\AnnouncementCounterProperty;
 use App\Models\Announcement;
+use App\Models\AnnouncementCounter;
+use App\Usecases\Batch\Announcement\AggregateAnnouncementCounterPerDayUsecase;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class AnnouncementSeeder extends Seeder
 {
+    private AggregateAnnouncementCounterPerDayUsecase $aggregateAnnouncementCounterPerDayUsecase;
+
+    public function __construct(
+        AggregateAnnouncementCounterPerDayUsecase $aggregateAnnouncementCounterPerDayUsecase
+    ) {
+        $this->aggregateAnnouncementCounterPerDayUsecase = $aggregateAnnouncementCounterPerDayUsecase;
+    }
+
     /**
      * Run the database seeds.
      *
@@ -17,17 +29,103 @@ class AnnouncementSeeder extends Seeder
     public function run()
     {
         DB::beginTransaction();
+        $now = Carbon::now();
 
+        $ids = [];
+
+        DB::beginTransaction();
         try {
             // 通常のお知らせ
-            Announcement::factory()->count(1)->create();
-            // 管理者画面のみ固定
-            Announcement::factory()->count(1)->isAdminViewFixed()->create();
+            $announcement = Announcement::factory()
+                ->count(1)
+                ->create()
+                ->first();
+            $ids[] = $announcement->id;
 
+            // 管理者画面のみ固定
+            $announcement = Announcement::factory()
+                ->count(1)
+                ->isAdminViewFixed()
+                ->create()
+                ->first();
+            $ids[] = $announcement->id;
             DB::commit();
         } catch (\Exception $e) {
-            DB::rollBack();
-            throw $e;
+            DB::rollback();
         }
+
+        // ここはトランザクションを関数内で貼りたいので、トランザクションの外にしている。
+        $this->insertAnnouncementCounter($ids, $now);
+    }
+
+    private function insertAnnouncementCounter(array $announcementIds, Carbon $now)
+    {
+        $columnNumbers = AnnouncementCounter::columnNumbers();
+
+        $announcementCounter = [];
+
+        // 2日前
+        $twoDaysAgo = $now->copy()->subDay()->subDay();
+        foreach ($announcementIds as $announcementId) {
+            foreach ($columnNumbers as $announcementPlace => $maxColumnNumber) {
+                for ($i = $maxColumnNumber; $i > 0; $i--) {
+                    $announcementCounter[] = [
+                        AnnouncementCounterProperty::count              => rand(0, 1000),
+                        AnnouncementCounterProperty::announcement_id    => $announcementId,
+                        AnnouncementCounterProperty::announcement_place => $announcementPlace,
+                        AnnouncementCounterProperty::created_at         => $twoDaysAgo,
+                        AnnouncementCounterProperty::updated_at         => $twoDaysAgo,
+                    ];
+                }
+            }
+        }
+        AnnouncementCounter::insert($announcementCounter);
+        // 2日前に集計する
+        $this->aggregateAnnouncementCounterPerDayUsecase->invoke($twoDaysAgo);
+
+        // 1日前
+        $oneDaysAgo = $now->copy()->subDay();
+        AnnouncementCounter::query()
+            ->chunk(
+                200,
+                function ($announcementCounters) {
+                    DB::beginTransaction();
+                    try {
+                        /** @var AnnouncementCounter $announcementCounter */
+                        foreach ($announcementCounters as $announcementCounter) {
+                            $announcementCounter
+                                ->increment(AnnouncementCounterProperty::count, rand(0, 1000));
+                        }
+
+                        DB::commit();
+                    } catch (\Exception $e) {
+                        DB::rollback();
+                    }
+                }
+            );
+        // 1日前に集計する
+        $this->aggregateAnnouncementCounterPerDayUsecase->invoke($oneDaysAgo);
+
+        // 今日
+        AnnouncementCounter::query()
+            ->chunk(
+                200,
+                function ($announcementCounters) {
+                    DB::beginTransaction();
+                    try {
+                        /** @var AnnouncementCounter $announcementCounter */
+                        foreach ($announcementCounters as $announcementCounter) {
+                            $announcementCounter
+                                ->increment(AnnouncementCounterProperty::count, rand(0, 1000));
+                        }
+
+                        DB::commit();
+                    } catch (\Exception $e) {
+                        DB::rollback();
+                    }
+                }
+            );
+        // 今日、集計する
+        $this->aggregateAnnouncementCounterPerDayUsecase->invoke($now);
     }
 }
