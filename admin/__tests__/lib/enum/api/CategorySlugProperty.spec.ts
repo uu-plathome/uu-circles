a871e9af9295b7f4aa616adafeb20961b85d0ac2
Lint Action <lint-action@samuelmeuli.com>
2021-05-14 11:42:11 +0000

    Fix code style issues with Prettier

--- admin/__tests__/lib/enum/api/CategorySlugProperty.spec.ts
+++ admin/__tests__/lib/enum/api/CategorySlugProperty.spec.ts
@@ -2,15 +2,27 @@ import * as TestFunc from '@/lib/enum/api/CategorySlugProperty'
 
 describe('Enum CategorySlugProperty', () => {
   it('isOfficialOrganization', () => {
-    expect(TestFunc.CategorySlugProperty.official_organization).toBe('official_organization')
-    expect(TestFunc.isCategorySlugProperty('official_organization')).toBeTruthy()
-    expect(TestFunc.isOfficialOrganization('official_organization')).toBeTruthy()
+    expect(TestFunc.CategorySlugProperty.official_organization).toBe(
+      'official_organization'
+    )
+    expect(
+      TestFunc.isCategorySlugProperty('official_organization')
+    ).toBeTruthy()
+    expect(
+      TestFunc.isOfficialOrganization('official_organization')
+    ).toBeTruthy()
     expect(TestFunc.isOfficialOrganization('aaaaabbbbcccc')).toBeFalsy()
   })
   it('isUnofficialOrganization', () => {
-    expect(TestFunc.CategorySlugProperty.unofficial_organization).toBe('unofficial_organization')
-    expect(TestFunc.isCategorySlugProperty('unofficial_organization')).toBeTruthy()
-    expect(TestFunc.isUnofficialOrganization('unofficial_organization')).toBeTruthy()
+    expect(TestFunc.CategorySlugProperty.unofficial_organization).toBe(
+      'unofficial_organization'
+    )
+    expect(
+      TestFunc.isCategorySlugProperty('unofficial_organization')
+    ).toBeTruthy()
+    expect(
+      TestFunc.isUnofficialOrganization('unofficial_organization')
+    ).toBeTruthy()
     expect(TestFunc.isUnofficialOrganization('aaaaabbbbcccc')).toBeFalsy()
   })
   it('isStudentGroup', () => {
