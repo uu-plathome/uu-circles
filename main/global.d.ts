b3a3e79a2f0c8ff79959fa3482f0890cd4b6494a
H37kouya <h37kouya@gmail.com>
2021-05-16 10:03:56 +0900

    check stg

---
+++ main/global.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="node" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+    readonly API_URL: string;
+    readonly GOOGLE_ANALYTICS_ID: string;
+    readonly BUGSNAG_API_KEY: string;
+  }
+}
