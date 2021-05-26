4dfa5f6bef738fee48a949627b43bda41fd5eafb
Ayupon <ems04zudahpon@gmail.com>
2021-05-26 17:55:52 +0900

    ガチャを実装しました。

---
+++ main/pages/gacha/index.tsx
@@ -0,0 +1,47 @@
+import { BaseFooter } from '@/components/layouts/BaseFooter'
+import { BaseHead } from '@/components/layouts/BaseHead'
+import { BaseLayout } from '@/components/layouts/BaseLayout'
+import { BaseContainer } from '@/components/molecules/Container/BaseContainer'
+import { NextPage } from 'next'
+import Image from 'next/image'
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <BaseHead title="サークルガチャ" />
+
+      <BaseLayout>
+        <div className="bg-gray-100 px-2">
+          <BaseContainer>
+            <div className="flex justify-center items-center pt-6">
+                <Image src="/images/gacha-logo.png" width="360" height="120" />
+            </div>
+            <h1 className="text-2xl pt-6 px-4 text-center">
+              サークルガチャ
+            </h1>
+            <div className="flex justify-center items-center pt-6">
+                <Image src="/images/gacha800.png" width="400" height="500" />
+            </div>
+            <div className="flex justify-center">
+                <a className="" href="">
+                    <div className="flex-initial rounded bg-green-500 px-8 py-4 text-white mx-6 my-6 font-black">
+                    1連ガチャ
+                    </div>
+                </a>
+                <a className="" href="">
+                    <div className="flex-initial rounded bg-green-500 px-8 py-4 text-white mx-6 my-6 font-black">
+                    10連ガチャ
+                    </div>
+                </a>
+            </div>
+          </BaseContainer>
+        </div>
+
+        {/*  フッター */}
+        <BaseFooter />
+      </BaseLayout>
+    </div>
+  )
+}
+
+export default Page
