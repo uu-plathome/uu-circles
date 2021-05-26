168d41961706e5c0c5e5040a0711a9b30fae5ded
H37kouya <h37kouya@gmail.com>
2021-05-26 10:26:06 +0900

    feat add cache

--- api/app/Http/Controllers/Circle/Announcement/FixedCircleViewAnnouncementController.php
+++ api/app/Http/Controllers/Circle/Announcement/FixedCircleViewAnnouncementController.php
@@ -8,10 +8,13 @@ use App\Models\Announcement;
 use App\Support\Arr;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class FixedCircleViewAnnouncementController extends Controller
 {
+    const TTL = 60 * 60;
+
     /**
      * サークル管理画面に固定表示するお知らせの取得
      *
@@ -25,15 +28,25 @@ class FixedCircleViewAnnouncementController extends Controller
         $now = Carbon::now();
 
         // お知らせ取得
-        $announcement = Announcement::nowPublic($now)
-            ->whereIsCircleViewFixed(true)
-            ->orderByDesc(AnnouncementProperty::updated_at)
-            ->first();
+        /** @var Announcement $announcement */
+        $announcement = Cache::remember(
+            $this->getCacheKey(),
+            self::TTL,
+            fn () => Announcement::nowPublic($now)
+                ->whereIsCircleViewFixed(true)
+                ->orderByDesc(AnnouncementProperty::updated_at)
+                ->first()
+        );
 
         return [
             'data' => !is_null($announcement)
                 ? Arr::camel_keys($announcement->toArray())
                 : null,
         ];
+    }
+
+    private function getCacheKey(): string
+    {
+        return 'FixedCircleViewAnnouncementController' . Carbon::today()->format("Y-m-d H");
     }
 }
