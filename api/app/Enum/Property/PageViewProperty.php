21850b59821763bda64a342ecf2b01d074127357
H37kouya <h37kouya@gmail.com>
2021-05-27 11:54:32 +0900

    feat Google AnalyticsのデータをDBに入れることができるようになった

---
+++ api/app/Enum/Property/PageViewProperty.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enum\Property;
+
+class PageViewProperty
+{
+    const id = 'id';
+    const url = 'url';
+    const page_views = 'page_views';
+    const active_users = 'active_users';
+    const created_at = 'created_at';
+    const updated_at = 'updated_at';
+}
