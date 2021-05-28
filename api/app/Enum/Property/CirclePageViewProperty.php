f2d85805f012a7d2f59240a095d646b10d5e648f
H37kouya <h37kouya@gmail.com>
2021-05-28 18:28:46 +0900

    feat CirclePageViewの作成

---
+++ api/app/Enum/Property/CirclePageViewProperty.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enum\Property;
+
+class CirclePageViewProperty
+{
+    const id = 'id';
+    const circle_id = 'circle_id';
+    const slug = 'slug';
+    const page_views = 'page_views';
+    const active_users = 'active_users';
+    const created_at = 'created_at';
+    const updated_at = 'updated_at';
+}
