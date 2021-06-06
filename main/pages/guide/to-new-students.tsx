6cc5305ab6bd536818b909e64377140380d0b034
H37kouya <h37kouya@gmail.com>
2021-06-06 11:17:07 +0900

    fix

--- main/pages/guide/to-new-students.tsx
+++ main/pages/guide/to-new-students.tsx
@@ -72,15 +72,14 @@ const Page: NextPage<Props> = () => {
             <h1 className="text-2xl pt-6 pb-8 px-4">新入生へ</h1>
             <div className="bg-white px-8 py-6 mx-4 rounded-md text-gray-500">
               <h1 className="text-2xl pt-1">目次</h1>
-              <p className="pt-2">0.新入生へ</p>
-              <p className="pt-2">1.気になったサークルを見てみよう</p>
-              <p className="pt-2">2.オンライン新歓会場に参加してみよう</p>
-              <p className="pt-2">3.いつ・どんな新歓があるか確認してみよう</p>
-              <p className="pt-2">4.宇都宮大学の情報を知ろう</p>
-              <p className="pt-2">5.先輩のお話を聞いてみよう</p>
+              <p className="pt-2">0. 新入生へ</p>
+              <p className="pt-2">1. 気になったサークルを見てみよう</p>
+              {/* <p className="pt-2">2. オンライン新歓会場に参加してみよう</p> */}
+              <p className="pt-2">2. いつ・どんな新歓があるか確認してみよう</p>
+              <p className="pt-2">3. uu-yellをみよう</p>
             </div>
 
-            <SubHeader>0.新入生へ</SubHeader>
+            <SubHeader>0. 新入生へ</SubHeader>
             <p className="px-4 pb-6">新入生の方々合格おめでとうございます。</p>
             <p className="px-4 pb-6">
               新型コロナという残念な状況ではありますが、
@@ -88,7 +87,7 @@ const Page: NextPage<Props> = () => {
               在校生一同皆様のご入学を心からお祝い申し上げます
             </p>
 
-            <SubHeader>1.気になったサークルを見てみよう</SubHeader>
+            <SubHeader>1. 気になったサークルを見てみよう</SubHeader>
 
             <p className="pb-6 px-4">
               ホームのサークル一覧から、「なんだか楽しそう！」と思ったビラをタップして
@@ -99,7 +98,7 @@ const Page: NextPage<Props> = () => {
               <GreenLgButton href="/">UU-Circleのトップ画面へ</GreenLgButton>
             </div>
 
-            <SubHeader>2.オンライン新歓会場に参加してみよう</SubHeader>
+            {/* <SubHeader>2.オンライン新歓会場に参加してみよう</SubHeader>
 
             <div className="px-4 pb-6">
               <p>
@@ -113,9 +112,9 @@ const Page: NextPage<Props> = () => {
               <GreenLgButton href="/guide/discord">
                 オンライン新歓会場への参加方法へ
               </GreenLgButton>
-            </div>
+            </div> */}
 
-            <SubHeader>3.いつ・どんな新歓があるか確認してみよう</SubHeader>
+            <SubHeader>2. いつ・どんな新歓があるか確認してみよう</SubHeader>
 
             <div className="px-4 pb-6">
               <p>
@@ -130,7 +129,7 @@ const Page: NextPage<Props> = () => {
               </GreenLgButton>
             </div>
 
-            <SubHeader>4.uu-yellをみよう</SubHeader>
+            <SubHeader>3. uu-yellをみよう</SubHeader>
 
             <div className="px-4 pb-6">
               <p className="pb-6 px-4"> キャッチコピーは「uu-mailのとなりにuu-yell。」</p>
