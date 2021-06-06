d96397ce1de27d744adc9d791a287816a59fedfb
H37kouya <h37kouya@gmail.com>
2021-06-06 11:57:48 +0900

    feat タグの集計追加

---
+++ api/app/Enum/Property/TagPageViewProperty.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enum\Property;
+
+class TagPageViewProperty
+{
+    const id = 'id';
+    const tag_name = 'tag_name';
+    const page_views = 'page_views';
+    const active_users = 'active_users';
+    const created_at = 'created_at';
+    const updated_at = 'updated_at';
+}
