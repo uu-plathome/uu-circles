15f67f333d69f1531f7c701d1c659713dc774162
H37kouya <h37kouya@gmail.com>
2021-06-08 19:02:38 +0900

    fix

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -17,7 +17,8 @@ class Kernel extends HttpKernel
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
-        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        // メンテナンス中かどうかを確認
+        // \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
