fa23fdc5329b26cb6d4c31a8cd4b517d3df3d562
H37kouya <h37kouya@gmail.com>
2021-06-08 21:05:15 +0900

    refactor admin

---
+++ api/app/Enum/RouteProperty/AdminRouteProperty.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enum\RouteProperty;
+
+class AdminRouteProperty
+{
+    // 管理者ユーザー
+    const AdminAdminUserIndex = 'admin.admin-user.index';
+    const AdminAdminUserRegister = 'admin.admin-user.register';
+    const AdminAdminUserShow = 'admin.admin-user.show';
+    const AdminAdminUserUpdate = 'admin.admin-user.update';
+    const AdminAdminUserDelete = 'admin.admin-user.delete';
+
+    // サークル
+    const AdminCircleIndex = 'admin.circle.index';
+    const AdminCirclePaginate = 'admin.circle.paginate';
+    const AdminCircleRegister = 'admin.circle.register';
+    const AdminCircleShow = 'admin.circle.show';
+    const AdminCircleUpdate = 'admin.circle.update';
+    const AdminCircleDelete = 'admin.circle.delete';
+}
--- api/routes/admin.php
+++ api/routes/admin.php
@@ -1,136 +1,140 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminUser\DeleteAdminUserController;
-use App\Http\Controllers\Admin\AdminUser\IndexAdminUserController;
-use App\Http\Controllers\Admin\AdminUser\ShowAdminUserController;
-use App\Http\Controllers\Admin\AdminUser\UpdateAdminUserController;
-use App\Http\Controllers\Admin\Advertise\CreateAdvertiseController;
-use App\Http\Controllers\Admin\Advertise\DeleteAdvertiseController;
-use App\Http\Controllers\Admin\Advertise\DownloadAdvertiseCounterHistoryExcelController;
-use App\Http\Controllers\Admin\Advertise\DownloadAdvertiseExcelController;
-use App\Http\Controllers\Admin\Advertise\IndexAdvertiseController;
-use App\Http\Controllers\Admin\Advertise\ShowAdvertiseController;
-use App\Http\Controllers\Admin\Advertise\UpdateAdvertiseController;
-use App\Http\Controllers\Admin\Announcement\CreateAnnouncementController;
-use App\Http\Controllers\Admin\Announcement\DeleteAnnouncementController;
-use App\Http\Controllers\Admin\Announcement\FixedAdminViewAnnouncementController;
-use App\Http\Controllers\Admin\Announcement\IndexAnnouncementController;
-use App\Http\Controllers\Admin\Announcement\ShowAnnouncementController;
-use App\Http\Controllers\Admin\Announcement\UpdateAnnouncementController;
-use App\Http\Controllers\Admin\Auth\ForgotPasswordAdminController;
-use App\Http\Controllers\Admin\Auth\LoginAdminController;
-use App\Http\Controllers\Admin\Auth\LogoutAdminController;
-use App\Http\Controllers\Admin\Auth\RegisterAdminController;
-use App\Http\Controllers\Admin\Auth\ResetPasswordAdminController;
-use App\Http\Controllers\Admin\Auth\ShowOwnAdminUserController;
-use App\Http\Controllers\Admin\Auth\VerificationConfirmController;
-use App\Http\Controllers\Admin\Auth\VerificationResendController;
-use App\Http\Controllers\Admin\Auth\VerificationVerifyController;
-use App\Http\Controllers\Admin\Circle\CreateCircleController;
-use App\Http\Controllers\Admin\Circle\DeleteCircleController;
-use App\Http\Controllers\Admin\Circle\IndexCircleController;
-use App\Http\Controllers\Admin\Circle\PaginateCircleController;
-use App\Http\Controllers\Admin\Circle\ShowCircleController;
-use App\Http\Controllers\Admin\Circle\UpdateCircleController;
-use App\Http\Controllers\Admin\CircleNewJoy\CopyCircleNewJoyController;
-use App\Http\Controllers\Admin\CircleNewJoy\DeleteCircleNewJoyController;
-use App\Http\Controllers\Admin\CircleNewJoy\IndexCircleNewJoyController;
-use App\Http\Controllers\Admin\CircleNewJoy\RegisterCircleNewJoyController;
-use App\Http\Controllers\Admin\CircleNewJoy\ShowCircleNewJoyController;
-use App\Http\Controllers\Admin\CircleNewJoy\UpdateCircleNewJoyController;
-use App\Http\Controllers\Admin\CircleTag\CreateOrUpdateCircleTagController;
-use App\Http\Controllers\Admin\CircleTag\GetCircleTagController;
-use App\Http\Controllers\Admin\CircleUser\CreateCircleUserRelationController;
-use App\Http\Controllers\Admin\CircleUser\DeleteCircleUserController;
-use App\Http\Controllers\Admin\CircleUser\DeleteCircleUserRelationController;
-use App\Http\Controllers\Admin\AllUser\IndexAllUserController;
-use App\Http\Controllers\Admin\CircleUser\IndexCircleUserByUserIdController;
-use App\Http\Controllers\Admin\CircleUser\IndexCircleUserController;
-use App\Http\Controllers\Admin\CircleUser\RegisterCircleUserController;
-use App\Http\Controllers\Admin\CircleUser\ShowCircleUserController;
-use App\Http\Controllers\Admin\CircleUser\UpdateCircleUserController;
-use App\Http\Controllers\Admin\Storage\PutStorageController;
+use App\Enum\RouteProperty\AdminRouteProperty as ARP;
+use App\Http\Controllers\Admin\AdminUser;
+use App\Http\Controllers\Admin\Advertise;
+use App\Http\Controllers\Admin\AllUser;
+use App\Http\Controllers\Admin\Announcement;
+use App\Http\Controllers\Admin\Auth;
+use App\Http\Controllers\Admin\Circle;
+use App\Http\Controllers\Admin\CircleNewJoy;
+use App\Http\Controllers\Admin\CircleTag;
+use App\Http\Controllers\Admin\CircleUser;
+use App\Http\Controllers\Admin\Storage;
 
 Route::middleware('throttle:60,1')->group(function () {
-    Route::post('email/resend', VerificationResendController::class)->name('admin.verification.resend');
+    Route::post('email/resend', Auth\VerificationResendController::class)->name('admin.verification.resend');
 });
 
 Route::group(['middleware' => 'guest:adminUser'], function () {
     Route::middleware('throttle:60,1')->group(function () {
-        Route::post('/login', LoginAdminController::class)->name('admin.auth.login');
+        Route::post('/login', Auth\LoginAdminController::class)->name('admin.auth.login');
     });
-    
-    Route::middleware('throttle:30,1')->group(function () {
-        Route::get('email/verify/{userId}', VerificationVerifyController::class)->name('admin.verification.verify');
-        Route::post('email/verify/{userId}', VerificationConfirmController::class);
 
-        Route::post('password/reset', ForgotPasswordAdminController::class);
-        Route::post('password/confirm', ResetPasswordAdminController::class)->name('admin.password.confirm');
+    Route::middleware('throttle:30,1')->group(function () {
+        Route::get('email/verify/{userId}', Auth\VerificationVerifyController::class)->name('admin.verification.verify');
+        Route::post('email/verify/{userId}', Auth\VerificationConfirmController::class);
+
+        Route::post('password/reset', Auth\ForgotPasswordAdminController::class);
+        Route::post('password/confirm', Auth\ResetPasswordAdminController::class)->name('admin.password.confirm');
     });
 });
 
 Route::middleware('auth:adminUser')->group(function () {
-    Route::get('/user', ShowOwnAdminUserController::class);
-    Route::post('/logout', LogoutAdminController::class);
+    Route::get('/user', Auth\ShowOwnAdminUserController::class);
+    Route::post('/logout', Auth\LogoutAdminController::class);
 
     // AdminUser 管理者アカウント
-    Route::get('/admin-user', IndexAdminUserController::class);
-    Route::post('/admin-user', RegisterAdminController::class)->name('admin.auth.register');
-    Route::get('/admin-user/{userId}', ShowAdminUserController::class);
-    Route::put('/admin-user/{userId}', UpdateAdminUserController::class);
-    Route::delete('/admin-user/{userId}', DeleteAdminUserController::class);
+    Route::get('/admin-user', AdminUser\IndexAdminUserController::class)
+        ->name(ARP::AdminAdminUserIndex);
+    Route::post('/admin-user', Auth\RegisterAdminController::class)
+        ->name(ARP::AdminAdminUserRegister);
+    Route::get('/admin-user/{userId}', AdminUser\ShowAdminUserController::class)
+        ->name(ARP::AdminAdminUserShow)
+        ->where('userId', '[0-9]+');
+    Route::put('/admin-user/{userId}', AdminUser\UpdateAdminUserController::class)
+        ->name(ARP::AdminAdminUserUpdate)
+        ->where('userId', '[0-9]+');
+    Route::delete('/admin-user/{userId}', AdminUser\DeleteAdminUserController::class)
+        ->name(ARP::AdminAdminUserDelete)
+        ->where('userId', '[0-9]+');
 
     // Circle サークル管理
-    Route::get('/circle', IndexCircleController::class);
-    Route::get('/circle/paginate', PaginateCircleController::class);
-    Route::post('/circle', CreateCircleController::class);
-    Route::get('/circle/{id}', ShowCircleController::class);
-    Route::put('/circle/{id}', UpdateCircleController::class);
-    Route::delete('/circle/{id}', DeleteCircleController::class);
+    Route::get('/circle', Circle\IndexCircleController::class)
+        ->name(ARP::AdminCircleIndex);
+    Route::get('/circle/paginate', Circle\PaginateCircleController::class)
+        ->name(ARP::AdminCirclePaginate);
+    Route::post('/circle', Circle\CreateCircleController::class)
+        ->name(ARP::AdminCircleRegister);
+    Route::get('/circle/{circleId}', Circle\ShowCircleController::class)
+        ->name(ARP::AdminCircleShow)
+        ->where('circleId', '[0-9]+');
+    Route::put('/circle/{circleId}', Circle\UpdateCircleController::class)
+        ->name(ARP::AdminCircleUpdate)
+        ->where('circleId', '[0-9]+');
+    Route::delete('/circle/{circleId}', Circle\DeleteCircleController::class)
+        ->name(ARP::AdminCircleDelete)
+        ->where('circleId', '[0-9]+');
 
     // CircleUser サークルユーザー
-    Route::get('/user/circle', IndexAllUserController::class);
-    Route::get('/circle/{circleId}/user', IndexCircleUserController::class)->where('circleId', '[0-9]+');
-    Route::post('/circle/{circleId}/user', RegisterCircleUserController::class)->where('circleId', '[0-9]+');
-    Route::get('/circle/{circleId}/user/{userId}', ShowCircleUserController::class)->where('circleId', '[0-9]+');
-    Route::put('/circle/{circleId}/user/{userId}', UpdateCircleUserController::class)->where('circleId', '[0-9]+');
-    Route::delete('/circle/{circleId}/user/{userId}', DeleteCircleUserController::class)->where('circleId', '[0-9]+');
-    Route::get('/circle-user/{userId}', IndexCircleUserByUserIdController::class);
-    Route::post('/circle-user/{userId}/{circleId}', CreateCircleUserRelationController::class);
-    Route::delete('/circle-user/{userId}/{circleId}', DeleteCircleUserRelationController::class);
+    Route::get('/user/circle', AllUser\IndexAllUserController::class);
+    Route::get('/circle/{circleId}/user', CircleUser\IndexCircleUserController::class)
+        ->where('circleId', '[0-9]+');
+    Route::post('/circle/{circleId}/user', CircleUser\RegisterCircleUserController::class)
+        ->where('circleId', '[0-9]+');
+    Route::get('/circle/{circleId}/user/{userId}', CircleUser\ShowCircleUserController::class)
+        ->where('circleId', '[0-9]+')
+        ->where('userId', '[0-9]+');
+    Route::put('/circle/{circleId}/user/{userId}', CircleUser\UpdateCircleUserController::class)
+        ->where('circleId', '[0-9]+')
+        ->where('userId', '[0-9]+');
+    Route::delete('/circle/{circleId}/user/{userId}', CircleUser\DeleteCircleUserController::class)
+        ->where('circleId', '[0-9]+')
+        ->where('userId', '[0-9]+');
+    Route::get('/circle-user/{userId}', CircleUser\IndexCircleUserByUserIdController::class)
+        ->where('userId', '[0-9]+');
+    Route::post('/circle-user/{userId}/{circleId}', CircleUser\CreateCircleUserRelationController::class)
+        ->where('circleId', '[0-9]+')
+        ->where('userId', '[0-9]+');
+    Route::delete('/circle-user/{userId}/{circleId}', CircleUser\DeleteCircleUserRelationController::class)
+        ->where('circleId', '[0-9]+')
+        ->where('userId', '[0-9]+');
 
     // CircleNewJoy サークル新歓管理
-    Route::get('/circle/{circleId}/newjoy', IndexCircleNewJoyController::class);
-    Route::post('/circle/{circleId}/newjoy', RegisterCircleNewJoyController::class);
-    Route::get('/circle/{circleId}/newjoy/{circleNewJoyId}', ShowCircleNewJoyController::class);
-    Route::put('/circle/{circleId}/newjoy/{circleNewJoyId}', UpdateCircleNewJoyController::class);
-    Route::delete('/circle/{circleId}/newjoy/{circleNewJoyId}', DeleteCircleNewJoyController::class);
-    Route::post('/circle/{circleId}/newjoy/{circleNewJoyId}/copy', CopyCircleNewJoyController::class);
+    Route::get('/circle/{circleId}/newjoy', CircleNewJoy\IndexCircleNewJoyController::class)
+        ->where('circleId', '[0-9]+');
+    Route::post('/circle/{circleId}/newjoy', CircleNewJoy\RegisterCircleNewJoyController::class)
+        ->where('circleId', '[0-9]+');
+    Route::get('/circle/{circleId}/newjoy/{circleNewJoyId}', CircleNewJoy\ShowCircleNewJoyController::class)
+        ->where('circleId', '[0-9]+')
+        ->where('circleNewJoyId', '[0-9]+');
+    Route::put('/circle/{circleId}/newjoy/{circleNewJoyId}', CircleNewJoy\UpdateCircleNewJoyController::class)
+        ->where('circleId', '[0-9]+')
+        ->where('circleNewJoyId', '[0-9]+');
+    Route::delete('/circle/{circleId}/newjoy/{circleNewJoyId}', CircleNewJoy\DeleteCircleNewJoyController::class)
+        ->where('circleId', '[0-9]+')
+        ->where('circleNewJoyId', '[0-9]+');
+    Route::post('/circle/{circleId}/newjoy/{circleNewJoyId}/copy', CircleNewJoy\CopyCircleNewJoyController::class)
+        ->where('circleId', '[0-9]+')
+        ->where('circleNewJoyId', '[0-9]+');
 
     // CircleTag サークルタグ管理
-    Route::get('/circle/{circleId}/tag', GetCircleTagController::class);
-    Route::post('/circle/{circleId}/tag', CreateOrUpdateCircleTagController::class);
+    Route::get('/circle/{circleId}/tag', CircleTag\GetCircleTagController::class);
+    Route::post('/circle/{circleId}/tag', CircleTag\CreateOrUpdateCircleTagController::class);
 
     // Advertise 広告管理
-    Route::get('/advertise', IndexAdvertiseController::class);
-    Route::post('/advertise', CreateAdvertiseController::class);
-    Route::get('/advertise/download', DownloadAdvertiseExcelController::class);
-    Route::get('/advertise/{id}', ShowAdvertiseController::class);
-    Route::get('/advertise/{advertiseId}/download', DownloadAdvertiseCounterHistoryExcelController::class);
-    Route::put('/advertise/{id}', UpdateAdvertiseController::class);
-    Route::delete('/advertise/{id}', DeleteAdvertiseController::class);
+    Route::get('/advertise', Advertise\IndexAdvertiseController::class);
+    Route::post('/advertise', Advertise\CreateAdvertiseController::class);
+    Route::get('/advertise/download', Advertise\DownloadAdvertiseExcelController::class);
+    Route::get('/advertise/{id}', Advertise\ShowAdvertiseController::class)
+        ->where('id', '[0-9]+');
+    Route::get('/advertise/{advertiseId}/download', Advertise\DownloadAdvertiseCounterHistoryExcelController::class);
+    Route::put('/advertise/{id}', Advertise\UpdateAdvertiseController::class)
+        ->where('id', '[0-9]+');
+    Route::delete('/advertise/{id}', Advertise\DeleteAdvertiseController::class)
+        ->where('id', '[0-9]+');
 
     // Announcement お知らせ
-    Route::get('/announcement', IndexAnnouncementController::class);
-    Route::post('/announcement', CreateAnnouncementController::class);
-    Route::get('/announcement/{announcementId}', ShowAnnouncementController::class)
+    Route::get('/announcement', Announcement\IndexAnnouncementController::class);
+    Route::post('/announcement', Announcement\CreateAnnouncementController::class);
+    Route::get('/announcement/{announcementId}', Announcement\ShowAnnouncementController::class)
         ->where('announcementId', '[0-9]+');
-    Route::put('/announcement/{announcementId}', UpdateAnnouncementController::class)
+    Route::put('/announcement/{announcementId}', Announcement\UpdateAnnouncementController::class)
         ->where('announcementId', '[0-9]+');
-    Route::delete('/announcement/{announcementId}', DeleteAnnouncementController::class)
+    Route::delete('/announcement/{announcementId}', Announcement\DeleteAnnouncementController::class)
         ->where('announcementId', '[0-9]+');
-    Route::get('/announcement/fixed', FixedAdminViewAnnouncementController::class);
+    Route::get('/announcement/fixed', Announcement\FixedAdminViewAnnouncementController::class);
 
     // Storage
-    Route::post('/storage', PutStorageController::class);
+    Route::post('/storage', Storage\PutStorageController::class);
 });
