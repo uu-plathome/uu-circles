487a228c88417a945d83524c18f383851b352d76
H37kouya <h37kouya@gmail.com>
2021-06-08 19:02:38 +0900

    fix

--- api/app/Usecases/Main/Advertise/GetMainTopAdvertiseUsecase.php
+++ api/app/Usecases/Main/Advertise/GetMainTopAdvertiseUsecase.php
@@ -8,16 +8,25 @@ use App\Enum\AdvertiseType;
 use App\Enum\Property\AdvertiseProperty;
 use App\Models\Advertise;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 
 final class GetMainTopAdvertiseUsecase
 {
     public function invoke()
     {
+        Log::debug("GetMainTopAdvertiseUsecase args none");
+
         $now = Carbon::now();
 
         $advertises = Advertise::nowPublic($now)
             ->whereNotNull(AdvertiseProperty::main_image_url)
             ->whereAdvertiseType(AdvertiseType::MAIN_TOP)
+            ->select([
+                AdvertiseProperty::title,
+                AdvertiseProperty::link,
+                AdvertiseProperty::slug,
+                AdvertiseProperty::main_image_url,
+            ])
             ->get()
             ->toArray();
 
