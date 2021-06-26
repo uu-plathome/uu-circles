f59245966121ba5c700bf695a1bd51e3f6e80376
Lint Action <lint-action@samuelmeuli.com>
2021-06-26 14:55:42 +0000

    Fix code style issues with Prettier

--- main/pages/circle/[slug]/index.tsx
+++ main/pages/circle/[slug]/index.tsx
@@ -245,8 +245,8 @@ const Page: NextPage<Props> = ({
               </div>
 
               {wpPosts &&
-                wpPosts.postsExistTags &&
-                wpPosts.postsExistTags.length > 0 ? (
+              wpPosts.postsExistTags &&
+              wpPosts.postsExistTags.length > 0 ? (
                 <div className="order-5 pt-10 px-6 md:px-0">
                   <ShowCircleTitle>おすすめの投稿</ShowCircleTitle>
 
@@ -274,8 +274,8 @@ const Page: NextPage<Props> = ({
               )}
 
               {wpPosts &&
-                wpPosts.postsNotTags &&
-                wpPosts.postsNotTags.length > 0 ? (
+              wpPosts.postsNotTags &&
+              wpPosts.postsNotTags.length > 0 ? (
                 <div className="order-6 pt-10 px-6 md:px-0">
                   <ShowCircleTitle>最新の投稿</ShowCircleTitle>
 
@@ -301,8 +301,8 @@ const Page: NextPage<Props> = ({
             </div>
 
             {uuYellForCircles &&
-              uuYellForCircles.posts &&
-              uuYellForCircles.posts.length > 0 ? (
+            uuYellForCircles.posts &&
+            uuYellForCircles.posts.length > 0 ? (
               <div className="pt-10 px-6 md:px-0">
                 <ShowCircleTitle>
                   uu-yellでサークルを詳しく知ろう！
