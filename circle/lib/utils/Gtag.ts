1776903056e82ec6dbc5aaace03e04fdb095a4c7
Lint Action <lint-action@samuelmeuli.com>
2021-06-27 08:23:38 +0000

    Fix code style issues with Prettier

--- circle/lib/utils/Gtag.ts
+++ circle/lib/utils/Gtag.ts
@@ -17,7 +17,7 @@ export const GA_ID = process.env.GOOGLE_ANALYTICS_ID
 // IDが取得できない場合を想定する
 export const existsGaId = GA_ID !== ''
 
-declare const window: any;
+declare const window: any
 const gtag = window.gtag || (() => ({}))
 
 // PVを測定する
