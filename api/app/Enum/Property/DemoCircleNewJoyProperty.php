acafa28807fbbe571ad508d89d55582aef4f4588
H37kouya <h37kouya@gmail.com>
2021-06-30 22:42:06 +0900

    feat add model

---
+++ api/app/Enum/Property/DemoCircleNewJoyProperty.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Enum\Property;
+
+class DemoCircleNewJoyProperty
+{
+    const id = 'id';
+    const created_at = 'created_at';
+    const updated_at = 'updated_at';
+
+    const circle_id = 'circle_id';
+    const title = 'title';
+    const description = 'description';
+    const url = 'url';
+    const place_of_activity = 'place_of_activity';
+    const place_of_activity_detail = 'place_of_activity_detail';
+    const start_date = 'start_date';
+    const end_date = 'end_date';
+    const published = 'published';
+}
