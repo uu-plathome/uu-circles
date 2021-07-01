64125eff8339d3795ef6777fd7118eb291cb32a8
H37kouya <h37kouya@gmail.com>
2021-07-01 15:50:18 +0900

    feat システム管理者用のUpdateの修正

--- api/app/Http/Requests/Admin/Circle/UpdateCircleFormRequest.php
+++ api/app/Http/Requests/Admin/Circle/UpdateCircleFormRequest.php
@@ -11,6 +11,7 @@ use App\Models\CircleHandbill;
 use App\Models\CircleInformation;
 use App\Rules\SmallAlphaNum;
 use App\Support\Arr;
+use App\Usecases\Admin\Circle\Param\UpdateCircleForSystemUsecaseParam;
 use App\ValueObjects\CircleValueObject;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -45,6 +46,8 @@ class UpdateCircleFormRequest extends FormRequest
             ],
             CircleProperty::release                               => ['required', 'boolean'],
             CircleProperty::is_main_fixed                         => ['nullable', 'boolean'],
+            CircleProperty::is_demo_fixed                         => ['nullable', 'boolean'],
+            CircleProperty::demo_priority                         => ['nullable', 'integer', 'min:0'],
             CircleProperty::name                                  => ['required', 'string', 'max:50'],
             CircleInformationProperty::name_kana                  => ['nullable', 'string', 'max:100'],
             CircleInformationProperty::short_name                 => ['nullable', 'string', 'max:20'],
@@ -173,5 +176,75 @@ class UpdateCircleFormRequest extends FormRequest
                 'image_url' => Arr::snake_keys($this->validated())['handbill_image_url'],
             ])
         );
+    }
+
+    public function makeUpdateCircleForSystemUsecaseParam(): UpdateCircleForSystemUsecaseParam
+    {
+        $param = new UpdateCircleForSystemUsecaseParam();
+        $request = Arr::snake_keys($this->validated());
+
+        $param->circle_id = Arr::get($request, CircleProperty::id);
+        $param->slug = Arr::get($request, CircleProperty::slug);
+        $param->release = Arr::get($request, CircleProperty::release);
+        $param->name = Arr::get($request, CircleProperty::name);
+        $param->is_main_fixed = Arr::get($request, CircleProperty::is_main_fixed);
+        $param->is_demo_fixed = Arr::get($request, CircleProperty::is_demo_fixed);
+        $param->demo_priority = Arr::get($request, CircleProperty::demo_priority);
+
+        $param->circle_type = Arr::get($request, CircleInformationProperty::circle_type);
+        $param->name_kana = Arr::get($request, CircleInformationProperty::name_kana);
+        $param->short_name = Arr::get($request, CircleInformationProperty::short_name);
+        $param->prefix_name = Arr::get($request, CircleInformationProperty::prefix_name);
+        $param->description = Arr::get($request, CircleInformationProperty::description);
+        $param->common_place_of_activity = Arr::get($request, CircleInformationProperty::common_place_of_activity);
+        $param->common_place_of_activity_detail = Arr::get($request, CircleInformationProperty::common_place_of_activity_detail);
+        $param->common_date_of_activity_monday =  Arr::get($request, CircleInformationProperty::common_date_of_activity_monday);
+        $param->common_date_of_activity_tuesday = Arr::get($request, CircleInformationProperty::common_date_of_activity_tuesday);
+        $param->common_date_of_activity_wednesday = Arr::get($request, CircleInformationProperty::common_date_of_activity_wednesday);
+        $param->common_date_of_activity_thursday = Arr::get($request, CircleInformationProperty::common_date_of_activity_thursday);
+        $param->common_date_of_activity_friday = Arr::get($request, CircleInformationProperty::common_date_of_activity_friday);
+        $param->common_date_of_activity_saturday = Arr::get($request, CircleInformationProperty::common_date_of_activity_saturday);
+        $param->common_date_of_activity_sunday = Arr::get($request, CircleInformationProperty::common_date_of_activity_sunday);
+        $param->common_date_of_activity_detail = Arr::get($request, CircleInformationProperty::common_date_of_activity_detail);
+        $param->is_online_activity = Arr::get($request, CircleInformationProperty::is_online_activity);
+        $param->online_place_of_activity_detail = Arr::get($request, CircleInformationProperty::online_place_of_activity_detail);
+        $param->online_date_of_activity_monday = Arr::get($request, CircleInformationProperty::online_date_of_activity_monday);
+        $param->online_date_of_activity_tuesday = Arr::get($request, CircleInformationProperty::online_date_of_activity_tuesday);
+        $param->online_date_of_activity_wednesday = Arr::get($request, CircleInformationProperty::online_date_of_activity_wednesday);
+        $param->online_date_of_activity_thursday = Arr::get($request, CircleInformationProperty::online_date_of_activity_thursday);
+        $param->online_date_of_activity_friday = Arr::get($request, CircleInformationProperty::online_date_of_activity_friday);
+        $param->online_date_of_activity_saturday = Arr::get($request, CircleInformationProperty::online_date_of_activity_saturday);
+        $param->online_date_of_activity_sunday = Arr::get($request, CircleInformationProperty::online_date_of_activity_sunday);
+        $param->online_date_of_activity_detail = Arr::get($request, CircleInformationProperty::online_date_of_activity_detail);
+        $param->is_club_activities = Arr::get($request, CircleInformationProperty::is_club_activities);
+        $param->appealing_point1 = Arr::get($request, CircleInformationProperty::appealing_point1);
+        $param->appealing_point2 = Arr::get($request, CircleInformationProperty::appealing_point2);
+        $param->appealing_point3 = Arr::get($request, CircleInformationProperty::appealing_point3);
+        $param->admission_fee_per_year = Arr::get($request, CircleInformationProperty::admission_fee_per_year);
+        $param->number_of_members = Arr::get($request, CircleInformationProperty::number_of_members);
+        $param->public_email = Arr::get($request, CircleInformationProperty::public_email);
+        $param->twitter_url = Arr::get($request, CircleInformationProperty::twitter_url);
+        $param->facebook_url = Arr::get($request, CircleInformationProperty::facebook_url);
+        $param->instagram_url = Arr::get($request, CircleInformationProperty::instagram_url);
+        $param->line_url = Arr::get($request, CircleInformationProperty::line_url);
+        $param->youtube_url = Arr::get($request, CircleInformationProperty::youtube_url);
+        $param->homepage_url = Arr::get($request, CircleInformationProperty::homepage_url);
+        $param->peing_url = Arr::get($request, CircleInformationProperty::peing_url);
+        $param->github_url = Arr::get($request, CircleInformationProperty::github_url);
+        $param->tiktok_url = Arr::get($request, CircleInformationProperty::tiktok_url);
+        $param->participation_url = Arr::get($request, CircleInformationProperty::participation_url);
+        $param->main_image_url = Arr::get($request, CircleInformationProperty::main_image_url);
+        $param->activity_image_url1 = Arr::get($request, CircleInformationProperty::activity_image_url1);
+        $param->activity_image_url2 = Arr::get($request, CircleInformationProperty::activity_image_url2);
+        $param->activity_image_url3 = Arr::get($request, CircleInformationProperty::activity_image_url3);
+        $param->activity_image_url4 = Arr::get($request, CircleInformationProperty::activity_image_url4);
+        $param->activity_image_url5 = Arr::get($request, CircleInformationProperty::activity_image_url5);
+        $param->activity_image_url6 = Arr::get($request, CircleInformationProperty::activity_image_url6);
+        $param->wp_url = Arr::get($request, CircleInformationProperty::wp_url);
+        $param->is_view_wp_post = Arr::get($request, CircleInformationProperty::is_view_wp_post );
+        $param->wp_tag_taxonomy = Arr::get($request, CircleInformationProperty::wp_tag_taxonomy);
+        $param->handbill_image_url = Arr::get($request, 'handbill_image_url');
+
+        return $param;
     }
 }
