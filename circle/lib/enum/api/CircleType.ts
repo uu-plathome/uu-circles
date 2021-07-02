f0a024dfcb62c6dfdcb6d255f4671f3074b5cd46
Lint Action <lint-action@samuelmeuli.com>
2021-07-02 16:03:37 +0000

    Fix code style issues with Prettier

--- circle/lib/enum/api/CircleType.ts
+++ circle/lib/enum/api/CircleType.ts
@@ -20,7 +20,7 @@ export const CircleType = {
   /**
    * 学生団体.
    */
-  STUDENT_GROUP: 'STUDENT_GROUP'
+  STUDENT_GROUP: 'STUDENT_GROUP',
 } as const
 
 export type CircleTypeKey = keyof typeof CircleType
@@ -33,25 +33,32 @@ export const getAllCircleType = (): CircleType[] => Object.values(CircleType)
 /**
  * サークル種類.
  */
-export const getAllCircleTypeKey = (): CircleTypeKey[] => Object.keys(CircleType) as CircleTypeKey[]
+export const getAllCircleTypeKey = (): CircleTypeKey[] =>
+  Object.keys(CircleType) as CircleTypeKey[]
 /**
  * サークル種類.
  */
-export const isCircleType = (s: any): s is CircleType => Object.values(CircleType).includes(s)
+export const isCircleType = (s: any): s is CircleType =>
+  Object.values(CircleType).includes(s)
 
 /**
  * 公式団体.
  */
-export const isOfficialOrganization = (v: any): v is 'OFFICIAL_ORGANIZATION' => v === CircleType.OFFICIAL_ORGANIZATION
+export const isOfficialOrganization = (v: any): v is 'OFFICIAL_ORGANIZATION' =>
+  v === CircleType.OFFICIAL_ORGANIZATION
 /**
  * 非公式団体.
  */
-export const isUnofficialOrganization = (v: any): v is 'UNOFFICIAL_ORGANIZATION' => v === CircleType.UNOFFICIAL_ORGANIZATION
+export const isUnofficialOrganization = (
+  v: any
+): v is 'UNOFFICIAL_ORGANIZATION' => v === CircleType.UNOFFICIAL_ORGANIZATION
 /**
  * 届出団体.
  */
-export const isSendingOrganization = (v: any): v is 'SENDING_ORGANIZATION' => v === CircleType.SENDING_ORGANIZATION
+export const isSendingOrganization = (v: any): v is 'SENDING_ORGANIZATION' =>
+  v === CircleType.SENDING_ORGANIZATION
 /**
  * 学生団体.
  */
-export const isStudentGroup = (v: any): v is 'STUDENT_GROUP' => v === CircleType.STUDENT_GROUP
+export const isStudentGroup = (v: any): v is 'STUDENT_GROUP' =>
+  v === CircleType.STUDENT_GROUP
