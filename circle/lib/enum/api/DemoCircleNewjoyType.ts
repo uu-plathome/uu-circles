e290ff184fc5293667a122b097dae2ab6f5426ec
Lint Action <lint-action@samuelmeuli.com>
2021-07-02 16:03:37 +0000

    Fix code style issues with Prettier

--- circle/lib/enum/api/DemoCircleNewjoyType.ts
+++ circle/lib/enum/api/DemoCircleNewjoyType.ts
@@ -15,24 +15,28 @@ export const DemoCircleNewjoyType = {
   /**
    * 未来.
    */
-  FUTURE: 'FUTURE'
+  FUTURE: 'FUTURE',
 } as const
 
 export type DemoCircleNewjoyTypeKey = keyof typeof DemoCircleNewjoyType
-export type DemoCircleNewjoyType = typeof DemoCircleNewjoyType[keyof typeof DemoCircleNewjoyType]
+export type DemoCircleNewjoyType =
+  typeof DemoCircleNewjoyType[keyof typeof DemoCircleNewjoyType]
 
 /**
  * 広告種類.
  */
-export const getAllDemoCircleNewjoyType = (): DemoCircleNewjoyType[] => Object.values(DemoCircleNewjoyType)
+export const getAllDemoCircleNewjoyType = (): DemoCircleNewjoyType[] =>
+  Object.values(DemoCircleNewjoyType)
 /**
  * 広告種類.
  */
-export const getAllDemoCircleNewjoyTypeKey = (): DemoCircleNewjoyTypeKey[] => Object.keys(DemoCircleNewjoyType) as DemoCircleNewjoyTypeKey[]
+export const getAllDemoCircleNewjoyTypeKey = (): DemoCircleNewjoyTypeKey[] =>
+  Object.keys(DemoCircleNewjoyType) as DemoCircleNewjoyTypeKey[]
 /**
  * 広告種類.
  */
-export const isDemoCircleNewjoyType = (s: any): s is DemoCircleNewjoyType => Object.values(DemoCircleNewjoyType).includes(s)
+export const isDemoCircleNewjoyType = (s: any): s is DemoCircleNewjoyType =>
+  Object.values(DemoCircleNewjoyType).includes(s)
 
 /**
  * 今.
@@ -41,8 +45,10 @@ export const isNow = (v: any): v is 'NOW' => v === DemoCircleNewjoyType.NOW
 /**
  * 今日.
  */
-export const isToday = (v: any): v is 'TODAY' => v === DemoCircleNewjoyType.TODAY
+export const isToday = (v: any): v is 'TODAY' =>
+  v === DemoCircleNewjoyType.TODAY
 /**
  * 未来.
  */
-export const isFuture = (v: any): v is 'FUTURE' => v === DemoCircleNewjoyType.FUTURE
+export const isFuture = (v: any): v is 'FUTURE' =>
+  v === DemoCircleNewjoyType.FUTURE
