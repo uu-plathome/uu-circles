691c16876b607fcf93a79aeaa8d8a8819a00feec
H37kouya <h37kouya@gmail.com>
2021-07-03 18:24:25 +0900

    feat is_only_demoの追加

--- api/database/migrations/2021_07_03_182112_add_is_demo_to_circles.php
+++ api/database/migrations/2021_07_03_182112_add_is_demo_to_circles.php
@@ -14,7 +14,7 @@ class AddIsDemoToCircles extends Migration
     public function up()
     {
         Schema::table('circles', function (Blueprint $table) {
-            $table->boolean('is_only_demo')->default(false)->comment('デモ画面でのみ表示するかどうか');
+            $table->boolean('is_only_demo')->default(false)->after('is_main_fixed')->comment('デモ画面でのみ表示するかどうか');
         });
     }
 
