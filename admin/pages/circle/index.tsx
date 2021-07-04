3d81243c36c4a1f563d561f5befddcf90f6b9f5f
H37kouya <h37kouya@gmail.com>
2021-07-04 20:48:51 +0900

    fix

--- admin/pages/circle/index.tsx
+++ admin/pages/circle/index.tsx
@@ -1,76 +1,182 @@
-import { SearchTextField } from '@/components/atoms/form/SearchTextField'
-import { BaseContainer } from '@/components/layouts/BaseContainer'
-import { BaseHeader } from '@/components/layouts/BaseHeader'
-import { BaseWrapper } from '@/components/layouts/BaseWrapper'
-import { CircleListItem } from '@/components/molecules/list_items/CircleListItem'
+import { SubmitLoading } from '@/components/atoms/loading/SubmitLoading'
+import { Index } from '@/components/pages/circle/Index'
 import { useStringInput } from '@/hooks/useInput'
-import { useMediaQuery } from '@/hooks/useMediaQuery'
-import { paginateCircleList } from '@/infra/api/circle'
-import { Circle } from '@/lib/types/model/Circle'
-import {
-  faChevronCircleLeft,
-  faChevronCircleRight,
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Color from 'colors'
+import { getCircleList } from '@/infra/api/circle'
+import { CircleType } from '@/lib/enum/api/CircleType'
 import { NextPage } from 'next'
 import Head from 'next/head'
-import { FormEvent, useEffect, useState } from 'react'
-import { scroller } from 'react-scroll'
+import { useMemo, useState } from 'react'
+import useSWR from 'swr'
 
-type PaginateCircleCursor = {
-  'circles.id'?: number
-  'circleInformation.updatedAt'?: string
-  previous: boolean
-  next: boolean
-  name?: string
-} | null
-const IndexPage: NextPage = () => {
-  const [circles, setCircles] = useState<{
-    hasNext: boolean | null
-    hasPrevious: boolean | null
-    nextCursor: PaginateCircleCursor
-    previousCursor: PaginateCircleCursor
-    records: Circle[]
-  }>(undefined)
-  const { isMd } = useMediaQuery()
-  const name = useStringInput('')
+const usePageInput = ({
+  initialMaxPage,
+  pageSize
+}: { initialMaxPage: number, pageSize: number }) => {
+  const [page, setPage] = useState(1)
+  const [maxPage, setMaxPage] = useState(initialMaxPage)
 
-  const foundCircleList = async (cursor: PaginateCircleCursor = null) => {
-    setCircles(await paginateCircleList(cursor))
+  const previousPage = () => { updatePage(page - 1) }
+  const nextPage = () => { updatePage(page + 1) }
 
-    scroller.scrollTo('top', {
-      duration: 800,
-      delay: 0,
-      smooth: 'easeInOutQuart',
-    })
+  const updatePage = (newPage: number) => {
+    console.log(page)
+    if (maxPage < newPage) {
+      setPage(maxPage)
+      return
+    }
+
+    if (1 > newPage) {
+      setPage(1)
+      return
+    }
+
+    setPage(newPage)
   }
 
-  useEffect(() => {
-    const f = async () => {
-      setCircles(
-        await paginateCircleList({
-          'circles.id': null,
-          'circleInformation.updatedAt': null,
-          previous: false,
-          next: true,
-          name: name.value,
+  return {
+    page,
+    pageSize,
+    maxPage,
+    setMaxPage,
+    previousPage,
+    nextPage,
+    updatePage
+  }
+}
+
+const IndexPage: NextPage = () => {
+  const { data: originalCircles } = useSWR('/circles', getCircleList)
+
+  const searchName = useStringInput('')
+  const searchRelease = useStringInput('')
+  const searchCircleType = useStringInput('')
+
+  const page = usePageInput({
+    initialMaxPage: Math.ceil(originalCircles ? originalCircles.length / 10 : 1),
+    pageSize: 10
+  })
+
+  const searchedCircles = useMemo(() => {
+    if (!originalCircles) {
+      return {
+        circles: []
+      }
+    }
+
+    const newCircles = (() => {
+      const filteredName = originalCircles.filter((c) => {
+        if (c.name && ~c.name.indexOf(searchName.value)) {
+          return true
+        }
+
+        if (c.shortName && ~c.shortName.indexOf(searchName.value)) {
+          return true
+        }
+
+        if (c.nameKana && ~c.nameKana.indexOf(searchName.value)) {
+          return true
+        }
+
+        if (c.prefixName && ~c.prefixName.indexOf(searchName.value)) {
+          return true
+        }
+
+        if (c.slug && ~c.slug.indexOf(searchName.value)) {
+          return true
+        }
+
+        if (c.description && ~c.description.indexOf(searchName.value)) {
+          return true
+        }
+
+        return false
+      })
+
+      const filteredRelease = searchRelease.value === ''
+        ? filteredName
+        : filteredName.filter((c) => {
+          if (searchRelease.value === 'true' && c.release === true) {
+            return true
+          }
+
+          if (searchRelease.value === 'false' && c.release === false) {
+            return true
+          }
+
+          return false
         })
+
+      const filteredCircleType = searchCircleType.value === ''
+        ? filteredRelease
+        : filteredRelease.filter((c) => {
+          if (
+            searchCircleType.value === CircleType.OFFICIAL_ORGANIZATION
+            && c.circleType === CircleType.OFFICIAL_ORGANIZATION
+          ) {
+            return true
+          }
+
+          if (
+            searchCircleType.value === CircleType.SENDING_ORGANIZATION
+            && c.circleType === CircleType.SENDING_ORGANIZATION
+          ) {
+            return true
+          }
+
+          if (
+            searchCircleType.value === CircleType.STUDENT_GROUP
+            && c.circleType === CircleType.STUDENT_GROUP
+          ) {
+            return true
+          }
+
+          if (
+            searchCircleType.value === CircleType.UNOFFICIAL_ORGANIZATION
+            && c.circleType === CircleType.UNOFFICIAL_ORGANIZATION
+          ) {
+            return true
+          }
+
+          if (
+            searchCircleType.value === '不明'
+            && ['', null].includes(c.circleType)
+          ) {
+            return true
+          }
+
+          return false
+        })
+
+      page.setMaxPage(Math.ceil(filteredCircleType ? filteredCircleType.length / 10 : 1))
+
+      return filteredCircleType
+    })()
+
+    return {
+      circles: newCircles.slice(
+        (page.page - 1) * page.pageSize,
+        page.page * page.pageSize
       )
     }
-    f()
-  }, [])
+  }, [
+    originalCircles,
+    page.page,
+    page.pageSize,
+    searchName.value,
+    searchRelease.value,
+    searchCircleType.value,
+  ])
 
-  const onSearchSubmit = (event: FormEvent) => {
-    event.preventDefault()
+  useMemo(() => {
+    page.updatePage(1)
+  }, [
+    searchName.value,
+    searchRelease.value,
+    searchCircleType.value,
+  ])
 
-    foundCircleList({
-      'circles.id': null,
-      'circleInformation.updatedAt': null,
-      previous: false,
-      next: true,
-      name: name.value,
-    })
+  if (!originalCircles) {
+    return <SubmitLoading isOpen={true} />
   }
 
   return (
@@ -79,97 +185,18 @@ const IndexPage: NextPage = () => {
         <title>サークル一覧へようこそ</title>
       </Head>
 
-      {isMd ? <BaseHeader /> : ''}
-
-      <BaseContainer>
-        <BaseWrapper
-          title="サークル一覧へようこそ"
-          actionText="サークル新規作成"
-          actionHref="/circle/create"
-        >
-          <div className="border-2 border-gray-800 p-2">
-            {circles ? (
-              <div className="py-4 mb-8">
-                <p className="text-white">サークル名検索</p>
-
-                <form onSubmit={onSearchSubmit}>
-                  <SearchTextField
-                    id="nameSearch"
-                    name="nameSearch"
-                    expand
-                    {...name}
-                  />
-                </form>
-              </div>
-            ) : (
-              ''
-            )}
-
-            {circles && circles.records.length > 0
-              ? circles.records.map((circle: Circle) => {
-                  return (
-                    <CircleListItem
-                      key={`circle-${circle.id}`}
-                      circle={circle}
-                    />
-                  )
-                })
-              : ''}
-
-            {circles && circles.records.length === 0 ? (
-              <div className="py-4">
-                <p className="text-white">まだサークルが登録されていません</p>
-              </div>
-            ) : (
-              ''
-            )}
-
-            {circles ? (
-              <div className="text-center">
-                <button
-                  className="mx-2 disabled:opacity-50 "
-                  disabled={!circles.hasPrevious}
-                  onClick={() =>
-                    foundCircleList({
-                      ...circles.previousCursor,
-                      previous: true,
-                      next: false,
-                      name: name.value,
-                    })
-                  }
-                >
-                  <FontAwesomeIcon
-                    color={Color.white}
-                    icon={faChevronCircleLeft}
-                    size="2x"
-                  />
-                </button>
-
-                <button
-                  className="mx-2 disabled:opacity-50 "
-                  disabled={!circles.hasNext}
-                  onClick={() =>
-                    foundCircleList({
-                      ...circles.nextCursor,
-                      previous: false,
-                      next: true,
-                      name: name.value,
-                    })
-                  }
-                >
-                  <FontAwesomeIcon
-                    color={Color.white}
-                    icon={faChevronCircleRight}
-                    size="2x"
-                  />
-                </button>
-              </div>
-            ) : (
-              ''
-            )}
-          </div>
-        </BaseWrapper>
-      </BaseContainer>
+      <Index
+        circles={searchedCircles.circles}
+        searchValue={{
+          name: searchName,
+          release: searchRelease,
+          circleType: searchCircleType
+        }}
+        hasPrevious={page.page !== 1}
+        hasNext={page.page !== page.maxPage}
+        onPrevious={page.previousPage}
+        onNext={page.nextPage}
+      />
     </div>
   )
 }
