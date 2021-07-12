aee98ff55f353e34c7b1b4216d7eee5abf971699
H37kouya <h37kouya@gmail.com>
2021-07-12 11:36:19 +0900

    feature gacha resultの追加

--- main/pages/gacha/result/[gachaHash].tsx
+++ main/pages/gacha/result/[gachaHash].tsx
@@ -2,13 +2,38 @@ import { BaseFooter } from '@/components/layouts/BaseFooter'
 import { BaseHead } from '@/components/layouts/BaseHead'
 import { BaseLayout } from '@/components/layouts/BaseLayout'
 import { BaseContainer } from '@/components/molecules/Container/BaseContainer'
-import { NextPage } from 'next'
+import { PageNotFoundError } from '@/infra/api/error'
+import { resultGacha } from '@/infra/api/gacha'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import Error from 'next/error'
 import Image from 'next/image'
 
-const Page: NextPage = () => {
+type Props = {
+  count?: number
+  createdAt?: string
+  gachaHash?: string
+  pickupCircles?: {
+    handbillImageUrl: string
+    name: string
+    slug: string
+  }[]
+  resultCircles?: {
+    handbillImageUrl: string
+    name: string
+    slug: string
+  }[]
+  errorCode?: number
+}
+const Page: NextPage<Props> = ({
+  errorCode
+}) => {
+  if (errorCode) {
+    return <Error statusCode={errorCode} />
+  }
+
   return (
     <div>
-      <BaseHead title="サークルガチャ" />
+      <BaseHead title="サークルガチャ結果" />
 
       <BaseLayout>
         <div className="bg-gray-100 px-2 pb-36">
@@ -25,5 +50,39 @@ const Page: NextPage = () => {
     </div>
   )
 }
+
+
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  if (!params.slug || Array.isArray(params.gachaHash)) {
+    return {
+      notFound: true,
+    }
+  }
+
+  try {
+    const res = await resultGacha({
+      gachaHash: params.gachaHash
+    })
+
+    return {
+      props: res,
+      revalidate: 60 * 60 * 24,
+    }
+  } catch (e) {
+    if (e instanceof PageNotFoundError) {
+      return {
+        notFound: true,
+      }
+    }
+
+    return { props: { errorCode: 500 } }
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: [],
+  fallback: 'blocking',
+})
 
 export default Page
