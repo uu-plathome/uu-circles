3877eb7b5c99e03a2af7ce41833043a91f8bec5a
H37kouya <h37kouya@gmail.com>
2021-07-16 01:00:22 +0900

    update

--- main/pages/gacha/index.tsx
+++ main/pages/gacha/index.tsx
@@ -53,8 +53,8 @@ const Page: NextPage<Props> = ({ pickupCircle }) => {
             </div>
 
             {pickupCircle.list &&
-            Array.isArray(pickupCircle.list) &&
-            pickupCircle.list.length > 0 ? (
+              Array.isArray(pickupCircle.list) &&
+              pickupCircle.list.length > 0 ? (
               <div className="flex justify-center">
                 <div style={{ width: 360 }}>
                   <div className="flex justify-center items-center mb-4">
@@ -113,7 +113,7 @@ const Page: NextPage<Props> = ({ pickupCircle }) => {
               ''
             )}
 
-            <div className="py-8">
+            <div className="py-8 flex justify-center">
               <GreenButton href="/gacha/history">ガチャ結果一覧</GreenButton>
             </div>
           </BaseContainer>
