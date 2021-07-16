6ca23a80c2895f7f91fe4800e4bb43dac692768f
Lint Action <lint-action@samuelmeuli.com>
2021-07-16 00:09:34 +0000

    Fix code style issues with Prettier

--- main/pages/gacha/history.tsx
+++ main/pages/gacha/history.tsx
@@ -49,7 +49,9 @@ const Page: NextPage<Props> = () => {
               <Image src="/images/gacha-logo.png" width="360" height="120" />
             </div>
 
-            <h1 className="text-2xl pt-6 px-4 text-center font-bold">- ガチャ結果 -</h1>
+            <h1 className="text-2xl pt-6 px-4 text-center font-bold">
+              - ガチャ結果 -
+            </h1>
 
             <h2 className="text-lg pt-6 px-4 text-center">
               もう一度ガチャを引く
@@ -89,7 +91,9 @@ const Page: NextPage<Props> = () => {
                             className="rounded border border-gray-300"
                           >
                             <Image
-                              src={gachaResult.resultCircles[0].handbillImageUrl}
+                              src={
+                                gachaResult.resultCircles[0].handbillImageUrl
+                              }
                               width="60"
                               height="60"
                             />
@@ -98,7 +102,9 @@ const Page: NextPage<Props> = () => {
                           <div className="pl-2">
                             <h3 className="font-bold text-lg mb-2">
                               {gachaResult.resultCircles[0].name}
-                              {gachaResult.resultCircles.length > 1 ? 'など' : ''}
+                              {gachaResult.resultCircles.length > 1
+                                ? 'など'
+                                : ''}
                             </h3>
                             <p className="text-sm max-line-2 text-gray-400">
                               {gachaResult.resultCircles.length}連ガチャ
