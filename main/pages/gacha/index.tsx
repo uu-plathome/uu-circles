c6516af109f16b9ad3b2ef8e7e635566a00ff932
H37kouya <h37kouya@gmail.com>
2021-07-17 01:16:41 +0900

    fix

--- main/pages/gacha/index.tsx
+++ main/pages/gacha/index.tsx
@@ -42,9 +42,9 @@ const Page: NextPage<Props> = ({ pickupCircle }) => {
                     1連ガチャ
                   </div>
                 </Link>
-                <Link href="/gacha/draw/10">
+                <Link href="/gacha/draw/5">
                   <div className="flex-initial rounded-md bg-green-500 px-4 md:px-8 py-4 text-white mx-4 my-6 font-black cursor-pointer shadow-sm hover:shadow-md">
-                    10連ガチャ
+                    5連ガチャ
                   </div>
                 </Link>
               </div>
