ed11aa53e31b1cef8951825497168e77bbeff39f
H37kouya <h37kouya@gmail.com>
2021-07-17 00:58:35 +0900

    fix

--- main/pages/gacha/history.tsx
+++ main/pages/gacha/history.tsx
@@ -41,80 +41,82 @@ const Page: NextPage<Props> = () => {
       <BaseLayout>
         <div className="bg-gray-100 px-2 pb-36">
           <BaseContainer>
-            <div className="flex justify-center items-center pt-6">
-              <Image src="/images/gacha-logo.png" width="360" height="120" />
-            </div>
-
-            <h1 className="text-2xl pt-6 px-4 text-center font-bold">
-              - ガチャ結果 -
-            </h1>
-
-            <h2 className="text-lg pt-6 px-4 text-center">
-              もう一度ガチャを引く
-            </h2>
-
-            <div className="flex justify-center">
-              <Link href="/gacha/draw/1">
-                <div className="flex-initial rounded bg-green-500 px-4 md:px-8 py-4 text-white mx-4 my-6 font-black cursor-pointer shadow-sm hover:shadow-md">
-                  1連ガチャ
-                </div>
-              </Link>
-              <Link href="/gacha/draw/10">
-                <div className="flex-initial rounded bg-green-500 px-4 md:px-8 py-4 text-white mx-4 my-6 font-black cursor-pointer shadow-sm hover:shadow-md">
-                  10連ガチャ
-                </div>
-              </Link>
-            </div>
-
-            <h2 className="text-lg pt-6 px-4 text-center mb-4">ガチャ結果</h2>
-
             <div className="px-4">
-              {gachaHistory &&
-                gachaHistory.history.list &&
-                gachaHistory.history.list.map((gachaResult, index) => {
-                  return (
-                    <div
-                      key={`gachaHistory-${index}-container-${gachaResult.gachaHash}`}
-                      className="mb-4 cursor-pointer"
-                    >
-                      <Link
-                        href="/gacha/result/[gachaHash]"
-                        as={`/gacha/result/${gachaResult.gachaHash}`}
+              <div className="flex justify-center items-center pt-6">
+                <Image src="/images/gacha-logo.png" width="360" height="120" />
+              </div>
+
+              <h1 className="text-2xl pt-6 px-4 text-center font-bold">
+                - ガチャ結果 -
+              </h1>
+
+              <h2 className="text-lg pt-6 px-4 text-center">
+                もう一度ガチャを引く
+              </h2>
+
+              <div className="flex justify-center">
+                <Link href="/gacha/draw/1">
+                  <div className="flex-initial rounded-md bg-green-500 px-4 md:px-8 py-4 text-white mx-4 my-6 font-black cursor-pointer shadow-sm hover:shadow-md">
+                    1連ガチャ
+                  </div>
+                </Link>
+                <Link href="/gacha/draw/10">
+                  <div className="flex-initial rounded-md bg-green-500 px-4 md:px-8 py-4 text-white mx-4 my-6 font-black cursor-pointer shadow-sm hover:shadow-md">
+                    10連ガチャ
+                  </div>
+                </Link>
+              </div>
+
+              <h2 className="text-lg pt-6 px-4 text-center mb-4">ガチャ結果</h2>
+
+              <div className="px-4">
+                {gachaHistory &&
+                  gachaHistory.history.list &&
+                  gachaHistory.history.list.map((gachaResult, index) => {
+                    return (
+                      <div
+                        key={`gachaHistory-${index}-container-${gachaResult.gachaHash}`}
+                        className="mb-4 cursor-pointer"
                       >
-                        <div className="rounded bg-white flex items-center px-6 py-4">
-                          <div
-                            style={{ minWidth: 60 }}
-                            className="rounded border border-gray-300"
-                          >
-                            <Image
-                              src={
-                                gachaResult.resultCircles[0].handbillImageUrl
-                              }
-                              width="60"
-                              height="60"
-                            />
-                          </div>
+                        <Link
+                          href="/gacha/result/[gachaHash]"
+                          as={`/gacha/result/${gachaResult.gachaHash}`}
+                        >
+                          <div className="rounded bg-white flex items-center px-6 py-4">
+                            <div
+                              style={{ minWidth: 60 }}
+                              className="rounded border border-gray-300"
+                            >
+                              <Image
+                                src={
+                                  gachaResult.resultCircles[0].handbillImageUrl
+                                }
+                                width="60"
+                                height="60"
+                              />
+                            </div>
 
-                          <div className="pl-2">
-                            <h3 className="font-bold text-lg mb-2">
-                              {gachaResult.resultCircles[0].name}
-                              {gachaResult.resultCircles.length > 1
-                                ? 'など'
-                                : ''}
-                            </h3>
-                            <p className="text-sm max-line-2 text-gray-400">
-                              {gachaResult.resultCircles.length}連ガチャ
-                            </p>
+                            <div className="pl-2">
+                              <h3 className="font-bold text-lg mb-2">
+                                {gachaResult.resultCircles[0].name}
+                                {gachaResult.resultCircles.length > 1
+                                  ? 'など'
+                                  : ''}
+                              </h3>
+                              <p className="text-sm max-line-2 text-gray-400">
+                                {gachaResult.resultCircles.length}連ガチャ
+                              </p>
+                            </div>
                           </div>
-                        </div>
-                      </Link>
-                    </div>
-                  )
-                })}
-            </div>
+                        </Link>
+                      </div>
+                    )
+                  })}
+              </div>
 
-            <div className="py-8 flex justify-center">
-              <GreenButton href="/gacha">ガチャTOP</GreenButton>
+              <div className="py-8 flex justify-center">
+                <GreenButton href="/gacha">ガチャTOP</GreenButton>
+              </div>
             </div>
           </BaseContainer>
         </div>
