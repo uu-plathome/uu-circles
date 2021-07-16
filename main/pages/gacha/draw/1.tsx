9e90c2abe95c695c4680bff9c20e111763ce392d
H37kouya <h37kouya@gmail.com>
2021-07-16 23:39:57 +0900

    fix

--- main/pages/gacha/draw/1.tsx
+++ main/pages/gacha/draw/1.tsx
@@ -2,21 +2,12 @@ import { BaseFooter } from '@/components/layouts/BaseFooter'
 import { BaseHead } from '@/components/layouts/BaseHead'
 import { BaseLayout } from '@/components/layouts/BaseLayout'
 import { BaseContainer } from '@/components/molecules/Container/BaseContainer'
-import { drawGacha, SimpleGachaDto } from '@/infra/api/gacha'
-import {
-  isOfficialOrganization,
-  isStudentGroup,
-  isUnofficialOrganization,
-} from '@/lib/enum/api/CategorySlugProperty'
-import { isSendingOrganization } from '@/lib/enum/api/CircleType'
+import { drawGacha } from '@/infra/api/gacha'
 import { GachaMovieId } from '@/lib/enum/app/GachaMovieId'
 import { LocalStorageKey } from '@/lib/enum/app/LocalStorageKey'
 import { NextPage } from 'next'
 import { useRouter } from 'next/dist/client/router'
 import Image from 'next/image'
-import Link from 'next/link'
-import { useEffect, useMemo } from 'react'
-import { useState } from 'react'
 import YouTube, { Options } from 'react-youtube'
 
 const opts: Options = {
@@ -26,73 +17,26 @@ const opts: Options = {
     // https://developers.google.com/youtube/player_parameters
     autoplay: 1,
     mute: 1,
+    controls: 0,
+    modestbranding: 1,
+    playsinline: 1,
   },
-}
-
-const getGachaMovieId = (simpleGachaDto: SimpleGachaDto): GachaMovieId => {
-  if (!simpleGachaDto) {
-    return GachaMovieId.C
-  }
-
-  if (simpleGachaDto.isClubActivity) {
-    return GachaMovieId.A
-  }
-
-  if (
-    isOfficialOrganization(simpleGachaDto.circleType) ||
-    isSendingOrganization(simpleGachaDto.circleType)
-  ) {
-    return GachaMovieId.B
-  }
-
-  if (isUnofficialOrganization(simpleGachaDto.circleType)) {
-    return GachaMovieId.C
-  }
-
-  if (isStudentGroup(simpleGachaDto.circleType)) {
-    return GachaMovieId.D
-  }
-
-  return GachaMovieId.C
 }
 
 const Page: NextPage = () => {
   const router = useRouter()
-  const [gachaHash, setGachaHash] = useState(null)
-  const [gachaData, setGachaData] = useState<{
-    count: number
-    createdAt: string
-    gachaHash: string
-    pickupCircles: SimpleGachaDto[]
-    resultCircles: SimpleGachaDto[]
-  }>(null)
-
-  useEffect(() => {
-    const f = async () => {
-      setGachaHash(null)
-      setGachaData(null)
-
-      const identifierHash = localStorage.getItem(
-        LocalStorageKey.identifierHash
-      )
-
-      const data = await drawGacha({
-        identifierHash,
-        num: 1,
-      })
-
-      setGachaHash(data.gachaHash)
-      setGachaData(data)
-    }
-    f()
-  }, [])
-
-  const nowGachaMovieId = useMemo(() => {
-    return getGachaMovieId(gachaData.resultCircles[0])
-  }, gachaData.resultCircles)
 
   const redirect = async () => {
-    await router.push(`/gacha/result/${gachaHash}`)
+    const identifierHash = localStorage.getItem(
+      LocalStorageKey.identifierHash
+    )
+
+    const data = await drawGacha({
+      identifierHash,
+      num: 1,
+    })
+
+    await router.push(`/gacha/result/${data.gachaHash}`)
   }
 
   return (
@@ -111,32 +55,22 @@ const Page: NextPage = () => {
                 className="w-full relative h-0 overflow-hidden mb-12"
                 style={{ paddingBottom: '56.25%' }}
               >
-                {gachaHash && gachaData && gachaData.resultCircles && Array.isArray(gachaData.resultCircles) ? (
-                  <YouTube
-                    className="w-full h-full absolute top-0 left-0"
-                    videoId={nowGachaMovieId}
-                    opts={opts}
-                    onEnd={redirect}
-                  />
-                ) : (
-                  ''
-                )}
+                <YouTube
+                  className="w-full h-full absolute top-0 left-0"
+                  videoId={GachaMovieId.B}
+                  opts={opts}
+                  onEnd={redirect}
+                />
               </div>
             </div>
 
             <div className="flex justify-end">
-              {gachaHash ? (
-                <Link
-                  href="/gacha/result/[gachaHash]"
-                  as={`/gacha/result/${gachaHash}`}
-                >
-                  <a className="underline text-blue-600 text-right cursor-pointer">
-                    動画が再生されない場合はこちらをクリック
-                  </a>
-                </Link>
-              ) : (
-                ''
-              )}
+              <a
+                onClick={redirect}
+                className="underline text-blue-600 text-right cursor-pointer"
+              >
+                動画が再生されない場合はこちらをクリック
+              </a>
             </div>
           </BaseContainer>
         </div>
