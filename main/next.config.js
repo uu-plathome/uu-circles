fedd28d1717f772f381eeeaaa35f229e77e2f240
H37kouya <h37kouya@gmail.com>
2021-08-06 16:05:56 +0900

    feature colorsをsrc/stylesに移動

--- main/next.config.js
+++ main/next.config.js
@@ -1,7 +1,7 @@
-const withPWA = require('next-pwa')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
+const withPWA = require('next-pwa')
 
 module.exports = withBundleAnalyzer(
   withPWA({
