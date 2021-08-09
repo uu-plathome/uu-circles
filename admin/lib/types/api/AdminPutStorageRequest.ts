153d121b22ef297c0e925f90c1a230147b34f97c
Lint Action <lint-action@samuelmeuli.com>
2021-08-09 10:53:40 +0000

    Fix code style issues with Prettier

--- admin/lib/types/api/AdminPutStorageRequest.ts
+++ admin/lib/types/api/AdminPutStorageRequest.ts
@@ -17,5 +17,9 @@ export interface AdminPutStorageRequestValidationError {
   message: string
 }
 
-export const isAdminPutStorageRequest = (v: any): v is AdminPutStorageRequest => v && v.type === 'AdminPutStorageRequest'
-export const isAdminPutStorageRequestValidationError = (v: any): v is AdminPutStorageRequestValidationError => v && v.type === 'AdminPutStorageRequestValidationError'
+export const isAdminPutStorageRequest = (v: any): v is AdminPutStorageRequest =>
+  v && v.type === 'AdminPutStorageRequest'
+export const isAdminPutStorageRequestValidationError = (
+  v: any
+): v is AdminPutStorageRequestValidationError =>
+  v && v.type === 'AdminPutStorageRequestValidationError'
