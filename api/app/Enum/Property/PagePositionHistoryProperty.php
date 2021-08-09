be6d134547cbfd20e787cd9904e427079f296c06
H37kouya <h37kouya@gmail.com>
2021-08-09 18:56:03 +0900

    feature 画面幅の記録をできるようにした

--- api/app/Enum/Property/PagePositionHistoryProperty.php
+++ api/app/Enum/Property/PagePositionHistoryProperty.php
@@ -9,6 +9,8 @@ final class PagePositionHistoryProperty
     const page_url = 'page_url';
     const page_name = 'page_name';
     const page_position_id = 'page_position_id';
+    const screen_width = 'screen_width';
+    const screen_height = 'screen_height';
     const created_at = 'created_at';
     const updated_at = 'updated_at';
 }
