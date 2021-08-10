2e27a9bb98d71a895b067079b84bda3c02b0bb9b
H37kouya <h37kouya@gmail.com>
2021-08-10 17:33:58 +0900

    update

--- main/tailwind.config.js
+++ main/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
   ],
+  mode: 'jit',
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     screens: {
