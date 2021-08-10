a6021e3b208760ff611f57052473433a82fcd725
H37kouya <h37kouya@gmail.com>
2021-08-10 17:28:18 +0900

    feature tailwindのバージョンアップ

--- main/tailwind.config.js
+++ main/tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
   ],
-  presets: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     screens: {
