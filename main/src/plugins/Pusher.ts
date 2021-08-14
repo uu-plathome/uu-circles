f2e9712976cd9e28fb9011f4307595de377e74ec
H37kouya <h37kouya@gmail.com>
2021-08-14 17:41:49 +0900

    refactor Pusher.logToConsole

--- main/src/plugins/Pusher.ts
+++ main/src/plugins/Pusher.ts
@@ -3,6 +3,8 @@ import Pusher from 'pusher-js'
 const PUSHER_KEY = 'a9b069e2da6cbb2a3766'
 const channelName = 'page-position-channel'
 
+Pusher.logToConsole = process.env.NODE_ENV === 'development'
+
 export const pusher = new Pusher(PUSHER_KEY, {
   cluster: 'ap3',
 })
