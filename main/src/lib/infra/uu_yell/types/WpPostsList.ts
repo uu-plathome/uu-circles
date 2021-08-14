e1a38393cbb5fc25677b62a473b80af84dbaa239
H37kouya <h37kouya@gmail.com>
2021-08-14 20:16:53 +0900

    feature infraにどけた

---
+++ main/src/lib/infra/uu_yell/types/WpPostsList.ts
@@ -0,0 +1,6 @@
+import { WP_REST_API_Attachments, WP_REST_API_Posts } from "wp-types";
+
+export type WpPostsList = {
+  posts: WP_REST_API_Posts
+  medias: WP_REST_API_Attachments
+}
--- main/src/pages/circle/[slug]/index.tsx
+++ main/src/pages/circle/[slug]/index.tsx
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import Error from 'next/error'
 import { useState } from 'react'
@@ -11,15 +10,13 @@ import { usePagePosition } from '@/src/hooks/usePagePosition'
 import { AnnouncementType } from '@/src/lib/enum/api/AnnouncementType'
 import { CircleTagModel } from '@/src/lib/enum/api/CircleTagModel'
 import { Importance } from '@/src/lib/enum/api/Importance'
-import { ApiUrl } from '@/src/lib/enum/app/ApiUrl'
 import { LocalStorageKey } from '@/src/lib/enum/app/LocalStorageKey'
 import { getCircleBySlug } from '@/src/lib/infra/api/circle'
 import { PageNotFoundError } from '@/src/lib/infra/api/error'
+import { fetchPostsByCircle } from '@/src/lib/infra/uu_yell/fetchPostsByCircle'
 import { Announcement } from '@/src/lib/types/model/Announcement'
 import { Circle } from '@/src/lib/types/model/Circle'
 import { CircleNewJoy } from '@/src/lib/types/model/CircleNewJoy'
-
-const UU_YELL_URL = ApiUrl.UU_YELL
 
 type Props = {
   circle?: Circle
@@ -52,49 +49,13 @@ const Page: NextPage<Props> = ({
   const { data: uuYellForCircles } = useSWR<{
     posts: WP_REST_API_Post[]
     medias: WP_REST_API_Attachment[]
-  }>(['/circle/[slug]', circle.slug], async () => {
-    const fetchedPosts = await Promise.all([
-      axios.get<WP_REST_API_Post[]>(
-        `${UU_YELL_URL}/wp-json/wp/v2/posts?context=embed&search=${circle.name}`
-      ),
-      axios.get<WP_REST_API_Post[]>(
-        `${UU_YELL_URL}/wp-json/wp/v2/posts?context=embed&search=https://uu-circles.com/circle/${circle.name}`
-      ),
-    ])
-
-    const allPosts = [...fetchedPosts[0].data, ...fetchedPosts[1].data]
-
-    // 重複している投稿の削除
-    const postIds = [
-      ...new Set([
-        ...fetchedPosts[0].data.map((post) => post.id),
-        ...fetchedPosts[1].data.map((post) => post.id),
-      ]),
-    ]
-    const posts = postIds.map((postId) =>
-      allPosts.find((post) => post.id === postId)
-    )
-
-    if (posts.length === 0) {
-      return {
-        posts: [],
-        medias: [],
-      }
-    }
-
-    const mediaIds = posts.map((post) => post.featured_media)
-    const queryMediaIds = mediaIds.join(',')
-
-    // 記事のアイキャッチ画像のURLを取得
-    const fetchedMedias = await axios.get<WP_REST_API_Attachment[]>(
-      `${UU_YELL_URL}/wp-json/wp/v2/media?context=embed&include=${queryMediaIds}`
-    )
-
-    return {
-      posts,
-      medias: fetchedMedias.data,
-    }
-  })
+  }>(['/circle/[slug]', circle.slug],
+    () => fetchPostsByCircle({
+      circleSlug: circle.slug,
+      circleName: circle.name,
+      circleShortName: circle.shortName,
+    })
+  )
 
   const { onChangeId } = usePagePosition({
     pageUrl: `/circle/${circle.slug}`,
