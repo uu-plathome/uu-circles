63fa571077fe44aff243e8ab1ce11fdf07e24335
H37kouya <h37kouya@gmail.com>
2021-08-26 10:26:57 +0900

    add seeder

--- api/app/Enum/SlugProperty/CategorySlugProperty.php
+++ api/app/Enum/SlugProperty/CategorySlugProperty.php
@@ -2,11 +2,23 @@
 
 namespace App\Enum\SlugProperty;
 
-class CategorySlugProperty
+final class CategorySlugProperty
 {
+    /**
+     * 公認団体.
+     */
     const official_organization = 'official_organization';
+    /**
+     * 非公認団体.
+     */
     const unofficial_organization = 'unofficial_organization';
+    /**
+     * 学生団体.
+     */
     const student_group = 'student_group';
+    /**
+     * 部活.
+     */
     const club = 'club';
 
     public static function getAll(): array
---
+++ api/database/seeders/TagPageViewsSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\Property\CircleTagProperty;
+use App\Enum\Property\TagPageViewProperty;
+use App\Enum\SlugProperty\TagSlugProperty;
+use App\Models\TagPageView;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+
+class TagPageViewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = Carbon::now();
+        $insertData = $this->insertData($now);
+        TagPageView::insert($insertData);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function insertData(Carbon $now): array
+    {
+        return (new Collection(TagSlugProperty::getAll()))->map(
+            fn (string $tag) => [
+                TagPageViewProperty::tag_name     => $tag,
+                TagPageViewProperty::page_views   => random_int(1e2, 1e8),
+                TagPageViewProperty::active_users => random_int(0, 1e3),
+                TagPageViewProperty::created_at   => $now,
+                TagPageViewProperty::updated_at   => $now,
+            ]
+        )->toArray();
+    }
+}
