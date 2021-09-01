df6f1398b81c60e0c2d6e0aaea5d8a49bef812ba
Lint Action <lint-action@samuelmeuli.com>
2021-09-01 03:03:45 +0000

    Fix code style issues with Prettier

--- main/jest.config.js
+++ main/jest.config.js
@@ -28,5 +28,5 @@ module.exports = {
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$',
   ],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
