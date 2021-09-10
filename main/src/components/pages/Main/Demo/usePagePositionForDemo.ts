aa371fa1020cf21942244b93fabb2edf0fa58517
H37kouya <h37kouya@gmail.com>
2021-09-10 10:24:02 +0900

    feat うたにアニメーションの追加

--- main/src/components/pages/Main/Demo/usePagePositionForDemo.ts
+++ main/src/components/pages/Main/Demo/usePagePositionForDemo.ts
@@ -1,11 +1,11 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { TOP_BUTTONS_ID_LIST } from '../Parts/MainUucircleTopButtons/top_buttons_id_list'
 import { ID_LIST } from '../id_list'
 import {
   UsePagePosition,
   UsePagePositionArg,
 } from '@/src/hooks/usePagePosition'
-import { PagePositions } from '@/src/lib/types/model/PagePosition'
+import { PagePositionItem } from '@/src/lib/types/model/PagePosition'
 
 type CircleSlugArg = {
   candidateCircleSlug: string[]
@@ -18,109 +18,56 @@ export const usePagePositionForDemo = ({
   candidateCircleSlug,
 }: UsePagePositionArg & CircleSlugArg): UsePagePosition => {
   const createdAt = useMemo(() => new Date().toString(), [])
-
-  const pageData: PagePositions = useMemo(() => {
-    return {
+  const [pagePositions, setPagePositions] = useState<PagePositionItem[]>(
+    pagePositionsDemo1({
       pageUrl,
-      pagePositions: [
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: ID_LIST.HEADER_CATCH_COPY,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: ID_LIST.RECOMMEND_TAG_LIST,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: ID_LIST.RECOMMEND_TAG_LIST,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: ID_LIST.UU_CIRCLES_AD,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: ID_LIST.UU_YELL_ARTICLES,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: ID_LIST.UU_YELL_ARTICLES,
-          createdAt,
-        },
-        {
-          pagePositionHistoryId: -1,
-          pageUrl,
-          pagePositionId: ID_LIST.SPONSORSHIP_FOOTER,
-          createdAt,
-        },
-        ...candidateCircleSlug.map((_slug) => ({
-          pagePositionHistoryId: -1,
-          pageUrl: `/circle/${_slug}`,
-          pagePositionId: '',
-          circleSlug: _slug,
-          createdAt,
-        })),
-      ],
-    }
-  }, [pageUrl, createdAt, candidateCircleSlug])
+      createdAt,
+      candidateCircleSlug,
+    })
+  )
+
+  useEffect(() => {
+    setInterval(() => {
+      const randInt = Math.floor( Math.random() * 3 ) /** 0, 1, 2 */
+
+      if (randInt === 0) {
+        setPagePositions(
+          pagePositionsDemo1({
+            pageUrl,
+            createdAt,
+            candidateCircleSlug,
+          })
+        )
+      }
+
+      if (randInt === 1) {
+        setPagePositions(
+          pagePositionsDemo2({
+            pageUrl,
+            createdAt,
+            candidateCircleSlug,
+          })
+        )
+      }
+
+      if (randInt > 1) {
+        setPagePositions(
+          pagePositionsDemo3({
+            pageUrl,
+            createdAt,
+            candidateCircleSlug,
+          })
+        )
+      }
+    }, 1000 * 30)
+  }, [])
 
   return {
     pagePositionId: '',
-    pageData,
+    pageData: {
+      pageUrl,
+      pagePositions
+    },
     pageUrl,
     circleSlug,
     identifierHash,
@@ -129,4 +76,328 @@ export const usePagePositionForDemo = ({
       return
     },
   }
+}
+
+const pagePositionsDemo1 = ({
+  pageUrl,
+  createdAt,
+  candidateCircleSlug,
+}: {
+  pageUrl: PagePositionItem['pageUrl'],
+  createdAt: PagePositionItem['createdAt'],
+  candidateCircleSlug: CircleSlugArg['candidateCircleSlug'],
+}): PagePositionItem[] => {
+  return [
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.HEADER_CATCH_COPY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.RECOMMEND_TAG_LIST,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.RECOMMEND_TAG_LIST,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_CIRCLES_AD,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_YELL_ARTICLES,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_YELL_ARTICLES,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.SPONSORSHIP_FOOTER,
+      createdAt,
+    },
+    ...candidateCircleSlug.map((_slug) => ({
+      pagePositionHistoryId: -1,
+      pageUrl: `/circle/${_slug}`,
+      pagePositionId: '',
+      circleSlug: _slug,
+      createdAt,
+    })),
+  ]
+}
+
+const pagePositionsDemo2 = ({
+  pageUrl,
+  createdAt,
+  candidateCircleSlug,
+}: {
+  pageUrl: PagePositionItem['pageUrl'],
+  createdAt: PagePositionItem['createdAt'],
+  candidateCircleSlug: CircleSlugArg['candidateCircleSlug'],
+}): PagePositionItem[] => {
+  return [
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.HEADER_CATCH_COPY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_NEW_JOY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.RECOMMEND_TAG_LIST,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_CIRCLES_AD,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_CIRCLES_AD,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_YELL_ARTICLES,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_YELL_ARTICLES,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_YELL_ARTICLES,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.SPONSORSHIP_FOOTER,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.SPONSORSHIP_FOOTER,
+      createdAt,
+    },
+    ...candidateCircleSlug.map((_slug) => ({
+      pagePositionHistoryId: -1,
+      pageUrl: `/circle/${_slug}`,
+      pagePositionId: '',
+      circleSlug: _slug,
+      createdAt,
+    })),
+  ]
+}
+
+const pagePositionsDemo3 = ({
+  pageUrl,
+  createdAt,
+  candidateCircleSlug,
+}: {
+  pageUrl: PagePositionItem['pageUrl'],
+  createdAt: PagePositionItem['createdAt'],
+  candidateCircleSlug: CircleSlugArg['candidateCircleSlug'],
+}): PagePositionItem[] => {
+  return [
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.HEADER_CATCH_COPY,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_DISCORD,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_DISCORD,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_DISCORD,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: TOP_BUTTONS_ID_LIST.TOP_BUTTONS_TO_NEW_STUDENTS,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.RECOMMEND_TAG_LIST,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.RECOMMEND_TAG_LIST,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_CIRCLES_AD,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_YELL_ARTICLES,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.UU_YELL_ARTICLES,
+      createdAt,
+    },
+    {
+      pagePositionHistoryId: -1,
+      pageUrl,
+      pagePositionId: ID_LIST.SPONSORSHIP_FOOTER,
+      createdAt,
+    },
+    ...candidateCircleSlug.map((_slug) => ({
+      pagePositionHistoryId: -1,
+      pageUrl: `/circle/${_slug}`,
+      pagePositionId: '',
+      circleSlug: _slug,
+      createdAt,
+    })),
+  ]
 }
