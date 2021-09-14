b11bd505207c7ca8c6f8bfd86c4b7a58a4cd015a
H37kouya <h37kouya@gmail.com>
2021-09-15 00:19:30 +0900

    fix

--- circle/tailwind.config.js
+++ circle/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('./colors')
 
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     screens: {
