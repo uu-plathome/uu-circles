93873227abae9d3291fe476c7df5bc4e19375298
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-12-09 20:03:10 +0900

    fixed typo

--- batch/newjoy_notification_js/yell_bot.ts
+++ batch/newjoy_notification_js/yell_bot.ts
@@ -5,7 +5,6 @@ const client = new Discord.Client();
 // import { Channel, Client } from 'discord.js';
 const { exit } = require('process');
 
-
 /* python-shellモジュールのインポート */
 // var {PythonShell} = require('python-shell');
 
@@ -25,7 +24,7 @@ client.login(token);
 client.on('ready', () => {
     // 起動するとconsoleにready...と表示される
     console.log('ready...');
-    // チャンネルに
+    // チャンネルにメッセージを送信
     client.channels.cache.get(process.env.TEST_CHANNEL_ID).send('!');
 });
 
