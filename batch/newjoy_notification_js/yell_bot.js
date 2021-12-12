34cc47699a9f0918b9267499e087ffebc6145370
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-12-12 23:59:36 +0900

    feat sending messages

---
+++ batch/newjoy_notification_js/yell_bot.js
@@ -0,0 +1,38 @@
+// discord.js モジュールのインポート
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// import { Channel, Client } from 'discord.js';
+const { exit } = require('process');
+
+/* python-shellモジュールのインポート */
+var {PythonShell} = require('python-shell');
+var pyshell = new PythonShell('create_text.py', {mode : 'text'})
+
+pyshell.send("");
+
+pyshell.on('message', function(data){
+    console.log(data);
+    console.log('python shell started');
+})
+
+/* .envの読み込み */
+require('dotenv').config();
+// bot自体のtoken
+const token = process.env.TOKEN;
+// 本番チャンネル
+// const channel_id = process.env.;
+// testチャンネル
+const test_channel_id = process.env.TEST_CHANNEL_ID;
+
+/* Discordへの接続 */
+client.login(token);
+
+/* 起動時の処理 */
+client.on('ready', () => {
+    // 起動するとconsoleにready...と表示される
+    console.log('ready...');
+    // チャンネルにメッセージを送信
+    client.channels.cache.get(test_channel_id).send('!');
+});
+
---
+++ batch/newjoy_notification_js/yell_bot.ts
@@ -0,0 +1,30 @@
+// discord.js モジュールのインポート
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// import { Channel, Client } from 'discord.js';
+const { exit } = require('process');
+
+/* python-shellモジュールのインポート */
+// var {PythonShell} = require('python-shell');
+
+/* .envの読み込み */
+require('dotenv').config();
+// bot自体のtoken
+const token = process.env.TOKEN;
+// 本番チャンネル
+// const channel_id = process.env.;
+// testチャンネル
+const test_channel_id = process.env.TEST_CHANNEL_ID;
+
+/* Discordへの接続 */
+client.login(token);
+
+/* 起動時の処理 */
+client.on('ready', () => {
+    // 起動するとconsoleにready...と表示される
+    console.log('ready...');
+    // チャンネルにメッセージを送信
+    client.channels.cache.get(process.env.TEST_CHANNEL_ID).send('!');
+});
+
