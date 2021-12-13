13959032e85f404864a9875f01ad9f31b3964097
H37kouya <h37kouya@gmail.com>
2021-12-13 23:25:27 +0900

    feature uu-yellの記事を取得する関数追加

--- batch/newjoy_notification_js/hello.ts
+++ batch/newjoy_notification_js/hello.ts
@@ -1,4 +1,20 @@
-import { exit } from 'process';
+import { dayjs } from './src/plugins/dayjs'
+import { fetchLatestUuYellPosts } from './src/infra/fetchUuYell'
+
 
 console.log('Hello, World!');
-exit(0);
+
+// 現在時刻
+const dtNow = dayjs()
+// 1日前の時刻
+const dtBefore = dayjs().add(-1, 'day')
+console.log({ dtNow, dtBefore });
+
+(async () => {
+    // uu-yell の記事取得
+    const posts = await fetchLatestUuYellPosts({
+        dtNow,
+        dtBefore,
+    })
+    console.log({ posts })
+})()
