4556272994d7c34dac18022d8a0d9ebd9a90c51a
s3igo <85787242+s3igo@users.noreply.github.com>
2023-03-27 13:51:49 +0900

    fix: 暗黙のchildrenを修正

--- admin/src/components/atoms/buttons/GreenButton.tsx
+++ admin/src/components/atoms/buttons/GreenButton.tsx
@@ -8,6 +8,7 @@ type Props = {
   as?: Url
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
   href?: LinkProps['href']
+  children: React.ReactNode
 }
 
 const GreenButton: React.FC<Props> = ({
