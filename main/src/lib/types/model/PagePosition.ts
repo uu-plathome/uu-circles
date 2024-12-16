5e8bad74a72455218233324ebe904958fdf0acbe
HinataKikuchi <haiao4041@gmail.com>
2024-12-16 22:21:13 +0900

    🚧 : demoのみ非同期処理を走らせるの厳しそう(DemoとMain templateと共用してるところある)(Work In Progress)

---
+++ main/src/lib/types/model/PagePosition.ts
@@ -0,0 +1,15 @@
+/**
+ * ページ位置
+ */
+export interface PagePositions {
+    pageUrl: string
+    pagePositions: PagePositionItem[]
+  }
+  
+  export interface PagePositionItem {
+    pagePositionHistoryId: number
+    pageUrl: string
+    pagePositionId: string
+    circleSlug?: string
+    createdAt: string
+  }
